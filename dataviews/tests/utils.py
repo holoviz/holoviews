b4def952aa0fe8dd8b322c5744f20469b189b629
jlstevens <jlstevens@ed.ac.uk>
2014-05-16 14:27:24 +0100

    Improved IPTestCase and removed deprecated IPHookTestCase

--- dataviews/tests/utils.py
+++ dataviews/tests/utils.py
@@ -228,53 +228,29 @@ class IPTestCase(ViewTestCase):
     """
     This class extends ViewTestCase to handle IPython specific objects.
     """
+
     def setUp(self):
         super(IPTestCase, self).setUp()
+        try:
+            import IPython
+            self.ip = IPython.InteractiveShell()
+            if self.ip is None:
+                raise TypeError()
+        except Exception:
+                raise SkipTest("IPython could not be started")
+
         self.addTypeEqualityFunc(HTML, self.skip_comparison)
         self.addTypeEqualityFunc(SVG,  self.skip_comparison)
 
     def skip_comparison(self, obj1, obj2, msg):
         pass
 
-
-class IPHookTestCase(IPTestCase):
-    """
-    This class
-    """
-
-    def setUp(self):
-        super(IPHookTestCase, self).setUp()
-        self.ip = self.get_ipython()
-        self.cell("from dataviews.ipython import view_display")
-        self.load_extension()
-        self.set_display_hooks()
-        self.object = None
-        # Removes unnecessary IPython prompts (and moves one line up)
-        self.cell("%config PromptManager.out_template = '\x1b\x5b\x31\x41'")
-
-    def get_ipython(self):
-        from IPython import get_ipython
-        return get_ipython()
-
-    def proxy_hook(self, hook):
-        def capture_hook(obj,  pprinter, cycles):
-            self.object = obj
-            return hook(obj)
-        return capture_hook
-
-    def get_object(self, line):
-        self.ip.run_cell(line)
-        obj = self.object
-        self.object = None
+    def get_object(self, name):
+        obj = self.ip._object_find(name).obj
+        if obj is None:
+            raise self.failureException("Could not find object %s" % name)
         return obj
 
-    def set_display_hooks(self):
-        html_formatter = self.ip.display_formatter.formatters['text/html']
-        printers = dict((tp, self.proxy_hook(h)) for (tp,h) in html_formatter.type_printers.items())
-        self.ip.display_formatter.formatters['text/plain'].type_printers = printers
-
-    def load_extension(self):
-        self.ip.run_line_magic("load_ext", "dataviews.ipython")
 
     def cell(self, line):
         "Run an IPython cell"
@@ -283,3 +259,8 @@ class IPHookTestCase(IPTestCase):
     def cell_magic(self, *args, **kwargs):
         "Run an IPython cell magic"
         self.ip.run_cell_magic(*args, **kwargs)
+
+
+    def line_magic(self, *args, **kwargs):
+        "Run an IPython line magic"
+        self.ip.run_line_magic(*args, **kwargs)
