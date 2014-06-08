36784b779a62e4011452a82a4f63e9186858f704
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2014-06-08 02:07:59 +0100

    Added ViewSelector widget

--- dataviews/ipython/widgets.py
+++ dataviews/ipython/widgets.py
@@ -1,16 +1,32 @@
 import sys, math, time
 
+import numpy as np
+from collections import OrderedDict
+
+from nose.plugins.skip import SkipTest
+
 try:
     import IPython
     from IPython.core.display import clear_output
 except:
     clear_output = None
-    from nose.plugins.skip import SkipTest
     raise SkipTest("IPython extension requires IPython >= 0.12")
-
-import param
+from IPython.display import display
+from IPython.core.pylabtools import print_figure
+try:
+    from IPython.html.widgets import interactive
+    from IPython.html import widgets
+except:
+    raise SkipTest("IPython widgets require IPython >= 2.0")
 ipython2 = (IPython.version_info[0] == 2)
 
+import param
+
+from .. import GridLayout, NdMapping, Stack
+from ..options import options
+from ..views import Layout
+from ..plots import GridLayoutPlot, Plot
+from .display_hooks import get_plot_size
 
 
 class ProgressBar(param.Parameterized):
@@ -101,3 +117,207 @@ class RunProgress(ProgressBar):
         if remaining != 0:
             self.run_hook(remaining)
             super(RunProgress, self).__call__(100)
+
+
+
+class FixedValueSliderWidget(widgets.FloatSliderWidget):
+    """
+    Subclass of FloatSliderWidget that jumps discretely
+    between a set of supplied values.
+    """
+
+    def __init__(self, values=[], *args, **kwargs):
+        value = round(values[0], 5)
+        vmin = min(values)
+        vmax = max(values)
+        step = min(abs(np.diff(values))) if len(values) > 1 else 0
+        self.values = np.array(values)
+        widgets.DOMWidget.__init__(self, step=step, min=vmin, max=vmax,
+                                   value=value, *args, **kwargs)
+        self.on_trait_change(self._snap_value, ['value'])
+        self.time = time.time()
+
+
+    def _snap_value(self, name, old_val, new_val):
+        """
+        Snap value to the closest specified value.
+        """
+        if self.time+0.05 > time.time():
+            return
+        diffs = np.abs(self.values - new_val)
+        idx = np.argmin(diffs)
+        val = self.values[idx]
+        if val != self.value:
+            self.value = round(val, 5)
+            self.time = time.time()
+
+
+def isnumeric(val):
+    try:
+        float(val)
+        return True
+    except:
+        return False
+
+
+class ViewSelector(param.Parameterized):
+    """
+    Interactive widget to select and view View objects contained
+    in an NdMapping. ViewSelector creates Slider and Dropdown widgets
+    for each dimension contained within the supplied object and
+    an image widget for the plotted View. All widgets are dynamically
+    updated to match the current selection.
+    """
+
+    cached = param.Boolean(default=True, doc="""
+        Whether to cache the View plots when initializing the object.""")
+
+    css = param.Dict(default={'margin-left': 'auto',
+                              'margin-right': 'auto'}, doc="""
+                              CSS to apply to the widgets.""")
+
+    def __init__(self, view, **params):
+        super(ViewSelector, self).__init__(**params)
+
+        self._process_view(view)
+        self._initialize_widgets()
+        self.refresh = True
+
+        if self.cached:
+            self.frames = OrderedDict((k, print_figure(self.plot[idx]))
+                                      for idx, k in enumerate(self._keys))
+
+
+    def _process_view(self, view):
+        """
+        Determine the dimensions and keys to be turned into widgets and
+        initialize the plots.
+        """
+        if isinstance(view, (GridLayout, Layout)):
+            view = GridLayout([view]) if isinstance(view, Layout) else view
+            shape = view.shape
+            grid_size = (shape[1]*get_plot_size()[1],
+                         shape[0]*get_plot_size()[0])
+            self.plot = GridLayoutPlot(view, **dict(size=grid_size))
+
+            keys_list = []
+            for v in view:
+                if isinstance(v, Layout): v = v.main
+                keys_list.append(list(v._data.keys()))
+
+            # Check if all elements in the Grid have common dimensions
+            if all(x == keys_list[0] for x in keys_list):
+                self._keys = keys_list[0]
+                element = view[0, 0]
+                if isinstance(element, Layout):
+                    self.dimensions = element.main.dimensions
+                else:
+                    self.dimensions = element.dimensions
+            else:
+                self._keys = [(k,) for k in range(len(view))]
+                self.dimensions = ['Frame']
+        elif isinstance(view, Stack):
+            opts = dict(options.plotting(view).opts, size=get_plot_size())
+            self.plot = Plot.defaults[view.type](view, **opts)
+            self._keys = view._data.keys()
+            self.dimensions = view.dimensions
+
+        # Create mock NdMapping to hold the common dimensions and keys
+        self.mock_obj = NdMapping([(k, 0) for k in self._keys],
+                                  dimensions=self.dimensions)
+
+
+    def _initialize_widgets(self):
+        """
+        Initialize widgets and dimension values.
+        """
+
+        self.pwidgets = {}
+        self.dim_val = {}
+        for didx, dim in enumerate(self.mock_obj.dimension_labels):
+            all_vals = [k[didx] for k in self._keys]
+
+            # Initialize dimension value
+            vals = self._get_dim_vals(list(self._keys[0]), didx)
+            self.dim_val[dim] = vals[0]
+
+            # Initialize widget
+            if isnumeric(vals[0]):
+                widget_type = FixedValueSliderWidget
+            else:
+                widget_type = widgets.DropdownWidget
+                all_vals = dict((str(v), v) for v in all_vals)
+            self.pwidgets[dim] = widget_type(values=all_vals)
+
+
+    def __call__(self):
+        # Initalize image widget
+        self.image_widget = widgets.ImageWidget()
+        if self.cached:
+            self.image_widget.value = list(self.frames.values())[0]
+        else:
+            self.image_widget.value = print_figure(self.plot[self._keys[0]])
+        self.image_widget.set_css(self.css)
+
+        # Initialize interactive widgets
+        interactive_widget = interactive(self.update_widgets, **self.pwidgets)
+        interactive_widget.set_css(self.css)
+
+        # Display widgets
+        display(interactive_widget)
+        display(self.image_widget)
+
+
+    def _get_dim_vals(self, indices, idx):
+        """
+        Get the dimension values along the supplied dimension,
+        computed from the supplied indices into the mock_obj.
+        """
+        indices[idx] = slice(None)
+        vals = [k[idx] if isinstance(k, tuple) else k
+                for k in self.mock_obj[tuple(indices)].keys()]
+        return vals
+
+
+    def update_widgets(self, **kwargs):
+        """
+        Callback method to process the new keys, find the closest matching
+        View and update all the widgets.
+        """
+
+        # Do nothing if dimension values are unchanged
+        if all(v==self.dim_val[k] for k, v in kwargs.items()):
+            return
+
+        # Place changed dimensions first
+        changed_fn = lambda x: x[1] == self.dim_val[x[0]]
+        dimvals = sorted(kwargs.items(), key=changed_fn)
+
+        # Find the closest matching key along each dimension and update
+        # the matching widget accordingly.
+        checked = [slice(None) for i in range(self.mock_obj.ndims)]
+        for dim, val in dimvals:
+            if not isnumeric(val): val = str(val)
+            dim_idx = self.mock_obj.dim_index(dim)
+            widget = self.pwidgets[dim]
+            vals = self._get_dim_vals(checked, dim_idx)
+            if val not in vals:
+                if isnumeric(val):
+                    val = vals[np.argmin(np.abs(np.array(vals) - val))]
+                else:
+                    val = str(vals[0])
+            if isnumeric(val):
+                if len(vals) == 0: widget.step = 0
+                widget.min = min(vals)
+                widget.max = max(vals)
+            checked[dim_idx] = val
+            self.dim_val[dim] = val
+            widget.value = val
+
+        # Update frame
+        checked = tuple(checked)
+        if self.cached:
+            self.image_widget.value = self.frames[checked]
+        else:
+            figure = print_figure(self.plot[self._keys.index(checked)])
+            self.image_widget.value = figure
