58c246a7887f3dc27547616e1994570fb0159b4a
philippjfr <P.Rudiger@ed.ac.uk>
2014-10-30 01:11:59 +0000

    Removed jinja2 unicode escaping function from widgets

--- holoviews/ipython/widgets.py
+++ holoviews/ipython/widgets.py
@@ -437,32 +437,6 @@ class ViewSelector(NdWidget):
 
 
 
-_js_escapes = {
-        '\\': '\\u005C',
-        '\'': '\\u0027',
-        '"': '\\u0022',
-        '>': '\\u003E',
-        '<': '\\u003C',
-        '&': '\\u0026',
-        '=': '\\u003D',
-        '-': '\\u002D',
-        ';': '\\u003B',
-        u'\u2028': '\\u2028',
-        u'\u2029': '\\u2029'
-}
-# Escape every ASCII character with a value less than 32.
-_js_escapes.update(('%c' % z, '\\u%04X' % z) for z in xrange(32))
-def jinja2_escapejs_filter(value):
-    import jinja2
-    retval = []
-    for letter in value:
-        if _js_escapes.has_key(letter):
-            retval.append(_js_escapes[letter])
-        else:
-            retval.append(letter)
-
-    return jinja2.Markup("".join(retval))
-
 class JSSelector(NdWidget):
     """
     Javascript based widget to select and view View objects contained
