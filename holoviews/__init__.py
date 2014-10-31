23bc08b63fe066af35cb270a165c65cb70c30a6a
jlstevens <jlstevens@ed.ac.uk>
2014-10-31 00:51:20 +0000

    Simplified __init__.py to expose the appropriate API

--- holoviews/__init__.py
+++ holoviews/__init__.py
@@ -7,16 +7,12 @@ import param
 __version__ = param.Version(release=(0,7), fpath=__file__,
                             commit="$Format:%h$", reponame='holoviews')
 
-from .core import boundingregion, sheetcoords
-from .core import * # pyflakes:ignore (API import)
-from .ipython import * # pyflakes:ignore (API import)
-from .operation import * # pyflakes:ignore (API import)
-from .view import * # pyflakes:ignore (API import)
+from .core import options                     # pyflakes:ignore (API import)
+from .core.dimension import Dimension         # pyflakes:ignore (API import)
+from .core.boundingregion import BoundingBox  # pyflakes:ignore (API import)
+from .core.layer import *                     # pyflakes:ignore (API import)
+from .core.layout import *                    # pyflakes:ignore (API import)
+from .core.sheetcoords import *               # pyflakes:ignore (API import)
 
-def public(obj):
-    if not isinstance(obj, type): return False
-    baseclasses = [NdMapping, View, Dimension, Overlay, ViewOperation]
-    return any([issubclass(obj, bc) for bc in baseclasses])
-
-_public = list(set([_k for _k, _v in locals().items() if public(_v)]))
-__all__ = _public + ["boundingregion", "ipython", "operation", "plotting", "sheetcoords" ]
+from .ipython import *              # pyflakes:ignore (API import)
+from .view import *                 # pyflakes:ignore (API import)
