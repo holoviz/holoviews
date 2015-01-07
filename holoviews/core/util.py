67f52260e12504ec6146e6e712a1eab29e2dab49
philippjfr <P.Rudiger@ed.ac.uk>
2015-01-07 17:08:10 +0000

    Fixed Py3 incompatibility in core/util.py

--- holoviews/core/util.py
+++ holoviews/core/util.py
@@ -17,9 +17,9 @@ def find_minmax(lims, olims):
 
 def int_to_roman(input):
    if type(input) != type(1):
-      raise TypeError, "expected integer, got %s" % type(input)
+      raise TypeError("expected integer, got %s" % type(input))
    if not 0 < input < 4000:
-      raise ValueError, "Argument must be between 1 and 3999"
+      raise ValueError("Argument must be between 1 and 3999")
    ints = (1000, 900,  500, 400, 100,  90, 50,  40, 10,  9,   5,  4,   1)
    nums = ('M',  'CM', 'D', 'CD','C', 'XC','L','XL','X','IX','V','IV','I')
    result = ""
