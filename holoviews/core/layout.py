9c6c17ad71d8c4fe207a35825825c6dd4d5a1a4b
philippjfr <P.Rudiger@ed.ac.uk>
2015-01-07 17:24:18 +0000

    ViewTree now uses util.py int_to_roman

--- holoviews/core/layout.py
+++ holoviews/core/layout.py
@@ -15,6 +15,7 @@ from .dimension import Dimension, Dimensioned
 from .ndmapping import NdMapping
 from .options import options
 from .tree import AttrTree
+from .util import int_to_roman
 from .view import View, Map
 
 
@@ -319,21 +320,6 @@ class ViewTree(AttrTree):
                 raise KeyError('Index %s is outside available item range' % str(key))
             key = keys[idx]
         return super(ViewTree, self).__getitem__(key)
-
-    @staticmethod
-    def int_to_roman(input):
-       if type(input) != type(1):
-          raise TypeError, "expected integer, got %s" % type(input)
-       if not 0 < input < 4000:
-          raise ValueError, "Argument must be between 1 and 3999"
-       ints = (1000, 900,  500, 400, 100,  90, 50,  40, 10,  9,   5,  4,   1)
-       nums = ('M',  'CM', 'D', 'CD','C', 'XC','L','XL','X','IX','V','IV','I')
-       result = ""
-       for i in range(len(ints)):
-          count = int(input / ints[i])
-          result += nums[i] * count
-          input -= ints[i] * count
-       return result
 
     @property
     def shape(self):
