21ae4e032930ac142150476dff3c74b92632e6bf
jlstevens <jlstevens@ed.ac.uk>
2015-02-05 19:01:21 +0000

    Added 51 new unit tests for composites built with the + and * operators

---
+++ tests/testcomposites.py
@@ -0,0 +1,311 @@
+"""
+Test cases for the Comparisons class over the composite types:
+
+LayoutTree (the + operator)
+Overlay    (the * operator)
+
+It does *not* test HoloMaps.
+"""
+
+from holoviews import Element, LayoutTree, Overlay
+from holoviews.element.comparison import ComparisonTestCase
+
+
+class ElementTestCase(ComparisonTestCase):
+
+    def setUp(self):
+        self.el1 = Element('data1')
+        self.el2 = Element('data2')
+        self.el3 = Element('data3')
+        self.el4 = Element('data4', value='ValA')
+        self.el5 = Element('data5', value='ValB')
+        self.el6 = Element('data6', label='LabelA')
+        self.el7 = Element('data7', value='ValA', label='LabelA')
+        self.el8 = Element('data8', value='ValA', label='LabelB')
+
+    def test_element_init(self):
+        Element('data1')
+
+
+class LayoutTreeTestCase(ElementTestCase):
+
+    def setUp(self):
+        super(LayoutTreeTestCase, self).setUp()
+
+    def test_layouttree_keys(self):
+        t = self.el1 + self.el2
+        self.assertEqual(t.keys(),
+                         [('Element', 'I'), ('Element', 'II')])
+
+    def test_layouttree_keys(self):
+        t = self.el1 + self.el2
+        self.assertEqual(t.values(), [self.el1, self.el2])
+
+    def test_triple_layouttree_keys(self):
+        t = self.el1 + self.el2 + self.el3
+        expected_keys = [('Element', 'I'), ('Element', 'II'), ('Element', 'III')]
+        self.assertEqual(t.keys(), expected_keys)
+
+    def test_triple_layouttree_values(self):
+        t = self.el1 + self.el2 + self.el3
+        self.assertEqual(t.values(), [self.el1 , self.el2 , self.el3])
+
+    def test_layouttree_varying_value_keys(self):
+        t = self.el1 + self.el4
+        self.assertEqual(t.keys(), [('Element', 'I'), ('ValA', 'I')])
+
+    def test_layouttree_varying_value_keys2(self):
+        t = self.el4 + self.el5
+        self.assertEqual(t.keys(), [('ValA', 'I'), ('ValB', 'I')])
+
+    def test_triple_layouttree_varying_value_keys(self):
+        t = self.el1 + self.el4 + self.el2 + self.el3
+        expected_keys = [('Element', 'I'), ('ValA', 'I'), ('Element', 'II'), ('Element', 'III')]
+        self.assertEqual(t.keys(), expected_keys)
+
+    def test_four_layouttree_varying_value_values(self):
+        t = self.el1 + self.el4 + self.el2 + self.el3
+        self.assertEqual(t.values(), [self.el1 , self.el4 , self.el2 , self.el3])
+
+    def test_layouttree_varying_label_keys(self):
+        t = self.el1 + self.el6
+        self.assertEqual(t.keys(), [('Element', 'I'), ('Element', 'LabelA')])
+
+    def test_triple_layouttree_varying_label_keys(self):
+        t = self.el1 + self.el6 + self.el2
+        expected_keys = [('Element', 'I'), ('Element', 'LabelA'), ('Element', 'II')]
+        self.assertEqual(t.keys(), expected_keys)
+
+    def test_layouttree_varying_label_keys2(self):
+        t = self.el7 + self.el8
+        self.assertEqual(t.keys(), [('ValA', 'LabelA'), ('ValA', 'LabelB')] )
+
+    def test_layouttree_varying_label_and_values_keys(self):
+        t = self.el6 + self.el7 + self.el8
+        expected_keys = [('Element', 'LabelA'), ('ValA', 'LabelA'), ('ValA', 'LabelB')]
+        self.assertEqual(t.keys(), expected_keys)
+
+    def test_layouttree_varying_label_and_values_values(self):
+        t = self.el6 + self.el7 + self.el8
+        self.assertEqual(t.values(), [self.el6, self.el7, self.el8])
+
+    def test_layouttree_associativity(self):
+        t1 = (self.el1 + self.el2 + self.el3)
+        t2 = ((self.el1 + self.el2) + self.el3)
+        t3 = (self.el1 + (self.el2 + self.el3))
+        self.assertEqual(t1.keys(), t2.keys())
+        self.assertEqual(t2.keys(), t3.keys())
+
+    def test_layouttree_from_view1(self):
+        t = LayoutTree.from_view(self.el1)
+        self.assertEqual(t.keys(),  [('Element', 'I')])
+
+    def test_layouttree_from_view2(self):
+        t = LayoutTree.from_view(self.el8)
+        self.assertEqual(t.keys(),  [('ValA', 'LabelB')])
+
+    def test_layouttree_group(self):
+        t1 = (self.el1 + self.el2)
+        t2 = t1.group('NewValue')
+        self.assertEqual(t2.keys(), [('NewValue', 'I'), ('NewValue', 'II')])
+
+
+
+class OverlayTestCase(ElementTestCase):
+    """
+    The tests here match those in LayoutTreeTestCase; Overlays inherit
+    from LayoutTree and behave in a very similar way (except for being
+    associated with * instead of the + operator)
+    """
+
+    def setUp(self):
+        super(OverlayTestCase, self).setUp()
+
+    def test_overlay_keys(self):
+        t = self.el1 * self.el2
+        self.assertEqual(t.keys(), [('Element', 'I'), ('Element', 'II')])
+
+    def test_overlay_values(self):
+        t = self.el1 * self.el2
+        self.assertEqual(t.values(), [self.el1, self.el2])
+
+    def test_triple_overlay_keys(self):
+        t = self.el1 * self.el2 * self.el3
+        expected_keys = [('Element', 'I'), ('Element', 'II'), ('Element', 'III')]
+        self.assertEqual(t.keys(), expected_keys)
+
+    def test_triple_overlay_values(self):
+        t = self.el1 * self.el2 * self.el3
+        self.assertEqual(t.values(), [self.el1 , self.el2 , self.el3])
+
+    def test_overlay_varying_value_keys(self):
+        t = self.el1 * self.el4
+        self.assertEqual(t.keys(), [('Element', 'I'), ('ValA', 'I')])
+
+    def test_overlay_varying_value_keys2(self):
+        t = self.el4 * self.el5
+        self.assertEqual(t.keys(), [('ValA', 'I'), ('ValB', 'I')])
+
+    def test_triple_overlay_varying_value_keys(self):
+        t = self.el1 * self.el4 * self.el2 * self.el3
+        expected_keys = [('Element', 'I'), ('ValA', 'I'), ('Element', 'II'), ('Element', 'III')]
+        self.assertEqual(t.keys(), expected_keys)
+
+    def test_four_overlay_varying_value_values(self):
+        t = self.el1 * self.el4 * self.el2 * self.el3
+        self.assertEqual(t.values(), [self.el1 , self.el4 , self.el2 , self.el3])
+
+    def test_overlay_varying_label_keys(self):
+        t = self.el1 * self.el6
+        self.assertEqual(t.keys(), [('Element', 'I'), ('Element', 'LabelA')])
+
+    def test_triple_overlay_varying_label_keys(self):
+        t = self.el1 * self.el6 * self.el2
+        expected_keys = [('Element', 'I'), ('Element', 'LabelA'), ('Element', 'II')]
+        self.assertEqual(t.keys(), expected_keys)
+
+    def test_overlay_varying_label_keys2(self):
+        t = self.el7 * self.el8
+        self.assertEqual(t.keys(), [('ValA', 'LabelA'), ('ValA', 'LabelB')] )
+
+    def test_overlay_varying_label_and_values_keys(self):
+        t = self.el6 * self.el7 * self.el8
+        expected_keys = [('Element', 'LabelA'), ('ValA', 'LabelA'), ('ValA', 'LabelB')]
+        self.assertEqual(t.keys(), expected_keys)
+
+    def test_overlay_varying_label_and_values_values(self):
+        t = self.el6 * self.el7 * self.el8
+        self.assertEqual(t.values(), [self.el6, self.el7, self.el8])
+
+    def test_deep_overlay_keys(self):
+        o1 = (self.el1 * self.el2)
+        o2 = (self.el1 * self.el2)
+        o3 = (self.el1 * self.el2)
+        t = (o1 * o2 * o3)
+        expected_keys = [('Element', 'I'), ('Element', 'II'), ('Element', 'III'),
+                         ('Element', 'IV'), ('Element', 'V'), ('Element', 'VI')]
+        self.assertEqual(t.keys(), expected_keys)
+
+    def test_deep_overlay_values(self):
+        o1 = (self.el1 * self.el2)
+        o2 = (self.el1 * self.el2)
+        o3 = (self.el1 * self.el2)
+        t = (o1 * o2 * o3)
+        self.assertEqual(t.values(), [self.el1 , self.el2,
+                                      self.el1 , self.el2,
+                                      self.el1 , self.el2])
+
+    def test_overlay_associativity(self):
+        o1 = (self.el1 * self.el2 * self.el3)
+        o2 = ((self.el1 * self.el2) * self.el3)
+        o3 = (self.el1 * (self.el2 * self.el3))
+        self.assertEqual(o1.keys(), o2.keys())
+        self.assertEqual(o2.keys(), o3.keys())
+
+    def test_overlay_from_view1(self):
+        t = Overlay.from_view(self.el1)
+        self.assertEqual(t.keys(),  [('Element', 'I')])
+
+    def test_overlay_from_view2(self):
+        t = Overlay.from_view(self.el8)
+        self.assertEqual(t.keys(),  [('ValA', 'LabelB')])
+
+    def test_overlay_group(self):
+        t1 = (self.el1 * self.el2)
+        t2 = t1.group('NewValue')
+        self.assertEqual(t2.keys(), [('NewValue', 'I'), ('NewValue', 'II')])
+
+
+
+class CompositeTestCase(ElementTestCase):
+    """
+    Test case for trees involving both + (LayoutTree) and * (Overlay)
+    """
+
+    def test_composite1(self):
+        t = (self.el1 * self.el2) + (self.el1 * self.el2)
+        self.assertEqual(t.keys(),  [('Overlay', 'I'), ('Overlay', 'II')])
+
+    def test_composite_relabelled_value1(self):
+        t = (self.el1 * self.el2) + (self.el1 * self.el2).relabel(value='Val2')
+        self.assertEqual(t.keys(), [('Overlay', 'I'), ('Val2', 'I')])
+
+    def test_composite_relabelled_label1(self):
+        t = ((self.el1 * self.el2)
+             + (self.el1 * self.el2).relabel(value='Val1', label='Label2'))
+        self.assertEqual(t.keys(), [('Overlay', 'I'), ('Val1', 'Label2')])
+
+    def test_composite_relabelled_label2(self):
+        t = ((self.el1 * self.el2).relabel(label='Label1')
+             + (self.el1 * self.el2).relabel(value='Val1', label='Label2'))
+        self.assertEqual(t.keys(), [('Overlay', 'Label1'), ('Val1', 'Label2')])
+
+    def test_composite_relabelled_value2(self):
+        t = ((self.el1 * self.el2).relabel(value='Val1')
+             + (self.el1 * self.el2).relabel(value='Val2'))
+        self.assertEqual(t.keys(), [('Val1', 'I'), ('Val2', 'I')])
+
+    def test_composite_relabelled_value_and_label(self):
+        t = ((self.el1 * self.el2).relabel(value='Val1', label='Label1')
+             + (self.el1 * self.el2).relabel(value='Val2', label='Label2'))
+        self.assertEqual(t.keys(), [('Val1', 'Label1'), ('Val2', 'Label2')])
+
+
+    def test_triple_composite_relabelled_value_and_label_keys(self):
+        t = ((self.el1 * self.el2)
+             +(self.el1 * self.el2).relabel(value='Val1', label='Label1')
+             + (self.el1 * self.el2).relabel(value='Val2', label='Label2'))
+        excepted_keys = [('Overlay', 'I'), ('Val1', 'Label1'), ('Val2', 'Label2')]
+        self.assertEqual(t.keys(), excepted_keys)
+
+    def test_deep_composite_values(self):
+        o1 = (self.el1 * self.el2)
+        o2 = (self.el1 * self.el2)
+        o3 = (self.el7 * self.el8)
+        t = (o1 + o2 + o3)
+        self.assertEqual(t.values(), [o1, o2, o3])
+
+    def test_deep_composite_keys(self):
+        o1 = (self.el1 * self.el2)
+        o2 = (self.el1 * self.el2)
+        o3 = (self.el7 * self.el8)
+        t = (o1 + o2 + o3)
+        expected_keys = [('Overlay', 'I'), ('Overlay', 'II'), ('Overlay', 'III')]
+        self.assertEqual(t.keys(), expected_keys)
+
+
+    def test_deep_composite_indexing(self):
+        o1 = (self.el1 * self.el2)
+        o2 = (self.el1 * self.el2)
+        o3 = (self.el7 * self.el8)
+        t = (o1 + o2 + o3)
+        expected_keys = [('Overlay', 'I'), ('Overlay', 'II'), ('Overlay', 'III')]
+        self.assertEqual(t.keys(), expected_keys)
+        self.assertEqual(t.Overlay.III, o3)
+        self.assertEqual(t.Overlay.III.ValA.LabelA, self.el7)
+        self.assertEqual(t.Overlay.III.ValA.LabelB, self.el8)
+
+
+    def test_deep_composite_getitem(self):
+        o1 = (self.el1 * self.el2)
+        o2 = (self.el1 * self.el2)
+        o3 = (self.el7 * self.el8)
+        t = (o1 + o2 + o3)
+        expected_keys = [('Overlay', 'I'), ('Overlay', 'II'), ('Overlay', 'III')]
+        self.assertEqual(t.keys(), expected_keys)
+        self.assertEqual(t['Overlay']['III'], o3)
+        self.assertEqual(t['Overlay']['III']['ValA']['LabelA'], self.el7)
+        self.assertEqual(t['Overlay']['III']['ValA']['LabelB'], self.el8)
+
+
+    def test_invalid_tree_structure(self):
+        try:
+            t = (self.el1 + self.el2) * (self.el1 + self.el2)
+        except TypeError as e:
+            self.assertEqual(str(e), "unsupported operand type(s) for *: 'LayoutTree' and 'LayoutTree'")
+
+
+if __name__ == "__main__":
+    import sys
+    import nose
+    nose.runmodule(argv=[sys.argv[0], "--logging-level", "ERROR"])
