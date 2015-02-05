8e8ea751e3f8f40337457fde24a2e6b93d2eafb7
jlstevens <jlstevens@ed.ac.uk>
2015-02-05 01:25:00 +0000

    Initial commit of testdimensions.py unit tests

---
+++ tests/testdimensions.py
@@ -0,0 +1,22 @@
+"""
+Test cases for Dimension and Dimensioned object behaviour.
+"""
+
+import unittest
+from holoviews.core import Dimensioned
+from . import ComparisonTestCase
+
+
+class DimensionedTest(ComparisonTestCase):
+
+    def test_dimensioned_init(self):
+        Dimensioned('An example of arbitrary data')
+
+    def test_dimensioned_constant_label(self):
+        label = 'label'
+        view = Dimensioned('An example of arbitrary data', label=label)
+        self.assertEqual(view.label, label)
+        try:
+            view.label = 'another label'
+            raise AssertionError("Label should be a constant parameter.")
+        except TypeError: pass
