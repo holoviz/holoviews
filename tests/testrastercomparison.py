a257fb478188ce4260f52bf5928f9959984a6406
jlstevens <jlstevens@ed.ac.uk>
2015-02-05 00:29:31 +0000

    Fixed unit test in testrastercomparison

--- tests/testrastercomparison.py
+++ tests/testrastercomparison.py
@@ -140,7 +140,7 @@ class RasterMapComparisonTest(RasterMapTestCase):
              self.assertEqual(self.map1_1D, self.map6_1D)
              raise AssertionError("Mismatch in dimension labels not detected.")
          except AssertionError as e:
-             self.assertEqual(str(e), "Maps have different dimension labels.")
+             self.assertEqual(str(e), 'Dimension names mismatched.')
 
 
     def test_key_len_mismatch(self):
