e70ab0a62582897f3a328f2634e22c73e87f3e71
jlstevens <jlstevens@ed.ac.uk>
2015-02-19 00:08:26 +0000

    Moved Collator from interface to core/element.py

--- holoviews/interface/__init__.py
+++ holoviews/interface/__init__.py
@@ -16,7 +16,7 @@ from .collector import *       # pyflakes:ignore (API import)
 
 def public(obj):
     if not isinstance(obj, type): return False
-    baseclasses = [Dimensioned, Collector, Collator, AttrTree]
+    baseclasses = [Dimensioned, Collector, AttrTree]
     return any([issubclass(obj, bc) for bc in baseclasses])
 
 __all__ = list(set([_k for _k, _v in locals().items() if public(_v)]))
