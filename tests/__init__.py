2b52af2d3445d5c82f9d7c8a11eaadcc1f532335
jlstevens <jlstevens@ed.ac.uk>
2015-02-28 19:41:55 +0000

    Attempting to switch matplotlib backend to 'agg' for testing

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,8 @@
 import os, sys
 
+# Standardize backend due to random inconsistencies
+from matplotlib import pyplot
+pyplot.switch_backend('agg')
+
 cwd = os.path.abspath(os.path.split(__file__)[0])
 sys.path.insert(0, os.path.join(cwd, '..'))
