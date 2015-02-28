ab9b27a6b5a7104f2ee10fb1070552aa3ecb8daf
jlstevens <jlstevens@ed.ac.uk>
2015-02-28 20:19:23 +0000

    Added four more unit tests of Export.save functionality

--- tests/testexportclass.py
+++ tests/testexportclass.py
@@ -30,24 +30,44 @@ class ExportTest(ComparisonTestCase):
     """
 
     def setUp(self):
-        self.image1 = Image(np.array([[0,1],[2,3]]))
-        self.image2 = Image(np.array([[1,0],[4,-2]]))
-        self.map1 = HoloMap({1:self.image1, 2:self.image2})
+        self.image1 = Image(np.array([[0,1],[2,3]]), label='Image1')
+        self.image2 = Image(np.array([[1,0],[4,-2]]), label='Image2')
+        self.map1 = HoloMap({1:self.image1, 2:self.image2}, label='TestMap')
 
     def test_simple_export_png1(self):
         Export.save(self.image1, fmt='png')
         self.assertEqual(digest_data(Export.captured_data),
-                         'a2daf8b589508d3d305f66b2b0c3c606396f0e5feb3c7eff60880bc3f1157c59')
+                         '31a9af4a57492b79814a9922a573ca550358d3cce21933fc4cfa5de490037416')
 
     def test_simple_export_png2(self):
         Export.save(self.image2, fmt='png')
         self.assertEqual(digest_data(Export.captured_data),
-                        'e47fca35d069cdbf5ab0f3337c408ed7ed3e366baba7ec650abb8711394e8b1d')
+                         '9eed2badfc9ff203b6f34e22e3d97845dff588a502eb0e82befb30f88584028b')
 
     def test_simple_export_gif(self):
         Export.save(self.map1, fmt='gif')
         self.assertEqual(digest_data(Export.captured_data),
-                        '8b225c069ba56fb67774996e4a07dd8e3ac11f4df6566a2513d1cf489003159f')
+                         '44fee52c887a6a3a4116cf6d9a0a0f4451849e1aaef1d03a9a1a6c098681771b')
+
+    def test_simple_export_png1_double_size(self):
+        Export.save(self.image1, fmt='png', size=200)
+        self.assertEqual(digest_data(Export.captured_data),
+                         '577a2f8eb63cd5189a536895bd4f8f2d358b901ee808d2b2c976dffac14e27f4')
+
+    def test_simple_export_gif_double_size(self):
+        Export.save(self.map1, fmt='gif', size=200)
+        self.assertEqual(digest_data(Export.captured_data),
+                         'e73cb85ecf8876eef4bd22caf86685e97b7f6d990da16d93a36cc215a0c4072f')
+
+    def test_simple_export_gif_half_fps(self):
+        Export.save(self.map1, fmt='gif', fps=10)
+        self.assertEqual(digest_data(Export.captured_data),
+                         '1f329070e9d2c70f1fc6e4254ff3c346887079c2d09e951d1742e81f3d7815ba')
+
+    def test_simple_export_png1_double_size(self):
+        Export.save(self.image1, fmt='png', size=200)
+        self.assertEqual(digest_data(Export.captured_data),
+                         '577a2f8eb63cd5189a536895bd4f8f2d358b901ee808d2b2c976dffac14e27f4')
 
 
 
