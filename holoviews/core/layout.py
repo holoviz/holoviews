5d50a3ce65edc5a234cfb77199c1f32442cbc634
jlstevens <jlstevens@ed.ac.uk>
2015-03-10 02:27:02 +0000

    Fixed Layout.from_values when passed collection containing single value

--- holoviews/core/layout.py
+++ holoviews/core/layout.py
@@ -309,13 +309,15 @@ class Layout(AttrTree, Dimensioned):
         Returns a Layout given a list (or tuple) of viewable
         elements or just a single viewable element.
         """
-        if isinstance(val, (list, tuple)):
-            return cls._from_values(val)
-        elif type(val) is cls:
+        collection = isinstance(val, (list, tuple))
+        if type(val) is cls:
             return val
-        else:
-            return cls(items=[((sanitize_identifier(val.group),
-                                sanitize_identifier(val.label if val.label else 'I')), val)])
+        elif collection and len(val)>1:
+            return cls._from_values(val)
+        elif collection:
+            val = val[0]
+        return cls(items=[((sanitize_identifier(val.group),
+                            sanitize_identifier(val.label if val.label else 'I')), val)])
 
 
     def __init__(self, *args, **kwargs):
