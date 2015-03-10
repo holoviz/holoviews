3ae2fdb2d6fb373271f04a8b6a3438c2ac2fb5ba
philippjfr <P.Rudiger@ed.ac.uk>
2015-03-10 17:15:05 +0000

    Fixed division by zero errors in normalization

--- holoviews/operation/normalization.py
+++ holoviews/operation/normalization.py
@@ -161,10 +161,14 @@ class raster_normalization(Normalization):
             if depth_range and len(norm_raster.data.shape) == 2:
                 depth_range = ranges[name]
                 norm_raster.data[:,:] -= depth_range[0]
-                norm_raster.data[:,:] /= (depth_range[1] - depth_range[0])
+                range = (depth_range[1] - depth_range[0])
+                if range:
+                    norm_raster.data[:,:] /= range
             elif depth_range:
                 norm_raster.data[:,:,depth] -= depth_range[0]
-                norm_raster.data[:,:,depth] /= (depth_range[1] - depth_range[0])
+                range = (depth_range[1] - depth_range[0])
+                if range:
+                    norm_raster.data[:,:,depth] /= range
         return norm_raster
 
 
