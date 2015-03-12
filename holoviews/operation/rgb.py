1385c9a52d5b7a3ee0bda70998382d3e6de6d379
jlstevens <jlstevens@ed.ac.uk>
2015-03-12 02:08:25 +0000

    Removed redundant toRGB operation now factory can be used instead

--- holoviews/operation/rgb.py
+++ holoviews/operation/rgb.py
@@ -22,56 +22,6 @@ rgb_to_hsv = np.vectorize(colorsys.rgb_to_hsv)
 hsv_to_rgb = np.vectorize(colorsys.hsv_to_rgb)
 
 
-class toRGB(ElementOperation):
-    """
-    Accepts an overlay containing either 3 or 4 layers. The first
-    three layers are the R,G, B channels and the last layer (if
-    supplied) is the (optional) alpha channel.
-
-    Note that the values in the input Image elements are expected to
-    be bounded between 0.0-1.0. If this isn't the case, the values of
-    these channels will be clipped into this range.
-    """
-
-    output_type = RGB
-
-    group = param.String(default='RGB', doc="""
-        The group string for the output RGB element.""")
-
-
-    def _process(self, overlay, key=None):
-        if len(overlay) not in [3, 4]:
-            raise Exception("Requires 3 or 4 layers to convert to RGB(A)")
-        if not all(isinstance(el, Image) for el in overlay):
-            raise Exception("All layers must be of type Image to convert to RGB")
-        if not all(el.depth == 1 for el in overlay):
-            raise Exception("All Image elements must have single value"
-                            " dimension for conversion to RGB")
-        if not all(el.bounds == overlay[0].bounds for el in overlay):
-            raise Exception("All input Image elements must have the same bounds.")
-        if not all(el.data.shape == overlay[0].data.shape for el in overlay):
-            raise Exception("All input Image must have data with matching shape.")
-
-        normfn = raster_normalization.instance()
-        if self.p.input_ranges:
-            overlay = normfn.process_element(overlay, key, *self.p.input_ranges)
-        else:
-            overlay = normfn.process_element(overlay, key)
-
-        arrays = []
-        for el in overlay:
-            if el.data.max() > 1.0 or el.data.min() < 0:
-                self.warning("Clipping data into the interval [0, 1]")
-                data = el.data.clip(0,1.0)
-                arrays.append(data)
-            else:
-                arrays.append(el.data.copy())
-
-        return RGB(np.dstack(arrays),
-                   bounds = self.get_overlay_extents(overlay),
-                   label  = self.get_overlay_label(overlay),
-                   group  = self.p.group)
-
 
 
 class toHCS(ElementOperation):
