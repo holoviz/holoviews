c47c2efbf4561d790b65d8c5d652d2f621b9efcf
jlstevens <jlstevens@ed.ac.uk>
2015-03-16 03:08:01 +0000

    Made unit tests in TestWidgets Python 3 compatible

--- tests/testwidgets.py
+++ tests/testwidgets.py
@@ -17,7 +17,7 @@ from holoviews.plotting import RasterPlot
 
 def digest_data(data):
     hashfn = sha256()
-    hashfn.update(data)
+    hashfn.update(data.encode('utf-16'))
     return hashfn.hexdigest()
 
 
@@ -30,15 +30,16 @@ filters += [re.compile('new NDSlider\([a-z0-9_, "]+')]
 def normalize(data):
     for f in filters:
         data = re.sub(f, '[CLEARED]', data)
-    return data
+    # Hack around inconsistencies in jinja between Python 2 and 3
+    return data.replace('0.0', '0').replace('1.0', '1')
 
 class TestWidgets(IPTestCase):
 
     def setUp(self):
         super(TestWidgets, self).setUp()
         im1 = Image(np.array([[1,2],[3,4]]))
-        im2 = Image(np.array([[1,2],[3,4]]))
-        holomap = HoloMap({0:im1, 1:im2})
+        im2 = Image(np.array([[1,2],[3,5]]))
+        holomap = HoloMap(initial_items=[(0,im1), (1,im2)], key_dimensions=['test'])
         self.plot1 = RasterPlot(im1)
         self.plot2 = RasterPlot(holomap)
 
@@ -47,19 +48,19 @@ class TestWidgets(IPTestCase):
 
     def test_scrubber_widget_1(self):
         html = normalize(ScrubberWidget(self.plot1)())
-        self.assertEqual(digest_data(html), 'af30bfad70f430017c168de292b1886672260250d3b6cd4bbae05c9087b60773')
+        self.assertEqual(digest_data(html), '300e45d633af83f1354d8208e37d7a1c4ca995d00c87c06e7f0250cd53411d1e')
 
     def test_selection_widget_1(self):
         html = normalize(SelectionWidget(self.plot1)())
-        self.assertEqual(digest_data(html), '64dae1444ab71ca3d2949b78388866ad4eb235460138afc6d17e4d6248bd2619')
+        self.assertEqual(digest_data(html), '46b4b1c50ae4530341b25a2f7761263413dec12ccde3e30b0210a07f841d5aaa')
 
     def test_scrubber_widget_2(self):
         html = normalize(ScrubberWidget(self.plot2)())
-        self.assertEqual(digest_data(html), 'f0e5558a6906c38f2e514890a935c6a1e760069785741a096366af7328a35455')
+        self.assertEqual(digest_data(html), '6fc2a1f43409e82da7d776aa30575ff9ed52e3a1925340df3a509bc8558788f6')
 
     def test_selection_widget_2(self):
         html = normalize(SelectionWidget(self.plot2)())
-        self.assertEqual(digest_data(html), 'c42ea1e290783ccdd4493526cabfd9a2a361aa9d9fb8e664ed00980e68ecd333')
+        self.assertEqual(digest_data(html), '41b4c724661eac26254c3202348d71281e1b0275421d2134e46e2ac594eeb620')
 
 
 if __name__ == "__main__":
