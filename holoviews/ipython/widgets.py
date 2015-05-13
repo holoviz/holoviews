6ccb8aa8a1d2922d105441f34ea26b54fbf8d548
philippjfr <P.Rudiger@ed.ac.uk>
2015-05-13 23:27:14 +0100

    Remove backslash from widget labels
    Fixes issue #71

--- holoviews/ipython/widgets.py
+++ holoviews/ipython/widgets.py
@@ -439,7 +439,7 @@ class SelectionWidget(NdWidget):
             else:
                 widget_type = 'dropdown'
             init_dim_vals.append(dim_vals[0])
-            dim_str = dim.name.replace(' ', '_').replace('$', '')
+            dim_str = dim.name.replace(' ', '_').replace('$', '').replace('\\', '')
             visibility = 'visibility: visible' if len(dim_vals) > 1 else 'visibility: hidden; height: 0;'
             widgets.append(dict(dim=dim_str, dim_idx=idx, vals=repr(dim_vals),
                                 type=widget_type, visibility=visibility))
