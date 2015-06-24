13149dfaa4a772266a835e9e714a4494ac05a202
philippjfr <P.Rudiger@ed.ac.uk>
2015-06-24 23:16:14 +0100

    Added Histogram constructor unit tests

--- tests/testelementconstructors.py
+++ tests/testelementconstructors.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from holoviews import Curve, Path
+from holoviews import Curve, Path, Histogram
 from holoviews.element.comparison import ComparisonTestCase
 from holoviews.interface.collector import ViewRef
 
@@ -13,12 +13,14 @@ class ElementConstructorTest(ComparisonTestCase):
 
     def setUp(self):
         self.xs = np.linspace(0, 2*np.pi, 11)
+        self.hxs = np.arange(len(self.xs))
         self.sin = np.sin(self.xs)
         self.cos = np.cos(self.xs)
         sine_data = np.column_stack((self.xs, self.sin))
         cos_data = np.column_stack((self.xs, self.cos))
         self.curve = Curve(sine_data)
         self.path = Path([sine_data, cos_data])
+        self.histogram = Histogram(self.sin, self.hxs)
         super(ElementConstructorTest, self).setUp()
 
     def test_chart_zipconstruct(self):
@@ -35,3 +37,12 @@ class ElementConstructorTest(ComparisonTestCase):
 
     def test_path_ziplist_construct(self):
         self.assertEqual(Path([list(zip(self.xs, self.sin)), list(zip(self.xs, self.cos))]), self.path)
+
+    def test_chart_zip_construct(self):
+        self.assertEqual(Histogram(list(zip(self.hxs, self.sin))), self.histogram)
+
+    def test_chart_array_construct(self):
+        self.assertEqual(Histogram(np.column_stack((self.hxs, self.sin))), self.histogram)
+
+    def test_chart_yvalues_construct(self):
+        self.assertEqual(Histogram(self.sin), self.histogram)
