5fb5d875938e5f46d28ec6d60ef3516c6f897ff9
philippjfr <P.Rudiger@ed.ac.uk>
2015-06-29 12:43:33 +0100

    Made bokeh ElementPlot axes more customizable

--- holoviews/plotting/bokeh/element.py
+++ holoviews/plotting/bokeh/element.py
@@ -7,7 +7,7 @@ from bokeh.models import ColumnDataSource
 from bokeh.models import GlyphRenderer
 from bokeh import mpl
 
-from ...core import OrderedDict, Dimension, Store
+from ...core import OrderedDict, Dimension, Store, HoloMap
 from ...core.util import match_spec
 from ...element import Chart, Image, HeatMap, RGB, Raster
 from ..plot import GenericElementPlot, GenericOverlayPlot
@@ -41,6 +41,18 @@ class ElementPlot(GenericElementPlot, BokehPlot):
     tools = param.String(default="pan,wheel_zoom,box_zoom,reset,resize")
 
     select = param.Boolean(default=True)
+
+    xaxis = param.ObjectSelector(default='bottom',
+                                 objects=['top', 'bottom', 'bare', 'top-bare',
+                                          'bottom-bare', None], doc="""
+        Whether and where to display the xaxis, bare options allow suppressing
+        all axis labels including ticks and xlabel.""")
+
+    yaxis = param.ObjectSelector(default='left',
+                                      objects=['left', 'right', 'bare', 'left-bare',
+                                               'right-bare', None], doc="""
+        Whether and where to display the yaxis, bare options allow suppressing
+        all axis labels including ticks and ylabel.""")
     
     def __init__(self, element, plot=None, subplot=False, **params):
         self.subplot = subplot
@@ -56,13 +68,13 @@ class ElementPlot(GenericElementPlot, BokehPlot):
         subplots = list(self.subplots.values()) if self.subplots else []
 
         plot_kwargs = {}
-        title = self._format_title(key)
+        title = self._format_title(key) if self.show_title else None
         xlabel, ylabel, zlabel = self._axis_labels(view, subplots, xlabel, ylabel, zlabel)
-
 
         # Try finding shared ranges in other plots in the same Layout
         if plots:
             for plot in plots:
+                if plot is None or not hasattr(plot, 'xaxis'): continue
                 if plot.xaxis[0].axis_label == xlabel:
                     plot_kwargs['x_range'] = plot.x_range
                 if plot.xaxis[0].axis_label == ylabel:
@@ -88,14 +100,14 @@ class ElementPlot(GenericElementPlot, BokehPlot):
                 if all(y is not None for y in (b, t)):
                     plot_kwargs['y_range'] = [b, t]
 
-        plot = figure(x_axis_type=x_axis_type, x_axis_label=xlabel,
+        plot = figure(x_axis_type=x_axis_type, x_axis_label=xlabel, min_border=2,
                       y_axis_type=y_axis_type, y_axis_label=ylabel, tools=self.tools,
                       title=title, width=self.width, height=self.height, **plot_kwargs)
         return plot
 
 
     def _update_plot(self, key, plot):
-        plot.title = self._format_title(key)
+        plot.title = self._format_title(key) if self.show_title else None
         plot.background_fill = self.bgcolor
         if self.title_color:
             plot.title_text_color = self.title_color
@@ -103,6 +115,18 @@ class ElementPlot(GenericElementPlot, BokehPlot):
             plot.title_text_font = self.title_font
         if self.title_size:
             plot.title_text_font_size = self.title_size
+        if self.xaxis in ['bottom-bare' or None]:
+            xaxis = plot.xaxis[0]
+            xaxis.axis_label = ''
+            xaxis.major_label_text_font_size = '0pt'
+            xaxis.major_tick_line_color = None
+            xaxis.ticker.num_minor_ticks = 0
+        if self.yaxis in ['left-bare' or None]:
+            yaxis = plot.yaxis[0]
+            yaxis.axis_label = ''
+            yaxis.major_label_text_font_size = '0pt'
+            yaxis.major_tick_line_color = None
+            yaxis.ticker.num_minor_ticks = 0
 
 
     def _init_datasource(self, element):
@@ -122,34 +146,25 @@ class ElementPlot(GenericElementPlot, BokehPlot):
         ranges = match_spec(element, ranges)
         if plot is None:
             plot = self._init_plot(key, ranges=ranges, plots=plots)
-        source = self._init_datasource(element)
+        if source is None:
+            source = self._init_datasource(element)
         style = self.style[self.cyclic_index]
         self.handles['plot'] = plot
         self.handles['source'] = source
         self.init_glyph(element, plot, source, style)
-        
-        return plot
+        self._update_plot(key, plot)
 
-        
-    def update(self, key):
-        """
-        Update the internal state of the Plot to represent the given
-        key tuple (where integers represent frames). Returns this
-        state.
-        """
-        if not self.drawn:
-            plot = self.initialize_plot()
-        self.update_frame(key, plot=plot)
-        return self.state
+        return plot
 
 
     def update_frame(self, key, ranges=None, plot=None):
         if plot is None:
             plot = self.handles['plot']
         element = self._get_frame(key)
-        source = self.handles['source']
-        self._update_datasource(source, element)
-        self._update_plot(key, plot)
+        if element:
+            source = self.handles['source']
+            self._update_datasource(source, element)
+            self._update_plot(key, plot)
 
 
 class BokehMPLWrapper(ElementPlot):
