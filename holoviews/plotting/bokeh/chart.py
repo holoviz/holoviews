73058c65ac36e24801db69c3edccdf139c758e72
philippjfr <P.Rudiger@ed.ac.uk>
2015-08-20 15:00:01 +0100

    Bokeh PointPlot now supports mapping values to sizes and colors

--- holoviews/plotting/bokeh/chart.py
+++ holoviews/plotting/bokeh/chart.py
@@ -5,19 +5,70 @@ import param
 from ...core import Dimension
 from ...core.util import max_range
 from ...element import Chart
+from ..util import compute_sizes
 from .element import ElementPlot, line_properties, fill_properties
 from .path import PathPlot
+from .util import map_colors, get_cmap
 
 
 class PointPlot(ElementPlot):
 
-    style_opts = ['marker', 'color'] + line_properties + fill_properties
+    color_index = param.Integer(default=3, doc="""
+      Index of the dimension from which the color will the drawn""")
+
+    size_index = param.Integer(default=2, doc="""
+      Index of the dimension from which the sizes will the drawn.""")
+
+    radius_index = param.Integer(default=None, doc="""
+      Index of the dimension from which the sizes will the drawn.""")
+
+    scaling_factor = param.Number(default=1, bounds=(1, None), doc="""
+      If values are supplied the area of the points is computed relative
+      to the marker size. It is then multiplied by scaling_factor to the power
+      of the ratio between the smallest point and all other points.
+      For values of 1 scaling by the values is disabled, a factor of 2
+      allows for linear scaling of the area and a factor of 4 linear
+      scaling of the point width.""")
+
+    size_fn = param.Callable(default=np.abs, doc="""
+      Function applied to size values before applying scaling,
+      to remove values lower than zero.""")
+
+    style_opts = ['cmap', 'palette', 'marker', 'size', 'alpha', 'color'] + line_properties + fill_properties
 
     def get_data(self, element, ranges=None):
-        return dict(x=element.data[:, 0], y=element.data[:, 1])
-    
+        dims = element.dimensions(label=True)
+        data = {}
+        cmap = self.style.get('palette', self.style.get('cmap', None))
+        if self.color_index < len(dims) and cmap:
+            cmap = get_cmap(cmap)
+            val_dim = dims[self.color_index]
+            clims = ranges.get(val_dim)
+            colors = element.data[:, self.color_index]
+            data['color'] = map_colors(colors, ranges, cmap)
+        if self.size_index < len(dims):
+            val_dim = dims[self.size_index]
+            clims = ranges.get(val_dim)
+            ms = self.style.get('size', 1)
+            sizes = element.data[:, self.size_index]
+            data['size'] = compute_sizes(sizes, self.size_fn,
+                                         self.scaling_factor,
+                                         ms, ranges[val_dim])
+
+        return dict(x=element.data[:, 0], y=element.data[:, 1], **data)
+
+
     def init_glyph(self, element, plot, source, ranges):
-        plot.scatter(x='x', y='y', source=source, legend=element.label, **self.style)
+        style = dict(self.style)
+        if 'color' in source.data:
+            style['fill_color'] = 'color'
+            style.pop('cmap', None)
+        if 'size' in source.data:
+            style.pop('size', None)
+            style['size'] = 'size'
+
+        plot.scatter(x='x', y='y', source=source, legend=element.label, **style)
+
 
 
 class CurvePlot(ElementPlot):
