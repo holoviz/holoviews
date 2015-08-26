8914d0858ae6e2c9004a41767cd1cba45b691a90
philippjfr <P.Rudiger@ed.ac.uk>
2015-08-26 02:21:36 +0100

    Updated widget test SHAs

--- tests/testwidgets.py
+++ tests/testwidgets.py
@@ -51,16 +51,16 @@ class TestWidgets(IPTestCase):
 
     def test_scrubber_widget_1(self):
         html = normalize(ScrubberWidget(self.plot1, display_options={'figure_format': 'png'})())
-        self.assertEqual(digest_data(html), '999137bba4702862425da5f89d564d70c0ac0b68fd73923f1a51042014ca5796')
+        self.assertEqual(digest_data(html), 'f5209407343a0b31821300f4bce5061c678bffbb38ddaace8196e58108d4a287')
 
     def test_scrubber_widget_2(self):
         html = normalize(ScrubberWidget(self.plot2, display_options={'figure_format': 'png'})())
-        self.assertEqual(digest_data(html), '2d0f8440e675ba6a2be31560472858e824aa82715cca9aa440a54e0aa1cb2e42')
+        self.assertEqual(digest_data(html), '22732920d243512c5f92c37acbd9767fcdbc3c73a911312b8d9e0a2759bc5dfb')
 
     def test_selection_widget_1(self):
         html = normalize(SelectionWidget(self.plot1, display_options={'figure_format': 'png'})())
-        self.assertEqual(digest_data(html), '2efacd2441e095b6da5fb9ec3fbd40f446478e0c78e3a0631075c450913e3e07')
+        self.assertEqual(digest_data(html), '6e4021fdd99a2dabb11917df797f5237515c54ed1eaf8895ca619dd3e8b6e439')
 
     def test_selection_widget_2(self):
         html = normalize(SelectionWidget(self.plot2, display_options={'figure_format': 'png'})())
-        self.assertEqual(digest_data(html), '5dde43d2f570151341d1174a25752b3eb88e7ad287b6db96c6804bfadf821c8f')
+        self.assertEqual(digest_data(html), 'e57531d56ee91ce8e93810929d33e354f8e5e94ab5aa4f8b1f9a6c346d20dc2e')
