d9ab3405fc90745518ad29221af5fd3eff61f3de
jlstevens <jlstevens@ed.ac.uk>
2015-09-02 00:13:47 +0100

    Added the toarray utility to support dask Arrays

--- holoviews/element/util.py
+++ holoviews/element/util.py
@@ -1,5 +1,20 @@
 import numpy as np
 
+try:
+    import dask
+except:
+    dask = None
+
+def toarray(v):
+    """
+    Interface helper function to turn dask Arrays into numpy arrays as
+    necessary.
+    """
+    if dask and isinstance(v, dask.array.Array):
+        return v.compute()
+    else:
+        return v
+
 def compute_edges(edges):
     """
     Computes edges from a number of bin centers,
