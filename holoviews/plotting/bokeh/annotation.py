7f886abfd7b76a26d885d9aaeaffa4bbfcf8b69d
philippjfr <P.Rudiger@ed.ac.uk>
2015-09-18 01:58:47 +0100

    Fixed bug in bokeh SplinePlot

--- holoviews/plotting/bokeh/annotation.py
+++ holoviews/plotting/bokeh/annotation.py
@@ -45,7 +45,10 @@ class LineAnnotationPlot(ElementPlot):
 
 
 class SplinePlot(ElementPlot):
-    "Draw the supplied Spline annotation (see Spline docstring)"
+    """
+    Draw the supplied Spline annotation (see Spline docstring).
+    Does not support matplotlib Path codes.
+    """
 
     style_opts = line_properties
     _plot_method = 'bezier'
@@ -54,7 +57,7 @@ class SplinePlot(ElementPlot):
         plot = self.handles['plot']
         verts = np.array(element.data[0])
         xs, ys = verts[:, 0], verts[:, 1]
-        return (dict(x0=[xs[0]], y0=[ys[0]], x1=[xs[-1]], y1=[xs[-1]],
+        return (dict(x0=[xs[0]], y0=[ys[0]], x1=[xs[-1]], y1=[ys[-1]],
                      cx0=[xs[1]], cy0=[ys[1]], cx1=[xs[2]], cy1=[ys[2]]),
                 dict(x0='x0', y0='y0', x1='x1', y1='y1',
                      cx0='cx0', cx1='cx1', cy0='cy0', cy1='cy1'))
