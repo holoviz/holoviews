b6d8b4773f76f55ed7bda5f7e78ba33181b00929
philippjfr <P.Rudiger@ed.ac.uk>
2015-09-20 23:01:30 +0100

    Disabled histogram mean_weighted option by default

--- holoviews/operation/element.py
+++ holoviews/operation/element.py
@@ -458,7 +458,7 @@ class histogram(ElementOperation):
     normed = param.Boolean(default=True, doc="""
       Whether the histogram frequencies are normalized.""")
 
-    mean_weighted = param.Boolean(default=True, doc="""
+    mean_weighted = param.Boolean(default=False, doc="""
       Whether the weighted frequencies are averaged.""")
 
     individually = param.Boolean(default=True, doc="""
@@ -487,11 +487,11 @@ class histogram(ElementOperation):
         if hist_range == (0, 0):
             hist_range = (0, 1)
         data = data[np.invert(np.isnan(data))]
-        normed = False if self.p.mean_weighted else self.p.normed
+        normed = False if self.p.mean_weighted and self.p.weight_dimension else self.p.normed
         try:
             hist, edges = np.histogram(data[np.isfinite(data)], normed=normed,
                                        range=hist_range, weights=weights, bins=self.p.num_bins)
-            if self.p.weight_dimension and self.p.mean_weighted:
+            if not normed and self.p.weight_dimension and self.p.mean_weighted:
                 hist_mean, _ = np.histogram(data[np.isfinite(data)], normed=normed,
                                             range=hist_range, bins=self.p.num_bins)
                 hist /= hist_mean
