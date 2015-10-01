8b005b96aa37d026b9a57a9fd30d0fc5a1c48cda
philippjfr <P.Rudiger@ed.ac.uk>
2015-10-01 04:27:34 +0100

    Handle titles appropriately in open mode

--- holoviews/plotting/plot.py
+++ holoviews/plotting/plot.py
@@ -228,9 +228,11 @@ class DimensionedPlot(Plot):
         Returns the formatted dimension group strings
         for a particular frame.
         """
+        if self.dynamic == 'open' and self.current_key:
+            key = self.current_key
         if self.layout_dimensions is not None:
             dimensions, key = zip(*self.layout_dimensions.items())
-        elif not self.uniform or len(self) == 1 or self.subplot:
+        elif not self.dynamic and (not self.uniform or len(self) == 1 or self.subplot):
             return ''
         else:
             key = key if isinstance(key, tuple) else (key,)
