Fixed minor bugs in layout calculation

holoviews/plotting/mpl/plot.py
@@ -812,8 +812,12 @@ class LayoutPlot(GenericLayoutPlot, CompositePlot):
         # Compute the number of rows and cols
         cols, rows = len(wr_list), len(hr_list)
 
-        width = sum(wr_list)
-        yscale = width/sum([(1/v)*4 for v in wr_unnormalized])
+
+        wr_list = [r if np.isfinite(r) else 1 for r in wr_list]
+        hr_list = [r if np.isfinite(r) else 1 for r in hr_list]
+
+        width = sum([r if np.isfinite(r) else 1 for r in wr_list])
+        yscale = width/sum([(1/v)*4 if np.isfinite(v) else 4 for v in wr_unnormalized])
         if self.absolute_scaling:
             width = width*np.nanmax(widths)
 

holoviews/plotting/mpl/util.py
@@ -39,7 +39,7 @@ def normalize_ratios(ratios):
     normalized = {}
     for i, v in enumerate(zip(*ratios.values())):
         arr = np.array(v)
-        normalized[i] = arr/np.nanmax(v)
+        normalized[i] = arr/float(np.nanmax(arr))
     return normalized
 
 def compute_ratios(ratios, normalized=True):