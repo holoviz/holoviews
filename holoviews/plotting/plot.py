37f3dbf79f92b103ab35f0c6f8273d89984470e0
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2016-02-09 20:50:59 +0000

    Fixed bug in GenericElementPlot._get_frame

--- holoviews/plotting/plot.py
+++ holoviews/plotting/plot.py
@@ -460,9 +460,9 @@ class GenericElementPlot(DimensionedPlot):
             self.current_key = key
             return self.current_frame
         elif self.dynamic:
-            dims = {d.name: k for d, k in zip(self.dimensions, key)
-                    if d in self.hmap.kdims}
             if isinstance(key, tuple):
+                dims = {d.name: k for d, k in zip(self.dimensions, key)
+                        if d in self.hmap.kdims}
                 frame = self.hmap.select(**dims)
             elif key < self.hmap.counter:
                 key = self.hmap.keys()[key]
