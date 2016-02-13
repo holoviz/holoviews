9a53751e5e5cd39cb7ddf0d904c9b8dd2f527b0b
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2016-02-13 16:24:03 +0000

    Refactored seaborn plots

--- holoviews/plotting/mpl/seaborn.py
+++ holoviews/plotting/mpl/seaborn.py
@@ -39,11 +39,9 @@ class FullRedrawPlot(ElementPlot):
 
     _abstract = True
 
-    def update_handles(self, axis, view, key, ranges, style):
+    def teardown_handles(self):
         if self.zorder == 0 and axis:
             axis.cla()
-        self._update_plot(axis, view)
-
 
 
 class RegressionPlot(FullRedrawPlot):
@@ -60,19 +58,12 @@ class RegressionPlot(FullRedrawPlot):
                   'scatter_kws', 'line_kws', 'ci', 'dropna',
                   'x_jitter', 'y_jitter', 'x_partial', 'y_partial']
 
-    def initialize_plot(self, ranges=None):
-        self._update_plot(self.handles['axis'], self.hmap.last)
-        return self._finalize_axis(self.keys[-1])
+    def init_artist(self, ax, element, plot_data, plot_kwargs):
+        return {'axis': sns.regplot(*plot_data, ax=ax, **plot_kwargs)}
 
-
-    def _update_plot(self, axis, view):
-        kwargs = self.style[self.cyclic_index]
-        label = view.label if self.overlaid >= 1 else ''
-        if label:
-            kwargs['label'] = label
-        sns.regplot(view.dimension_values(0), view.dimension_values(1),
-                    ax=axis, **kwargs)
-
+    def get_data(self, element, ranges, style):
+        xs, ys = (element[d] for d in self.dimensions()[:1])
+        return (xs, ys), style, {}
 
 
 class BivariatePlot(FullRedrawPlot):
@@ -93,29 +84,19 @@ class BivariatePlot(FullRedrawPlot):
                   'ci', 'kind', 'bw', 'kernel', 'cumulative',
                   'shade', 'vertical', 'cmap']
 
-    def initialize_plot(self, ranges=None):
-        kdeview = self.hmap.last
-        axis = self.handles['axis']
-        if self.joint and self.subplot:
-            raise Exception("Joint plots can't be animated or laid out in a grid.")
-        self._update_plot(axis, kdeview)
-
-        return self._finalize_axis(self.keys[-1])
-
-
-    def _update_plot(self, axis, view):
-        kwargs = self.style[self.cyclic_index]
+    def init_artist(self, ax, element, plot_data, plot_kwargs):
         if self.joint:
-            kwargs.pop('cmap', None)
-            self.handles['fig'] = sns.jointplot(view.data[:,0],
-                                                view.data[:,1],
-                                                **kwargs).fig
+            if self.joint and self.subplot:
+                raise Exception("Joint plots can't be animated or laid out in a grid.")
+            return {'fig': sns.jointplot(*plot_data, **plot_kwargs).fig}
         else:
-            kwargs = self.style[self.cyclic_index]
-            label = view.label if self.overlaid >= 1 else ''
-            if label:
-                kwargs['label'] = label
-            sns.kdeplot(view.data, ax=axis, zorder=self.zorder, **kwargs)
+            return {'axis': sns.kdeplot(*plot_data, ax=ax, **plot_kwargs)}
+
+    def get_data(self, element, ranges, style):
+        xs, ys = (element[d] for d in self.dimensions()[:1])
+        if self.joint:
+            style.pop('cmap', None)
+        return (xs, ys), style, {}
 
 
 
@@ -137,24 +118,16 @@ class TimeSeriesPlot(FullRedrawPlot):
                   'ci', 'n_boot', 'err_kws', 'err_palette',
                   'estimator', 'kwargs']
 
-    def initialize_plot(self, ranges=None):
-        element = self.hmap.last
-        axis = self.handles['axis']
-        self._update_plot(axis, element)
+    def get_data(self, element, ranges, style):
+        if label in style:
+            style['condition'] = style.pop('label')
+        axis_kwargs = {'xlabel': str(view.kdims[0]),
+                       'ylabel': str(view.vdims[0])}
+        return (element.data, element.xdata), style, axis_kwargs
 
-        return self._finalize_axis(self.keys[-1])
+    def init_artist(self, ax, element, plot_data, plot_kwargs):
+        return {'axis': sns.tsplot(*plot_data, ax=ax, **plot_kwargs)}
 
-    def _update_plot(self, axis, view):
-        kwargs = self.style[self.cyclic_index]
-        label = view.label if self.overlaid >= 1 else ''
-        if label:
-            kwargs['condition'] = label
-        sns.tsplot(view.data, view.xdata, ax=axis, zorder=self.zorder, **kwargs)
-
-    def _axis_labels(self, view, subplots, xlabel=None, ylabel=None, zlabel=None):
-        xlabel = xlabel if xlabel else str(view.kdims[0])
-        ylabel = ylabel if ylabel else str(view.vdims[0])
-        return xlabel, ylabel, zlabel
 
 
 class DistributionPlot(FullRedrawPlot):
@@ -173,23 +146,15 @@ class DistributionPlot(FullRedrawPlot):
     style_opts = ['bins', 'hist', 'kde', 'rug', 'fit', 'hist_kws',
                   'kde_kws', 'rug_kws', 'fit_kws', 'color']
 
-    def initialize_plot(self, ranges=None):
-        element = self.hmap.last
-        axis = self.handles['axis']
-        self._update_plot(axis, element)
-        dim = element.get_dimension(0)
-
-        return self._finalize_axis(self.keys[-1], xlabel='', ylabel=str(dim))
-
-
-    def _update_plot(self, axis, view):
-        kwargs = self.style[self.cyclic_index]
-        label = view.label if self.overlaid >= 1 else ''
-        if label:
-            kwargs['label'] = label
+    def get_data(self, element, ranges, style):
         if self.invert_axes:
-            kwargs['vertical'] = True
-        sns.distplot(view.dimension_values(0), ax=axis, **kwargs)
+            style['vertical'] = True
+        axis_kwargs = dict(xlabel='', ylabel=str(dim))
+        return (element.dimension_values(0),), style, axis_kwargs
+
+    def init_artist(self, ax, element, plot_data, plot_kwargs):
+        return {'axis': sns.distplot(*plot_data, ax=ax, **plot_kwargs)}
+
 
 
 class SideDistributionPlot(AdjoinedPlot, DistributionPlot):
