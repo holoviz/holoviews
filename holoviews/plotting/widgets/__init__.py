holoviz/holoviews

commit e3a0658e737e3bdd9eae3d3b2cad5e8a0ea3414a

Use specified formatters for slider and dropdown text

holoviews/plotting/widgets/__init__.py
@@ -296,6 +296,9 @@ class SelectionWidget(NdWidget):
                 else:
                     next_vals = {}
 
+                value_labels = escape_list(escape_vals([dim.pprint_value(v)
+                                                        for v in dim_vals]))
+
                 if isnumeric(dim_vals[0]):
                     dim_vals = [round(v, 10) for v in dim_vals]
                     if next_vals:
@@ -317,7 +320,7 @@ class SelectionWidget(NdWidget):
             widget_data = dict(dim=dimension_sanitizer(dim_str), dim_label=dim_str,
                                dim_idx=idx, vals=dim_vals, type=widget_type,
                                visibility=visibility, step=step, next_dim=next_dim,
-                               next_vals=next_vals)
+                               next_vals=next_vals, labels=value_labels)
 
             widgets.append(widget_data)
             dimensions.append(dim_str)

holoviews/plotting/widgets/jsslider.jinja
@@ -71,6 +71,7 @@
                         require(["jQueryUI", "underscore"], function(jUI, _){
                             if (noConflict) $.noConflict(true);
                             var vals = {{ widget_data['vals'] }};
+                            var labels = {{ widget_data['labels'] }};
                             var next_vals = {{ widget_data['next_vals'] }};
                             if ({{ dynamic }} && vals.constructor === Array) {
                                 var min = parseFloat(vals[0]);
@@ -93,16 +94,18 @@
                                 step: step,
                                 value: min,
                                 dim_vals: vals,
+                                dim_labels: labels,
                                 next_vals: next_vals,
                                 slide: _.throttle(function(event, ui) {
                                     var vals = slider.slider("option", "dim_vals");
                                     var next_vals = slider.slider("option", "next_vals");
+                                    var labels = slider.slider("option", "dim_labels");
                                     if ({{ dynamic }} && vals.constructor === Array) {
                                         var dim_val = ui.value;
                                     } else {
                                         var dim_val = vals[ui.value];
                                     }
-									$('#textInput{{ id }}_{{ widget_data['dim'] }}').val((dim_val*1).toString());
+									$('#textInput{{ id }}_{{ widget_data['dim'] }}').val(labels[ui.value]);
                                     anim{{ id }}.set_frame(dim_val, {{ widget_data['dim_idx'] }});
                                     if (Object.keys(next_vals).length > 0) {
                                         var new_vals = next_vals[dim_val];
@@ -141,7 +144,7 @@
                                     }
                                 }
                             });
-                            $('#textInput{{ id }}_{{ widget_data['dim'] }}').val((vals[0]*1).toString());
+                            $('#textInput{{ id }}_{{ widget_data['dim'] }}').val(labels[0]);
                         });
 						// Slider JS Block END
                     </script>
@@ -153,16 +156,18 @@
                     </div>
                     <script>
                         var vals = {{ widget_data['vals'] }};
+                        var labels = {{ widget_data['labels'] }};
                         var widget = $("#_anim_widget{{ id }}_{{ widget_data['dim'] }}");
+                        widget.data('values', vals)
                         for (var i=0; i<vals.length; i++){
                             widget.append($("<option>", {
                                 value: i,
-                                text: vals[i]
+                                text: labels[i]
                             }));
                         };
                         widget.data("next_vals", {{ widget_data['next_vals'] }});
                         widget.on('change', function(event, ui) {
-                            var dim_val = this.options[this.value].text;
+                            var dim_val = $.data(this, 'values')[this.value];
                             if(anim{{ id }}) {
                                 anim{{ id }}.set_frame(dim_val, {{ widget_data['dim_idx'] }});
                             }

commit 994ed018bca090e8a473bcd9f8792f4c55bf7159

Fixed DynamicMap error introduced by e3a0658e

holoviews/plotting/widgets/__init__.py
@@ -272,10 +272,17 @@ class SelectionWidget(NdWidget):
                         dim_vals = escape_list(escape_vals(dim.values))
                         widget_type = 'dropdown'
                     init_dim_vals.append(dim_vals[0])
+
+                    value_labels = escape_list(escape_vals([dim.pprint_value(v)
+                                                        for v in dim.values]))
                 else:
                     widget_type = 'slider'
                     dim_vals = [dim.soft_range[0] if dim.soft_range[0] else dim.range[0],
                                 dim.soft_range[1] if dim.soft_range[1] else dim.range[1]]
+
+                    value_labels = escape_list(escape_vals([dim.pprint_value(v)
+                                                        for v in dim_vals]))
+
                     dim_range = dim_vals[1] - dim_vals[0]
                     int_type = isinstance(dim.type, type) and issubclass(dim.type, int)
                     if isinstance(dim_range, int) or int_type: