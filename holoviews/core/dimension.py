6ec6204cd0b80bd040cdd4c79ba7880190ef149a
jlstevens <jlstevens@ed.ac.uk>
2016-04-28 12:48:00 +0100

    Fixed incorrect indentation level

--- holoviews/core/dimension.py
+++ holoviews/core/dimension.py
@@ -101,8 +101,8 @@ class Dimension(param.Parameterized):
         if isinstance(name, Dimension):
             existing_params = dict(name.get_param_values())
         elif (name, params.get('unit', None)) in self.presets.keys():
-                preset = self.presets[(str(name), str(params['unit']))]
-                existing_params = dict(preset.get_param_values())
+            preset = self.presets[(str(name), str(params['unit']))]
+            existing_params = dict(preset.get_param_values())
         elif name in self.presets.keys():
             existing_params = dict(self.presets[str(name)].get_param_values())
         elif (name,) in self.presets.keys():
