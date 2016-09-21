6a6bfaa240486dd4086508e5ce771ab625d720c9
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2016-09-21 12:23:45 +0100

    Registered Plotly plots

--- holoviews/plotting/plotly/__init__.py
+++ holoviews/plotting/plotly/__init__.py
@@ -9,23 +9,51 @@ from .chart import *
 from .chart3d import *
 from .raster import *
 from .plot import *
+from .tabular import *
 
 Store.renderers['plotly'] = PlotlyRenderer.instance()
 
 Store.register({Points: PointPlot,
                 Scatter: PointPlot,
                 Curve: CurvePlot,
-                Raster: RasterPlot,
-                Image: RasterPlot,
-                Surface: SurfacePlot,
+                ErrorBars: ErrorBarsPlot,
                 Bivariate: BivariatePlot,
                 Distribution: DistributionPlot,
+                Bars: BarPlot,
+                BoxWhisker: BoxWhiskerPlot,
+
+                # Raster plots
+                Raster: RasterPlot,
+                Image: RasterPlot,
                 HeatMap: HeatMapPlot,
-                VectorField: VectorPlot,
-                ErrorBars: ErrorBarsPlot,
+
+                # 3D Plot
                 Scatter3D: Scatter3dPlot,
+                Surface: SurfacePlot,
                 Trisurface: TrisurfacePlot,
+
+                # Tabular
+                Table: TablePlot,
+                ItemTable: TablePlot,
+
+                # Container Plots
                 Overlay: OverlayPlot,
                 NdOverlay: OverlayPlot,
                 Layout: LayoutPlot,
-                NdLayout: LayoutPlot}, backend='plotly')
+                NdLayout: LayoutPlot,
+                GridSpace: GridPlot,
+                GridMatrix: GridPlot}, backend='plotly')
+
+
+options = Store.options(backend='plotly')
+
+
+point_size = np.sqrt(6) # Matches matplotlib default
+Cycle.default_cycles['default_colors'] =  ['#30a2da', '#fc4f30', '#e5ae38',
+                                           '#6d904f', '#8b8b8b']
+
+# Charts
+options.Curve = Options('style', color=Cycle(), width=2)
+options.ErrorBars = Options('style', color='black')
+options.Scatter = Options('style', color=Cycle())
+options.Points = Options('style', color=Cycle())
