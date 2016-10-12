b30661386a235e67885025b01d72cd2cb9e7300c
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2016-10-12 21:10:19 +0100

    Restored default HeatMapPlot nan color to white

--- holoviews/plotting/mpl/raster.py
+++ holoviews/plotting/mpl/raster.py
@@ -90,6 +90,14 @@ class RasterPlot(ColorbarPlot):
 
 class HeatMapPlot(RasterPlot):
 
+    clipping_colors = param.Dict(default={'NaN': 'white'}, doc="""
+        Dictionary to specify colors for clipped values, allows
+        setting color for NaN values and for values above and below
+        the min and max value. The min, max or NaN color may specify
+        an RGB(A) color as a color hex string of the form #FFFFFF or
+        #FFFFFFFF or a length 3 or length 4 tuple specifying values in
+        the range 0-1 or a named HTML color.""")
+
     show_values = param.Boolean(default=False, doc="""
         Whether to annotate each pixel with its value.""")
 
