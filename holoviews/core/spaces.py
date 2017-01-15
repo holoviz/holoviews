601d1f804bab35eedbf8d8469b53eab60747ff6f
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2017-01-15 21:10:15 +0000

    Removed dimensionless_cache

--- holoviews/core/spaces.py
+++ holoviews/core/spaces.py
@@ -530,7 +530,6 @@ class DynamicMap(HoloMap):
 
         self.call_mode = self._validate_mode()
         self.mode = 'bounded' if self.call_mode == 'key' else 'open'
-        self._dimensionless_cache = False
 
 
     def _initial_key(self):
@@ -778,7 +777,7 @@ class DynamicMap(HoloMap):
         try:
             dimensionless = util.dimensionless_contents(get_nested_streams(self),
                                                         self.kdims, no_duplicates=False)
-            if (dimensionless and not self._dimensionless_cache):
+            if dimensionless:
                 raise KeyError('Using dimensionless streams disables DynamicMap cache')
             cache = super(DynamicMap,self).__getitem__(key)
             # Return selected cache items in a new DynamicMap
