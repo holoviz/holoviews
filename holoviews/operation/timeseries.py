9bf29113e13cd6c12d3c2b35848a29d8010ad68c
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2017-03-03 13:11:25 +0000

    Added resampling timeseries operation

--- holoviews/operation/timeseries.py
+++ holoviews/operation/timeseries.py
@@ -6,6 +6,54 @@ from ..core.util import pd
 from ..element import Scatter
 
 
+class rolling(ElementOperation):
+    """
+    Applies a function over a rolling window.
+    """
+
+    center = param.Boolean(default=True, doc="""
+        Whether to set the x-coordinate at the center or right edge
+        of the window.""")
+
+    function = param.Callable(default=np.mean, doc="""
+        The function to apply over the rolling window.""")
+
+    rolling_window = param.Integer(default=10, doc="""
+        The window size over which to apply the function.""")
+
+    def _apply(self, element, key=None):
+        xdim = element.kdims[0].name
+        df = df.rolling(window=self.p.rolling_window, center=self.p.center, on=xdim)
+        return element.clone(df.apply(self.p.function).reset_index())
+
+    def _process(self, element, key=None):
+        return element.map(self._apply, Element)
+
+
+class resample(ElementOperation):
+    """
+    Resamples a timeseries of dates with a frequency and function
+    """
+
+    edge = param.ObjectSelector(default='right', doc="""
+        The bin edge to label the bin with.""")
+
+    function = param.Callable(default=np.mean, doc="""
+        The function to apply over the rolling window.""")
+
+    rule = param.String(default='D', doc="""
+        A string representing the time interval over which to apply the resampling""")
+
+    def _apply(self, element, key=None):
+        xdim = element.kdims[0].name
+        df = element.dframe()
+        df = df.resample(rule=self.p.rule, label=self.p.edge, on=xdim)
+        return element.clone(df.apply(self.p.function).reset_index())
+
+    def _process(self, element, key=None):
+        return element.map(self._apply, Element)
+
+
 class rolling_outlier_std(ElementOperation):
     """
     Detect outliers for using the standard devitation within a rolling window.
@@ -32,31 +80,6 @@ class rolling_outlier_std(ElementOperation):
         # Get indices of outliers
         outliers = (np.abs(residual) > std * sigma).values
         return element[outliers].clone(new_type=Scatter, group='Outliers')
-
-    def _process(self, element, key=None):
-        return element.map(self._apply, Element)
-
-
-class rolling(ElementOperation):
-    """
-    Applies a function over a rolling window.
-    """
-
-    center = param.Boolean(default=False, doc="""
-        Whether to set the x-coordinate at the center or right edge
-        of the window.""")
-
-    function = param.Callable(default=np.mean, doc="""
-        The function to apply over the rolling window.""")
-
-    rolling_window = param.Integer(default=10, doc="""
-        The window size over which to apply the function.""")
-
-    def _apply(self, element, key=None):
-        df = element.dframe().set_index(element.kdims[0].name)
-        df = df.rolling(window=self.p.rolling_window,
-                        center=self.p.center).apply(self.p.function)
-        return element.clone(df.reset_index())
 
     def _process(self, element, key=None):
         return element.map(self._apply, Element)
