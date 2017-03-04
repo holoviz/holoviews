2105ea46e5e10dad71c85367c08b47f233798ddf
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2017-03-04 12:44:47 +0000

    Expanded functionality and added tests for timeseries ops

--- holoviews/operation/timeseries.py
+++ holoviews/operation/timeseries.py
@@ -42,14 +42,37 @@ class rolling(ElementOperation):
     function = param.Callable(default=np.mean, doc="""
         The function to apply over the rolling window.""")
 
+    min_periods = param.Integer(default=None, doc="""
+       Minimum number of observations in window required to have a
+       value (otherwise result is NA).""")
+
     rolling_window = param.Integer(default=10, doc="""
         The window size over which to apply the function.""")
+
+    window_type = param.ObjectSelector(default=None,
+        objects=['boxcar', 'triang', 'blackman', 'hamming', 'bartlett',
+                 'parzen', 'bohman', 'blackmanharris', 'nuttall',
+                 'barthann', 'kaiser', 'gaussian', 'general_gaussian',
+                 'slepian'], doc="The type of the window to apply")
 
     def _apply(self, element, key=None):
         xdim = element.kdims[0].name
         df = get_df_data(element)
-        df = df.set_index(xdim).rolling(window=self.p.rolling_window, center=self.p.center)
-        return element.clone(df.apply(self.p.function).reset_index())
+        roll_kwargs = {'window': self.p.rolling_window,
+                       'center': self.p.center,
+                       'win_type': self.p.window_type,
+                       'min_periods': self.p.min_periods}
+        df = df.set_index(xdim).rolling(**roll_kwargs)
+        if roll_kwargs['window'] is None:
+            rolled = df.apply(self.p.function)
+        else:
+            if self.p.function is np.mean:
+                rolled = df.mean()
+            elif self.p.function is np.sum:
+                rolled = df.sum()
+            else:
+                raise ValueError("Rolling window function only supports mean and sum")
+        return element.clone(rolled.reset_index())
 
     def _process(self, element, key=None):
         return element.map(self._apply, Element)
@@ -60,11 +83,14 @@ class resample(ElementOperation):
     Resamples a timeseries of dates with a frequency and function
     """
 
-    edge = param.ObjectSelector(default='right', doc="""
-        The bin edge to label the bin with.""")
+    closed = param.ObjectSelector(default=None, objects=['left', 'right'],
+        doc="Which side of bin interval is closed")
 
     function = param.Callable(default=np.mean, doc="""
         The function to apply over the rolling window.""")
+
+    label = param.ObjectSelector(default='right', doc="""
+        The bin edge to label the bin with.""")
 
     rule = param.String(default='D', doc="""
         A string representing the time interval over which to apply the resampling""")
@@ -72,7 +98,9 @@ class resample(ElementOperation):
     def _apply(self, element, key=None):
         xdim = element.kdims[0].name
         df = get_df_data(element)
-        df = df.set_index(xdim).resample(rule=self.p.rule, label=self.p.edge)
+        resample_kwargs = {'rule': self.p.rule, 'label': self.p.label,
+                           'closed': self.p.closed}
+        df = df.set_index(xdim).resample(**resample_kwargs)
         return element.clone(df.apply(self.p.function).reset_index())
 
     def _process(self, element, key=None):
@@ -81,7 +109,7 @@ class resample(ElementOperation):
 
 class rolling_outlier_std(ElementOperation):
     """
-    Detect outliers for using the standard devitation within a rolling window.
+    Detect outliers using the standard deviation within a rolling window.
 
     Outliers are the array elements outside `sigma` standard deviations from
     the smoothed trend line, as calculated from the trend line residuals.
@@ -104,7 +132,7 @@ class rolling_outlier_std(ElementOperation):
 
         # Get indices of outliers
         outliers = (np.abs(residual) > std * sigma).values
-        return element[outliers].clone(new_type=Scatter, group='Outliers')
+        return element[outliers].clone(new_type=Scatter)
 
     def _process(self, element, key=None):
         return element.map(self._apply, Element)
