fbb6d11332608fb020051986db97e3bda11548e0
jlstevens <jlstevens@ed.ac.uk>
2017-04-19 22:51:47 +0100

    Fixed logic in display hooks for rendering empty HoloMaps

--- holoviews/ipython/display_hooks.py
+++ holoviews/ipython/display_hooks.py
@@ -179,13 +179,16 @@ def element_display(element, max_frames):
 @display_hook
 def map_display(vmap, max_frames):
     if not isinstance(vmap, (HoloMap, DynamicMap)): return None
-    if len(vmap) == 0 and isinstance(vmap, DynamicMap) and vmap.unbounded:
+    if isinstance(vmap, DynamicMap) and vmap.unbounded:
         dims = ', '.join('%r' % dim for dim in  vmap.unbounded)
         msg = ('DynamicMap cannot be displayed without explicit indexing '
                'as {dims} dimension(s) are unbounded. '
                '\nSet dimensions bounds with the DynamicMap redim.range '
                'or redim.values methods.')
         sys.stderr.write(msg.format(dims=dims))
+        return None
+
+    if len(vmap) == 0:
         return None
 
     elif len(vmap) > max_frames:
