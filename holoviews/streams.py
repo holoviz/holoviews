8c50f9c0a79f84c943bc5853b31f60dba7fb955f
jlstevens <jlstevens@ed.ac.uk>
2017-04-23 11:56:57 +0100

    Updated description of subscriber precedence

--- holoviews/streams.py
+++ holoviews/streams.py
@@ -172,6 +172,12 @@ class Stream(param.Parameterized):
         Register a callable subscriber to this stream which will be
         invoked either when event is called or when this stream is
         passed to the trigger classmethod.
+
+        Precedence allows the subscriber ordering to be
+        controlled. Users should only add subscribers with precedence
+        between zero and one while HoloViews itself reserves the use of
+        higher precedence values. Subscribers with high precedence are
+        invoked later than ones with low precedence.
         """
         if not callable(subscriber):
             raise TypeError('Subscriber must be a callable.')
