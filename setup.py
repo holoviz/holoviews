8cefd9b9c8ac17da7be7e6efa30ced7830060075
jlstevens <jlstevens@ed.ac.uk>
2017-04-25 14:28:26 +0100

    Updated setup.py to include notebooks when installling from PyPI

--- setup.py
+++ setup.py
@@ -88,12 +88,29 @@ def check_pseudo_package(path):
             raise Exception("Please make sure pseudo-package %s is populated." % path)
 
 
-if __name__=="__main__":
+def pypi_version(pypi=False):
+    if not pypi: return
+    # Make sure to create these directories and populate them before upload
+    setup_args['packages'] += ["holoviews.assets", 'holoviews.notebooks']
+
+    # Add unit tests
+    setup_args['packages'].append('holoviews.tests')
+
+    setup_args['package_data']['holoviews.assets'] = ['*.png', '*.svg', '*.rst']
+    setup_args['package_data']['holoviews.notebooks'] = ['*.ipynb', '*.npy']
 
     if ('upload' in sys.argv) or ('sdist' in sys.argv):
+        check_pseudo_package(os.path.join('.', 'holoviews', 'tests'))
+        check_pseudo_package(os.path.join('.', 'holoviews', 'assets'))
+        check_pseudo_package(os.path.join('.', 'holoviews', 'notebooks'))
+
         import holoviews
         holoviews.__version__.verify(setup_args['version'])
 
+
+if __name__=="__main__":
+
+    pypi_version(False) # Should only be true when building sdist and uploading to PYPI
 
     if 'install' in sys.argv:
         header = "HOLOVIEWS INSTALLATION INFORMATION"
