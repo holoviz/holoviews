48dbfe3bfee5ce5c47601131c2c04bcb77d56f45
jlstevens <jlstevens@ed.ac.uk>
2017-05-28 21:11:58 +0100

    Added support for replacing line magics

--- convert.py
+++ convert.py
@@ -70,17 +70,35 @@ def strip_magics(source):
     return '\n'.join(filtered)
 
 
+def replace_line_magic(source, magic, template='{line}'):
+    """
+    Given a cell's source, replace line magics using a formatting
+    template, where {line} is the string that follows the magic.
+    """
+    filtered = []
+    for line in source.splitlines():
+        if line.strip().startswith(magic):
+            substitution = template.format(line=line.replace(magic, ''))
+            filtered.append(substitution)
+        else:
+            filtered.append(line)
+    return '\n'.join(filtered)
+
+
+
 class MagicConversion(Preprocessor):
 
     def preprocess_cell(self, cell, resources, index):
         if cell['cell_type'] == 'code':
             source, opts_lines = filter_magic(cell['source'], '%%opts')
+            source = replace_line_magic(source, '%output',
+                                        template='hv.util.output({line!r})')
             source = strip_magics(source)
             if opts_lines:
                 source = wrap_cell_expression(source,
                                               'hv.util.opts(',
                                               repr(' '.join(opts_lines))+')')
-                cell['source'] = source
+            cell['source'] = source
         return cell, resources
 
     def __call__(self, nb, resources): # Temporary hack around 'enabled' flag
