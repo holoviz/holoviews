5aa7aebc43af439456d325a2551b47a17c835f0d
jlstevens <jlstevens@ed.ac.uk>
2017-06-06 00:32:50 +0100

    Bumped version to 1.8dev2

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ extras_require['all'] = (extras_require['recommended']
 
 setup_args.update(dict(
     name='holoviews',
-    version="1.8dev1",
+    version="1.8dev2",
     install_requires = install_requires,
     extras_require = extras_require,
     description='Stop plotting your data - annotate your data and let it visualize itself.',
@@ -49,6 +49,7 @@ setup_args.update(dict(
                 "holoviews.element",
                 "holoviews.interface",
                 "holoviews.ipython",
+                "holoviews.util",
                 "holoviews.operation",
                 "holoviews.plotting",
                 "holoviews.plotting.mpl",
