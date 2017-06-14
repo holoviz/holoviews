7b654ee87c1451fa3b2bc5230af22f9e9d144f62
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2017-06-14 23:53:27 +0100

    Added unit tests for Dimension relabel

--- tests/testdimensions.py
+++ tests/testdimensions.py
@@ -245,6 +245,16 @@ class DimensionedTest(ComparisonTestCase):
         redimensioned = dimensioned.clone(kdims=['Test'])
         self.assertEqual(redimensioned, dimensioned.redim(x='Test'))
 
+    def test_dimensioned_redim_dict_label(self):
+        dimensioned = Dimensioned('Arbitrary Data', kdims=['x'])
+        redimensioned = dimensioned.clone(kdims=[('x', 'Test')])
+        self.assertEqual(redimensioned, dimensioned.redim.label(x='Test'))
+
+    def test_dimensioned_redim_dict_label_existing_error(self):
+        dimensioned = Dimensioned('Arbitrary Data', kdims=[('x', 'Test1')])
+        with self.assertRaisesRegexp(ValueError, 'Cannot override an existing Dimension label'):
+            dimensioned.redim.label(x='Test2')
+
     def test_dimensioned_redim_dimension(self):
         dimensioned = Dimensioned('Arbitrary Data', kdims=['x'])
         redimensioned = dimensioned.clone(kdims=['Test'])
