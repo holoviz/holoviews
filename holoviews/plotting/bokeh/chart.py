329bdf5c872ecb7711b07e9e21b9caef9330e73a
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2017-08-31 15:07:03 +0100

    Fixed bug coloring Histogram in bokeh

--- holoviews/plotting/bokeh/chart.py
+++ holoviews/plotting/bokeh/chart.py
@@ -377,7 +377,7 @@ class SideHistogramPlot(ColorbarPlot, HistogramPlot):
                         right=element.edges[1:])
 
         color_dims = self.adjoined.traverse(lambda x: x.handles.get('color_dim'))
-        dim = color_dims[0] if color_dims else None
+        dim = [cd for cd in color_dims if cd][0] if color_dims else None
         cmapper = self._get_colormapper(dim, element, {}, {})
         if cmapper and dim in element.dimensions():
             data[dim.name] = [] if self.static_source else element.dimension_values(dim)
