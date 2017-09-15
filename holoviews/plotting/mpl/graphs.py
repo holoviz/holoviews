8234c5efecf869c5577516f7b8aad621ae9de9a8
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2017-09-15 12:40:43 +0100

    Added support for coloring mpl Graph nodes

--- holoviews/plotting/mpl/graphs.py
+++ holoviews/plotting/mpl/graphs.py
@@ -1,25 +1,54 @@
+import param
+import numpy as np
+
 from matplotlib.collections import LineCollection
 
-from .chart import ChartPlot
+from ...core.util import basestring
+from .element import ColorbarPlot
 
-class GraphPlot(ChartPlot):
+class GraphPlot(ColorbarPlot):
     """
     GraphPlot
     """
 
+    color_index = param.ClassSelector(default=None, class_=(basestring, int),
+                                  allow_None=True, doc="""
+      Index of the dimension from which the color will the drawn""")
+
     style_opts = ['edge_alpha', 'edge_color', 'edge_linestyle', 'edge_linewidth',
                   'node_alpha', 'node_color', 'node_edgecolors', 'node_facecolors',
-                  'node_linewidth', 'node_marker', 'node_size', 'visible']
+                  'node_linewidth', 'node_marker', 'node_size', 'visible', 'cmap']
+
+    def _compute_styles(self, element, ranges, style):
+        cdim = element.get_dimension(self.color_index)
+        color = style.pop('node_color', None)
+        cmap = style.get('cmap', None)
+        if cdim and cmap:
+            cs = element.dimension_values(self.color_index)
+            # Check if numeric otherwise treat as categorical
+            if cs.dtype.kind in 'if':
+                style['c'] = cs
+            else:
+                categories = np.unique(cs)
+                xsorted = np.argsort(categories)
+                ypos = np.searchsorted(categories[xsorted], cs)
+                style['c'] = xsorted[ypos]
+            self._norm_kwargs(element, ranges, style, cdim)
+        elif color:
+            style['c'] = color
+        style['node_edgecolors'] = style.pop('node_edgecolors', 'none')
+        return style
 
     def get_data(self, element, ranges, style):
         xidx, yidx = (1, 0) if self.invert_axes else (0, 1)
         pxs, pys = (element.nodes.dimension_values(i) for i in range(2))
         dims = element.nodes.dimensions()
+        self._compute_styles(element.nodes, ranges, style)
 
         paths = element.nodepaths.data
         if self.invert_axes:
             paths = [p[:, ::-1] for p in paths]
-        return {'points': (pxs, pys), 'paths': paths}, style, {'dimensions': dims}
+        return {'nodes': (pxs, pys), 'edges': paths}, style, {'dimensions': dims}
 
     def get_extents(self, element, ranges):
         """
@@ -32,33 +61,33 @@ class GraphPlot(ChartPlot):
 
     def init_artists(self, ax, plot_args, plot_kwargs):
         # Draw edges
+        color_opts = ['c', 'cmap', 'vmin', 'vmax', 'norm']
         edge_opts = {k[5:] if 'edge_' in k else k: v
                      for k, v in plot_kwargs.items()
-                     if 'node_' not in k}
-        paths = plot_args['paths']
+                     if 'node_' not in k and k not in color_opts}
+        paths = plot_args['edges']
         edges = LineCollection(paths, **edge_opts)
         ax.add_collection(edges)
 
         # Draw nodes
-        xs, ys = plot_args['points']
+        xs, ys = plot_args['nodes']
         node_opts = {k[5:] if 'node_' in k else k: v
                      for k, v in plot_kwargs.items()
                      if 'edge_' not in k}
         if 'size' in node_opts: node_opts['s'] = node_opts.pop('size')**2
         nodes = ax.scatter(xs, ys, **node_opts)
-        
+
         return {'nodes': nodes, 'edges': edges}
 
     def update_handles(self, key, axis, element, ranges, style):
-        artist = self.handles['nodes']
+        nodes = self.handles['nodes']
         data, style, axis_kwargs = self.get_data(element, ranges, style)
         xs, ys = data['nodes']
-        artist.set_xdata(xs)
-        artist.set_ydata(ys)
-        
+        nodes.set_offsets(np.column_stack([xs, ys]))
+
         edges = self.handles['edges']
         paths = data['edges']
-        artist.set_paths(paths)
-        artist.set_visible(style.get('visible', True))
-        
+        edges.set_paths(paths)
+        edges.set_visible(style.get('visible', True))
+
         return axis_kwargs
