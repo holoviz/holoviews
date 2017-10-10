a5e74114c827f2b6831c7fd3924c40f9273331b4
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2017-10-10 13:27:03 +0100

    Rewrote matplotlib path plots

--- holoviews/plotting/mpl/path.py
+++ holoviews/plotting/mpl/path.py
@@ -1,23 +1,41 @@
 from matplotlib.patches import Polygon
-from matplotlib.collections import PatchCollection, LineCollection
+from matplotlib.collections import PolyCollection, LineCollection
 import numpy as np
 import param
 
+from ...core import util
 from .element import ElementPlot, ColorbarPlot
 
 
-class PathPlot(ElementPlot):
+class PathPlot(ColorbarPlot):
 
-    aspect = param.Parameter(default='equal', doc="""
+    aspect = param.Parameter(default='square', doc="""
         PathPlots axes usually define single space so aspect of Paths
         follows aspect in data coordinates by default.""")
+    
+    color_index = param.ClassSelector(default=0, class_=(util.basestring, int),
+                                      allow_None=True, doc="""
+      Index of the dimension from which the color will the drawn""")
 
-    style_opts = ['alpha', 'color', 'linestyle', 'linewidth', 'visible']
+    style_opts = ['alpha', 'color', 'linestyle', 'linewidth', 'visible', 'cmap']
 
     def get_data(self, element, ranges, style):
-        paths = [p.array(p.kdims[:2]) for p in element.split()]
-        if self.invert_axes:
-            paths = [p[:, ::-1] for p in paths]
+        cdim = element.get_dimension(self.color_index)
+        if cdim: cidx = element.get_dimension_index(cdim)
+        if not cdim:
+            paths = element.split(datatype='array', dimensions=element.kdims)
+            if self.invert_axes:
+                paths = [p[:, ::-1] for p in paths]
+            return (paths,), style, {}
+        paths, cvals = [], []
+        for path in element.split(datatype='array'):
+            splits = [0]+list(np.where(np.diff(path[:, cidx])!=0)[0]+1)
+            for (s1, s2) in zip(splits[:-1], splits[1:]):
+                cvals.append(path[s1, cidx])
+                paths.append(path[s1:s2+1, :2])
+        self._norm_kwargs(element, ranges, style, cdim)
+        style['array'] = np.array(cvals)
+        style['clim'] = style.pop('vmin', None), style.pop('vmax', None)
         return (paths,), style, {}
 
     def init_artists(self, ax, plot_args, plot_kwargs):
@@ -29,34 +47,44 @@ class PathPlot(ElementPlot):
         artist = self.handles['artist']
         data, style, axis_kwargs = self.get_data(element, ranges, style)
         artist.set_paths(data[0])
+        if 'colors' in style:
+            artist.set_array(style['array'])
+            artist.set_clim(style['clim'])
+        if 'norm' in style:
+            artist.set_norm(style['norm'])
         artist.set_visible(style.get('visible', True))
         return axis_kwargs
 
+class ContourPlot(PathPlot):
 
-class ContourPlot(PathPlot, ColorbarPlot):
+    color_index = param.ClassSelector(default=0, class_=(util.basestring, int),
+                                      allow_None=True, doc="""
+      Index of the dimension from which the color will the drawn""")
 
-    style_opts = PathPlot.style_opts + ['cmap']
 
     def get_data(self, element, ranges, style):
-        args, style, axis_kwargs = super(ContourPlot, self).get_data(element, ranges, style)
-        value = element.level
-        if element.vdims and value is not None and np.isfinite(value) and 'cmap' in style:
-            self._norm_kwargs(element, ranges, style, element.vdims[0])
-            style['clim'] = style.pop('vmin'), style.pop('vmax')
-            style['array'] = np.array([value]*len(args[0]))
-        return args, style, axis_kwargs
+        if None not in [element.level, self.color_index]:
+            cdim = element.vdims[0]
+        else:
+            cidx = self.color_index+2 if isinstance(self.color_index, int) else self.color_index
+            cdim = element.get_dimension(cidx)
+        paths = element.split(datatype='array', dimensions=element.kdims)
+        if self.invert_axes:
+            paths = [p[:, ::-1] for p in paths]
 
-
-    def update_handles(self, key, axis, element, ranges, style):
-        artist = self.handles['artist']
-        axis_kwargs = super(ContourPlot, self).update_handles(key, axis, element, ranges, style)
-        if 'array' in style:
-            artist.set_array(style['array'])
-            artist.set_clim(style['clim'])
-        return axis_kwargs
-
-
-class PolygonPlot(ColorbarPlot):
+        if cdim is None:
+            return (paths,), style, {}
+                
+        if element.level is not None:
+            style['array'] = np.full(len(paths), value)
+        else:
+            style['array'] = element.dimension_values(cdim, expanded=False)
+        self._norm_kwargs(element, ranges, style, cdim)
+        style['clim'] = style.pop('vmin'), style.pop('vmax')
+        return (paths,), style, {}
+    
+    
+class PolygonPlot(ContourPlot):
     """
     PolygonPlot draws the polygon paths in the supplied Polygons
     object. If the Polygon has an associated value the color of
@@ -71,43 +99,7 @@ class PolygonPlot(ColorbarPlot):
     style_opts = ['alpha', 'cmap', 'facecolor', 'edgecolor', 'linewidth',
                   'hatch', 'linestyle', 'joinstyle', 'fill', 'capstyle']
 
-
-    def get_data(self, element, ranges, style):
-        value = element.level
-        vdim = element.vdims[0] if element.vdims else None
-        polys = []
-        paths = [p.array(p.kdims[:2]) for p in element.split()]
-        for segments in paths:
-            if segments.shape[0]:
-                if self.invert_axes:
-                    segments = segments[:, ::-1]
-                polys.append(Polygon(segments))
-
-        if None not in [value, vdim] and np.isfinite(value):
-            self._norm_kwargs(element, ranges, style, vdim)
-            style['clim'] = style.pop('vmin'), style.pop('vmax')
-            style['array'] = np.array([value]*len(polys))
-        return (polys,), style, {}
-
     def init_artists(self, ax, plot_args, plot_kwargs):
-        collection = PatchCollection(*plot_args, **plot_kwargs)
-        ax.add_collection(collection)
-        if self.colorbar:
-            self._draw_colorbar()
-        return {'artist': collection, 'polys': plot_args[0]}
-
-
-    def update_handles(self, key, axis, element, ranges, style):
-        value = element.level
-        vdim = element.vdims[0] if element.vdims else None
-        collection = self.handles['artist']
-        paths = [p.array(p.kdims[:2]) for p in element.split()]
-        if any(not np.array_equal(data, poly.get_xy()) for data, poly in
-               zip(paths, self.handles['polys'])):
-            return super(PolygonPlot, self).update_handles(key, axis, element, ranges, style)
-        elif None not in [value, vdim] and np.isfinite(value):
-            self._norm_kwargs(element, ranges, style, vdim)
-            collection.set_array(np.array([value]*len(element.data)))
-            collection.set_clim((style['vmin'], style['vmax']))
-            if 'norm' in style:
-                collection.norm = style['norm']
+        polys = PolyCollection(*plot_args, **plot_kwargs)
+        ax.add_collection(polys)
+        return {'artist': polys}
