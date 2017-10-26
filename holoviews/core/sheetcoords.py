Fixed datetime density precision and added tests

holoviews/core/sheetcoords.py
@@ -223,14 +223,14 @@ class SheetCoordinateSystem(object):
         # sheet as the y index increases in the matrix.
         xdensity = self.__xdensity
         if isinstance(x, datetime_types):
-            xdensity = np.timedelta64(int(1./xdensity), self._time_unit)
+            xdensity = np.timedelta64(int(round(1./xdensity)), self._time_unit)
             float_col = (x-self.lbrt[0]) / xdensity
         else:
             float_col = (x-self.lbrt[0]) * xdensity
 
         ydensity = self.__ydensity
         if isinstance(y, datetime_types):
-            ydensity = np.timedelta64(int(1./ydensity), self._time_unit)
+            ydensity = np.timedelta64(int(round(1./ydensity)), self._time_unit)
             float_row = (self.lbrt[3]-y) / ydensity
         else:
             float_row = (self.lbrt[3]-y) * ydensity
@@ -273,11 +273,11 @@ class SheetCoordinateSystem(object):
         """
         xoffset = float_col*self.__xstep
         if isinstance(self.lbrt[0], datetime_types):
-            xoffset = np.timedelta64(int(xoffset), self._time_unit)
+            xoffset = np.timedelta64(int(round(xoffset)), self._time_unit)
         x = self.lbrt[0] + xoffset
         yoffset = float_row*self.__ystep
         if isinstance(self.lbrt[3], datetime_types):
-            yoffset = np.timedelta64(int(yoffset), self._time_unit)
+            yoffset = np.timedelta64(int(round(yoffset)), self._time_unit)
         y = self.lbrt[3] - yoffset
         return x, y
 

holoviews/core/util.py
@@ -1518,7 +1518,7 @@ def bound_range(vals, density, time_unit='us'):
         raise ValueError('Could not determine Image density, ensure it has a non-zero range.')
     halfd = 0.5/density
     if isinstance(low, datetime_types):
-        halfd = np.timedelta64(int(halfd), time_unit)
+        halfd = np.timedelta64(round(halfd), time_unit)
     return low-halfd, high+halfd, density, invert
 
 
@@ -1548,7 +1548,7 @@ def date_range(start, end, length, time_unit='us'):
     step = (1./compute_density(start, end, length, time_unit))
     if pd and isinstance(start, pd.Timestamp):
         start = start.to_datetime64()
-    step = np.timedelta64(int(step), time_unit)
+    step = np.timedelta64(int(round(step)), time_unit)
     return start+step/2.+np.arange(length)*step
 
 
@@ -1562,5 +1562,7 @@ def dt_to_int(value, time_unit='us'):
     elif isinstance(value, np.datetime64):
         value = value.tolist()
     if isinstance(value, int):
-        return value * 10e-4
+        # Handle special case of nanosecond precision which cannot be
+        # represented by python datetime
+        return value * 10**-(np.log10(tscale)-3)
     return int(value.timestamp()) * tscale

holoviews/element/raster.py
@@ -424,7 +424,7 @@ class Image(Dataset, Raster, SheetCoordinateSystem):
             density = self.ydensity if idx else self.xdensity
             halfd = (1./density)/2.
             if isinstance(low, datetime_types):
-                halfd = np.timedelta64(int(halfd), self._time_unit)
+                halfd = np.timedelta64(int(round(halfd)), self._time_unit)
             return (low-halfd, high+halfd)
         else:
             return super(Image, self).range(dim, data_range)