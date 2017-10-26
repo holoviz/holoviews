5295ebecc010ad1544bc62a2f205d178f69d6878
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2017-10-26 16:00:28 +0100

    Fixed RasterDataset test

--- tests/testdataset.py
+++ tests/testdataset.py
@@ -1557,3 +1557,16 @@ class RasterDatasetTest(GridTests, ComparisonTestCase):
 
     def tearDown(self):
         Dataset.datatype = self.restore_datatype
+
+    def test_canonical_vdim(self):
+        x = np.array([ 0.  ,  0.75,  1.5 ])
+        y = np.array([ 1.5 ,  0.75,  0.  ])
+        z = np.array([[ 0.06925999,  0.05800389,  0.05620127],
+                      [ 0.06240918,  0.05800931,  0.04969735],
+                      [ 0.05376789,  0.04669417,  0.03880118]])
+        dataset = Image((x, y, z), kdims=['x', 'y'], vdims=['z'])
+        canonical = np.array([[ 0.05376789,  0.04669417,  0.03880118],
+                              [ 0.06240918,  0.05800931,  0.04969735],
+                              [ 0.06925999,  0.05800389,  0.05620127]])
+        self.assertEqual(dataset.dimension_values('z', flat=False),
+                         canonical)
