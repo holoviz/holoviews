d99400e744e1f44ab4c757dc52b84d583fcc12a9
Philipp Rudiger <philippjfr@continuum.io>
2017-10-28 00:35:52 +0100

    Removed width/height style opts from TabularPlot (#2037)

--- holoviews/plotting/bokeh/tabular.py
+++ holoviews/plotting/bokeh/tabular.py
@@ -16,7 +16,7 @@ class TablePlot(BokehPlot, GenericElementPlot):
     width = param.Number(default=400)
 
     style_opts = ['row_headers', 'selectable', 'editable',
-                  'sortable', 'fit_columns', 'width', 'height']
+                  'sortable', 'fit_columns']
 
     finalize_hooks = param.HookList(default=[], doc="""
         Optional list of hooks called when finalizing a column.
