3f9a8bd28c5e7218fe0cfb1ab9f862f1cb90ac81
Philipp Rudiger <philippjfr@continuum.io>
2017-12-15 13:08:24 +0000

    Fixed bug validating stats elements in PandasInterface (#2199)

--- holoviews/core/data/pandas.py
+++ holoviews/core/data/pandas.py
@@ -108,7 +108,7 @@ class PandasInterface(Interface):
 
     @classmethod
     def validate(cls, dataset, vdims=True):
-        dim_types = 'key' if vdims else 'all'
+        dim_types = 'all' if vdims else 'key' 
         dimensions = dataset.dimensions(dim_types, label='name')
         not_found = [d for d in dimensions if d not in dataset.data.columns]
         if not_found:
