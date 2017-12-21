ee012a14c84f0de0db741389d77dac29bc87ddc7
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2017-12-21 15:36:50 +0000

    Losened Image sampling validation

--- holoviews/element/raster.py
+++ holoviews/element/raster.py
@@ -263,13 +263,13 @@ class Image(Dataset, Raster, SheetCoordinateSystem):
                                  % (self.shape, len(self.vdims)))
 
         xvals = np.unique(np.diff(self.dimension_values(0, expanded=False)))
-        if len(xvals) > 1 and np.abs(xvals.min()-xvals.max()) > sys.float_info.epsilon*10:
+        if len(xvals) > 1 and np.abs(xvals.min()-xvals.max()) > sys.float_info.epsilon*100:
             raise ValueError("%s dimension %s is not evenly sampled, "
                              "please use the QuadMesh element for "
                              "unevenly or irregularly sampled data." %
                              (type(self).__name__, self.get_dimension(0)))
         yvals = np.unique(np.diff(self.dimension_values(1, expanded=False)))
-        if len(yvals) > 1 and np.abs(yvals.min()-yvals.max()) > sys.float_info.epsilon*10:
+        if len(yvals) > 1 and np.abs(yvals.min()-yvals.max()) > sys.float_info.epsilon*100:
             raise ValueError("%s dimension %s is not evenly sampled, "
                              "please use the QuadMesh element for "
                              "unevenly or irregularly sampled data." %
