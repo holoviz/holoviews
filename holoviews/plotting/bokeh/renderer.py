cbabb5bc649881b016087ae078616e674524c623
Jean-Luc Stevens <jlstevens@ed.ac.uk>
2018-01-30 17:47:08 +0000

    Setting the theme to None now disables theming (#2283)

--- holoviews/plotting/bokeh/renderer.py
+++ holoviews/plotting/bokeh/renderer.py
@@ -119,8 +119,7 @@ class BokehRenderer(Renderer):
         combining the bokeh model with another plot.
         """
         doc = curdoc() if doc is None else doc
-        if self_or_cls.theme:
-            doc.theme = self_or_cls.theme
+        doc.theme = self_or_cls.theme
         plot = super(BokehRenderer, self_or_cls).get_plot(obj, renderer)
         plot.document = doc
         return plot
@@ -226,8 +225,8 @@ class BokehRenderer(Renderer):
 
         for m in model.references():
             m._document = None
-        if self.theme:
-            doc.theme = self.theme
+
+        doc.theme = self.theme
         doc.add_root(model)
 
         comm_id = plot.comm.id if plot.comm else None
