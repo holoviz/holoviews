c11bc8638f77eee3590d33cd721f8c8bf90f319a
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2018-02-10 15:24:19 +0000

    Reorganized test code

--- tests/utils.py
+++ tests/utils.py
@@ -4,13 +4,6 @@ import logging
 
 from holoviews.element.comparison import ComparisonTestCase
 
-try:
-    # Standardize backend due to random inconsistencies
-    from matplotlib import pyplot
-    pyplot.switch_backend('agg')
-except:
-    pass
-
 cwd = os.path.abspath(os.path.split(__file__)[0])
 sys.path.insert(0, os.path.join(cwd, '..'))
 
