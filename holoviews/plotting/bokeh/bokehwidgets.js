b2bbcc7d004d4bec3f3d2328ec4d627bfca2484a
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2018-02-24 13:55:05 +0000

    Cleaned up widget code indendation

--- holoviews/plotting/bokeh/bokehwidgets.js
+++ holoviews/plotting/bokeh/bokehwidgets.js
@@ -1,10 +1,10 @@
 // Define Bokeh specific subclasses
 function BokehSelectionWidget() {
-	SelectionWidget.apply(this, arguments);
+  SelectionWidget.apply(this, arguments);
 }
 
 function BokehScrubberWidget() {
-	ScrubberWidget.apply(this, arguments);
+  ScrubberWidget.apply(this, arguments);
 }
 
 // Let them inherit from the baseclasses
@@ -13,39 +13,39 @@ BokehScrubberWidget.prototype = Object.create(ScrubberWidget.prototype);
 
 // Define methods to override on widgets
 var BokehMethods = {
-	update_cache : function(){
-		$.each(this.frames, $.proxy(function(index, frame) {
-			this.frames[index] = JSON.parse(frame);
-		}, this));
-	},
-	update : function(current){
-		if (current === undefined) {
-			var data = undefined;
-		} else {
-			var data = this.frames[current];
-		}
-		if (data !== undefined) {
-			var doc = Bokeh.index[data.root].model.document;
-			doc.apply_json_patch(data.content);
-		}
-	},
-	init_comms: function() {
-		this.receiver = new Bokeh.Receiver()
-		return HoloViewsWidget.prototype.init_comms.call(this);
-	},
-    process_msg : function(msg) {
-		var doc = Bokeh.index[this.plot_id].model.document;
-		var receiver = this.receiver;
-		if (msg.buffers.length > 0) {
-			receiver.consume(msg.buffers[0].buffer)
-		} else {
-			receiver.consume(msg.content.data)
-		}
-		const comm_msg = receiver.message;
-		if ((comm_msg != null) && (doc != null)) {
-			doc.apply_json_patch(comm_msg.content, comm_msg.buffers)
-		}
+  update_cache : function(){
+    $.each(this.frames, $.proxy(function(index, frame) {
+      this.frames[index] = JSON.parse(frame);
+    }, this));
+  },
+  update : function(current){
+    if (current === undefined) {
+      var data = undefined;
+    } else {
+      var data = this.frames[current];
     }
+    if (data !== undefined) {
+      var doc = Bokeh.index[data.root].model.document;
+      doc.apply_json_patch(data.content);
+    }
+  },
+  init_comms: function() {
+    this.receiver = new Bokeh.Receiver()
+    return HoloViewsWidget.prototype.init_comms.call(this);
+  },
+  process_msg : function(msg) {
+    var doc = Bokeh.index[this.plot_id].model.document;
+    var receiver = this.receiver;
+    if (msg.buffers.length > 0) {
+      receiver.consume(msg.buffers[0].buffer)
+    } else {
+      receiver.consume(msg.content.data)
+    }
+    const comm_msg = receiver.message;
+    if ((comm_msg != null) && (doc != null)) {
+      doc.apply_json_patch(comm_msg.content, comm_msg.buffers)
+    }
+  }
 }
 
 // Extend Bokeh widgets with backend specific methods
--- holoviews/plotting/mpl/mplwidgets.js
+++ holoviews/plotting/mpl/mplwidgets.js
@@ -1,10 +1,10 @@
 // Define MPL specific subclasses
 function MPLSelectionWidget() {
-    SelectionWidget.apply(this, arguments);
+  SelectionWidget.apply(this, arguments);
 }
 
 function MPLScrubberWidget() {
-    ScrubberWidget.apply(this, arguments);
+  ScrubberWidget.apply(this, arguments);
 }
 
 // Let them inherit from the baseclasses
@@ -13,25 +13,25 @@ MPLScrubberWidget.prototype = Object.create(ScrubberWidget.prototype);
 
 // Define methods to override on widgets
 var MPLMethods = {
-    init_slider : function(init_val){
-        if(this.load_json) {
-            this.from_json()
-        } else {
-            this.update_cache();
-        }
-        this.update(0);
-        if(this.mode == 'nbagg') {
-            this.set_frame(init_val, 0);
-        }
-    },
-    process_msg : function(msg) {
-        if (!(this.mode == 'nbagg')) {
-            var data = msg.content.data;
-            this.frames[this.current] = data;
-            this.update_cache(true);
-            this.update(this.current);
-        }
+  init_slider : function(init_val){
+    if(this.load_json) {
+      this.from_json()
+    } else {
+      this.update_cache();
     }
+    this.update(0);
+    if(this.mode == 'nbagg') {
+      this.set_frame(init_val, 0);
+    }
+  },
+  process_msg : function(msg) {
+    if (!(this.mode == 'nbagg')) {
+      var data = msg.content.data;
+      this.frames[this.current] = data;
+      this.update_cache(true);
+      this.update(this.current);
+    }
+  }
 }
 // Extend MPL widgets with backend specific methods
 extend(MPLSelectionWidget.prototype, MPLMethods);
