2a5afca0b70b797cd0372d4417ee4f94d81aeeb8
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2018-03-09 01:55:21 +0000

    Ensure select on dask arrays in GridInterface is lazy

--- holoviews/core/data/grid.py
+++ holoviews/core/data/grid.py
@@ -243,7 +243,7 @@ class GridInterface(DictInterface):
         dropped = [dims.index(d) for d in dims
                    if d not in dataset.kdims+virtual_coords]
         if dropped:
-            data = data.squeeze(axis=tuple(dropped))
+            data = np.squeeze(data, axis=tuple(dropped))
 
         if not any(cls.irregular(dataset, d) for d in dataset.kdims):
             inds = [dims.index(kd.name) for kd in dataset.kdims]
@@ -354,7 +354,8 @@ class GridInterface(DictInterface):
                 group_data = group_data if np.isscalar(group_data) else group_data.columns()
             else:
                 group_data = cls.select(dataset, **select)
-            if np.isscalar(group_data):
+
+            if np.isscalar(group_data) or (isinstance(group_data, array_types) and da.group_data.shape == ()):
                 group_data = {dataset.vdims[0].name: np.atleast_1d(group_data)}
                 for dim, v in zip(dim_names, unique_key):
                     group_data[dim] = np.atleast_1d(v)
@@ -460,15 +461,25 @@ class GridInterface(DictInterface):
         int_inds = [np.argwhere(v) for v in value_select][::-1]
         index = np.ix_(*[np.atleast_1d(np.squeeze(ind)) if ind.ndim > 1 else np.atleast_1d(ind)
                          for ind in int_inds])
+
         for kdim in dataset.kdims:
             if cls.irregular(dataset, dim):
-                data[kdim.name] = np.asarray(data[kdim.name])[index]
+                if da and isinstance(dataset.data[kdim.name], da.Array):
+                    data[kdim.name] = dataset.data[kdim.name].vindex[index]
+                else:
+                    data[kdim.name] = np.asarray(data[kdim.name])[index]
+
         for vdim in dataset.vdims:
-            data[vdim.name] = np.asarray(dataset.data[vdim.name])[index]
+            if da and isinstance(dataset.data[vdim.name], da.Array):
+                data[vdim.name] = dataset.data[vdim.name].vindex[index]
+            else:
+                data[vdim.name] = np.asarray(dataset.data[vdim.name])[index]
 
         if indexed:
             if len(dataset.vdims) == 1:
                 arr = np.squeeze(data[dataset.vdims[0].name])
+                if da and isinstance(arr, da.Array):
+                    arr = arr.compute()
                 return arr if np.isscalar(arr) else arr[()]
             else:
                 return np.array([np.squeeze(data[vd.name])
@@ -503,7 +514,7 @@ class GridInterface(DictInterface):
             for vdim, array in zip(dataset.vdims, arrays):
                 flat_index = np.ravel_multi_index(tuple(int_inds)[::-1], array.shape)
                 if da and isinstance(array, da.Array):
-                    data[vdim.name].append(array.flatten()[tuple(flat_index)])
+                    data[vdim.name].append(array.flatten().vindex[tuple(flat_index)])
                 else:
                     data[vdim.name].append(array.flat[flat_index])
         concatenated = {d: np.concatenate(arrays).flatten() for d, arrays in data.items()}
@@ -547,7 +558,7 @@ class GridInterface(DictInterface):
                 if len(axes) > 1:
                     vdata = vdata.transpose(axes[::-1])
                 if dropped_axes:
-                    vdata = vdata.squeeze(axis=dropped_axes)
+                    vdata = np.squeeze(vdata, axis=dropped_axes)
                 data[vdim.name] = vdata
             return data
         elif dropped_kdims:
