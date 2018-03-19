8b4f68e33a9da5d46cf8f01666cfd732ba47d11a
Philipp Rudiger <philippjfr@continuum.io>
2018-03-19 16:57:02 +0000

    Allow overriding rtol on Image (#2440)

--- tests/element/testimage.py
+++ tests/element/testimage.py
@@ -68,3 +68,13 @@ class TestImage(ComparisonTestCase):
         hv.config.image_rtol = 10e-3
         Image({'vals':vals, 'xs':xs, 'ys':ys}, ['xs','ys'], 'vals')
         hv.config.image_rtol = image_rtol
+
+    def test_image_clone(self):
+        vals = np.random.rand(20,20)
+        xs = np.linspace(0,10,20)
+        ys = np.linspace(0,10,20)
+        ys[-1] += 0.001
+        img = Image({'vals':vals, 'xs':xs, 'ys':ys}, ['xs','ys'], 'vals', rtol=10e-3)
+        self.assertEqual(img.clone().rtol, 10e-3)
+        
+
