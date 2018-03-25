aa55697b661ca6a3338a28a743c298efa2eef084
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2018-03-25 13:22:24 +0100

    Add support for interpolating bokeh colormaps

--- holoviews/plotting/util.py
+++ holoviews/plotting/util.py
@@ -467,9 +467,7 @@ def map_colors(arr, crange, cmap, hex=True):
         arr = np.ma.array(arr, mask=np.logical_not(np.isfinite(arr)))
     arr = cmap(arr)
     if hex:
-        arr *= 255
-        return ["#{0:02x}{1:02x}{2:02x}".format(*(int(v) for v in c[:-1]))
-                for c in arr]
+        return rgb2hex(arr)
     else:
         return arr
 
@@ -498,11 +496,64 @@ def bokeh_palette_to_palette(cmap, ncolors=None):
              palette = palette[ncolors]
          else:
              palette = sorted(palette.items())[-1][1]
+             palette = polylinear_gradient(palette, ncolors or 255)['hex']
     elif callable(palette):
         palette = palette(ncolors or 255)
     if ncolors:
         return [palette[i%len(palette)] for i in range(ncolors)]
     return list(palette)
+
+
+def color_dict(gradient):
+  ''' Takes in a list of RGB sub-lists and returns dictionary of
+    colors in RGB and hex form for use in a graphing function
+    defined later on '''
+  return {"hex":[rgb2hex([c/255. for c in RGB]) for RGB in gradient],
+      "r":[RGB[0] for RGB in gradient],
+      "g":[RGB[1] for RGB in gradient],
+      "b":[RGB[2] for RGB in gradient]}
+
+
+def linear_gradient(start_hex, finish_hex="#FFFFFF", n=10):
+  ''' returns a gradient list of (n) colors between
+    two hex colors. start_hex and finish_hex
+    should be the full six-digit color string,
+    inlcuding the number sign ("#FFFFFF") '''
+  # Starting and ending colors in RGB form
+  s = hex2rgb(start_hex)
+  f = hex2rgb(finish_hex)
+  # Initilize a list of the output colors with the starting color
+  RGB_list = [s]
+  # Calcuate a color at each evenly spaced value of t from 1 to n
+  for t in range(1, n):
+    # Interpolate RGB vector for color at the current value of t
+    curr_vector = [
+      int(s[j] + (float(t)/(n-1))*(f[j]-s[j]))
+      for j in range(3)
+    ]
+    # Add it to our list of output colors
+    RGB_list.append(curr_vector)
+
+  return color_dict(RGB_list)
+
+
+def polylinear_gradient(colors, n):
+  ''' returns a list of colors forming linear gradients between
+      all sequential pairs of colors. "n" specifies the total
+      number of desired output colors '''
+  # The number of colors per individual linear gradient
+  n_out = int(float(n) / (len(colors) - 1))
+  # returns dictionary defined by color_dict()
+  gradient_dict = linear_gradient(colors[0], colors[1], n_out)
+
+  if len(colors) > 1:
+    for col in range(1, len(colors) - 1):
+      next = linear_gradient(colors[col], colors[col+1], n_out)
+      for k in ("hex", "r", "g", "b"):
+        # Exclude first point to avoid duplicates
+        gradient_dict[k] += next[k][1:]
+
+  return gradient_dict
 
 
 def list_cmaps():
@@ -614,6 +665,13 @@ def rgb2hex(rgb):
     if len(rgb) > 3:
         rgb = rgb[:-1]
     return "#{0:02x}{1:02x}{2:02x}".format(*(int(v*255) for v in rgb))
+
+
+def hex2rgb(hex):
+  ''' "#FFFFFF" -> [255,255,255] '''
+  # Pass 16 to the integer function for change of base
+  return [int(hex[i:i+2], 16) for i in range(1,6,2)]
+
 
 
 HTML_COLORS = {
