659c7227c1a5841550460d56ec483f9bcd508696
jlstevens <jlstevens@ed.ac.uk>
2018-03-26 16:45:36 +0100

    Updated docstring and isinstance check in Options.max_cycles method

--- holoviews/core/options.py
+++ holoviews/core/options.py
@@ -452,11 +452,11 @@ class Options(param.Parameterized):
 
     def max_cycles(self, num):
         """
-        Truncates all contained Cycle objects to a maximum number
-        of Cycles and returns a new Options object with the
-        truncated or resampled Cycles.
+        Truncates all contained Palette objects to a maximum number
+        of samples and returns a new Options object containing the
+        truncated or resampled Palettes.
         """
-        kwargs = {kw: (arg[num] if isinstance(arg, Cycle) else arg)
+        kwargs = {kw: (arg[num] if isinstance(arg, Palette) else arg)
                   for kw, arg in self.kwargs.items()}
         return self(max_cycles=num, **kwargs)
 
