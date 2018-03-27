0702ee34c61cd113c2c146258c6b2a481b87902a
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2018-03-27 19:57:15 +0100

    Provide full control over bokeh grid styles

--- tests/plotting/bokeh/testelementplot.py
+++ tests/plotting/bokeh/testelementplot.py
@@ -82,3 +82,15 @@ class TestElementPlotPlot(TestBokehPlot):
         plot = bokeh_renderer.get_plot(curve).state
         self.assertIsInstance(plot.yaxis[0].formatter, FuncTickFormatter)
 
+    def test_element_grid_options(self):
+        grid_style = {'grid_line_color': 'blue', 'grid_line_width': 1.5, 'ygrid_bounds': (0.3, 0.7),
+                      'minor_xgrid_line_color': 'lightgray', 'xgrid_line_dash': [4, 4]}
+        curve = Curve(range(10)).options(show_grid=True, gridstyle=grid_style)
+        plot = bokeh_renderer.get_plot(curve)
+        self.assertEqual(plot.state.xgrid[0].grid_line_color, 'blue')
+        self.assertEqual(plot.state.xgrid[0].grid_line_width, 1.5)
+        self.assertEqual(plot.state.xgrid[0].grid_line_dash, [4, 4])
+        self.assertEqual(plot.state.xgrid[0].minor_grid_line_color, 'lightgray')
+        self.assertEqual(plot.state.ygrid[0].grid_line_color, 'blue')
+        self.assertEqual(plot.state.ygrid[0].grid_line_width, 1.5)
+        self.assertEqual(plot.state.ygrid[0].bounds, (0.3, 0.7))
