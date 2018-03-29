8ba64219b375f7d7b14f44dd161be1b3f75d1984
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2018-03-29 17:27:47 +0100

    Fixed mpl HexTile test

--- tests/plotting/matplotlib/testhextilesplot.py
+++ tests/plotting/matplotlib/testhextilesplot.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from holoviews.element import HexTiles
 
+
 from .testplot import TestMPLPlot, mpl_renderer
 
 
@@ -12,14 +13,16 @@ class TestHexTilesPlot(TestMPLPlot):
         mpl_renderer.get_plot(tiles)
 
     def test_hex_tiles_opts(self):
+        from holoviews.plotting.util import process_cmap
         tiles = HexTiles([(0, 0), (0.5, 0.5), (-0.5, -0.5), (-0.4, -0.4)])
         plot = mpl_renderer.get_plot(tiles)
         args, style, _ = plot.get_data(tiles, {}, {})
         self.assertEqual(args[0], tiles['x'])
         self.assertEqual(args[1], tiles['y'])
         self.assertEqual(args[2], np.ones(4))
+        cmap = style.pop('cmap')
         self.assertEqual(style, {'reduce_C_function': np.sum,
                                  'vmin': None, 'vmax': None,
                                  'xscale': 'linear', 'yscale': 'linear',
-                                 'gridsize': 50, 'mincnt': None,
-                                 'cmap': None})
+                                 'gridsize': 50, 'mincnt': None})
+        self.assertEqual(cmap.colors, process_cmap('viridis'))
