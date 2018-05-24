a9878594908aa0d427bdc985b1043c407024243d
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2018-05-24 19:55:44 +0100

    Added unit tests for HistogramPlot datetime handling

--- tests/plotting/bokeh/testhistogramplot.py
+++ tests/plotting/bokeh/testhistogramplot.py
@@ -1,6 +1,11 @@
+import datetime as dt
+
 import numpy as np
 
-from holoviews.element import Image, Points
+from holoviews.element import Image, Points, Dataset
+from holoviews.operation import histogram
+
+from bokeh.models import DatetimeAxis
 
 from .testplot import TestBokehPlot, bokeh_renderer
 
@@ -47,3 +52,23 @@ class TestSideHistogramPlot(TestBokehPlot):
         self.assertIs(main_plot.handles['color_mapper'],
                       top_plot.handles['color_mapper'])
         self.assertEqual(main_plot.handles['color_dim'], img.vdims[0])
+
+    def test_histogram_datetime64_plot(self):
+        dates = np.array([dt.datetime(2017, 1, i) for i in range(1, 5)])
+        hist = histogram(Dataset(dates, 'Date'), num_bins=4)
+        plot = bokeh_renderer.get_plot(hist)
+        source = plot.handles['source']
+        data = {'top': np.array([  3.85802469e-18,   3.85802469e-18,   3.85802469e-18, 3.85802469e-18]),
+                'left': np.array(['2017-01-01T00:00:00.000000', '2017-01-01T17:59:59.999999',
+                                  '2017-01-02T12:00:00.000000', '2017-01-03T06:00:00.000000'],
+                                 dtype='datetime64[us]'),
+                'right': np.array(['2017-01-01T17:59:59.999999', '2017-01-02T12:00:00.000000',
+                                   '2017-01-03T06:00:00.000000', '2017-01-04T00:00:00.000000'],
+                                  dtype='datetime64[us]')}
+        for k, v in data.items():
+            self.assertEqual(source.data[k], v)
+        xaxis = plot.handles['xaxis']
+        range_x = plot.handles['x_range']
+        self.assertIsInstance(xaxis, DatetimeAxis)
+        self.assertEqual(range_x.start, np.datetime64('2017-01-01T00:00:00.000000', 'us'))
+        self.assertEqual(range_x.end, np.datetime64('2017-01-04T00:00:00.000000', 'us'))
