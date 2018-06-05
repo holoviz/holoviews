a5ec08c3c90c89e0912cea4cfe5f67357653f406
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2018-06-05 17:52:41 +0100

    Simplified JS window.HoloViews logic

--- holoviews/plotting/widgets/widgets.js
+++ holoviews/plotting/widgets/widgets.js
@@ -530,7 +530,7 @@ function init_dropdown(id, plot_id, dim, vals, value, next_vals, labels, next_di
 if (window.HoloViews === undefined) {
   window.HoloViews = {}
   window.PyViz = window.HoloViews
-} else if ((window.HoloViews !== undefined) && (window.PyViz === undefined)) {
+} else if (window.PyViz === undefined) {
   window.PyViz = window.HoloViews
 }
 
