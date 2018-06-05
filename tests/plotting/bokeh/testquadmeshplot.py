27829320dc73d805792aa23db72908e279f4aded
Philipp Rudiger <philippjfr@continuum.io>
2018-06-05 14:27:18 +0100

    Fixed inverted QuadMesh bug (#2771)

--- tests/plotting/bokeh/testquadmeshplot.py
+++ tests/plotting/bokeh/testquadmeshplot.py
@@ -36,3 +36,15 @@ class TestQuadMeshPlot(TestBokehPlot):
         plot = bokeh_renderer.get_plot(qmesh)
         self.assertIsInstance(plot.handles['colorbar'], ColorBar)
         self.assertIs(plot.handles['colorbar'].color_mapper, plot.handles['color_mapper'])
+
+    def test_quadmesh_inverted_coords(self):
+        xs = [0, 1, 2]
+        ys = [2, 1, 0]
+        qmesh = QuadMesh((xs, ys, np.random.rand(3, 3)))
+        plot = bokeh_renderer.get_plot(qmesh)
+        source = plot.handles['source']
+        self.assertEqual(source.data['z'], qmesh.dimension_values(2, flat=False).T.flatten())
+        self.assertEqual(source.data['left'], np.array([-0.5, -0.5, -0.5, 0.5, 0.5, 0.5, 1.5, 1.5, 1.5]))
+        self.assertEqual(source.data['right'], np.array([0.5, 0.5, 0.5, 1.5, 1.5, 1.5, 2.5, 2.5, 2.5]))
+        self.assertEqual(source.data['top'], np.array([0.5, 1.5, 2.5, 0.5, 1.5, 2.5, 0.5, 1.5, 2.5]))
+        self.assertEqual(source.data['bottom'], np.array([-0.5, 0.5, 1.5, -0.5, 0.5, 1.5, -0.5, 0.5, 1.5]))
