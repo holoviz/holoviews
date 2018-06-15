5486592841009f5cbb9d1b92b6fd721d59c0e906
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2018-06-15 16:29:38 +0100

    Small fix for split_overlay with NdOverlay

--- holoviews/core/spaces.py
+++ holoviews/core/spaces.py
@@ -1229,8 +1229,8 @@ class DynamicMap(HoloMap):
         """
         if not len(self):
             raise ValueError('Cannot split DynamicMap before it has been initialized')
-        elif not issubclass(self.type, Overlay):
-            return None, self
+        elif not issubclass(self.type, CompositeOverlay):
+            return [None], self
 
         from ..util import Dynamic
         keys = list(self.last.keys())
