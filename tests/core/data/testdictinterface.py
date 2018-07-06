b6fab7d3920789f4dbb703175881988f8307faaf
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2018-07-06 14:42:47 +0100

    Ignore non-referenced data in dictionary format

--- tests/core/data/testdictinterface.py
+++ tests/core/data/testdictinterface.py
@@ -29,3 +29,10 @@ class DictDatasetTest(HeterogeneousColumnTests, ScalarColumnTests, InterfaceTest
         ds = Dataset({('x', 'y'): []}, kdims=['x', 'y'])
         ds2 = Dataset({'x': [], 'y': []}, kdims=['x', 'y'])
         self.assertEqual(ds, ds2)
+
+    def test_dataset_ignore_non_dimensions(self):
+        ds = Dataset({'x': [0, 1], 'y': [1, 2], 'ignore_scalar': 1,
+                      'ignore_array': np.array([2, 3]), 'ignore_None': None},
+                     kdims=['x', 'y'])
+        ds2 = Dataset({'x': [0, 1], 'y': [1, 2]}, kdims=['x', 'y'])
+        self.assertEqual(ds, ds2)
