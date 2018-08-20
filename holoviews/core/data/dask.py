309f4d67854f4a1cfd2f9de52239ac1c0a70607c
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2018-08-20 14:00:25 +0100

    Fixed bug in DaskInterface.range for non-numeric columns

--- holoviews/core/data/dask.py
+++ holoviews/core/data/dask.py
@@ -67,7 +67,7 @@ class DaskInterface(PandasInterface):
         column = columns.data[columns.get_dimension(dimension).name]
         if column.dtype.kind == 'O':
             column = np.sort(column[column.notnull()].compute())
-            return column[0], column[-1]
+            return (column[0], column[-1]) if len(column) else (None, None)
         else:
             return dd.compute(column.min(), column.max())
 
