e6a0a3bb3321cf77bbc7e398e28e1f8fd495a519
jlstevens <jlstevens@ed.ac.uk>
2018-09-03 15:50:29 +0100

    Importing nb_mime_js directly from pyviz_comms

--- holoviews/ipython/__init__.py
+++ holoviews/ipython/__init__.py
@@ -2,8 +2,8 @@ import os
 from unittest import SkipTest
 
 import param
-import pyviz_comms
 import holoviews
+from pyviz_comms import nb_mime_js
 from IPython import version_info
 from param import ipython as param_ext
 from IPython.display import HTML, publish_display_data
@@ -240,9 +240,7 @@ class notebook_extension(extension):
             widgetjs, widgetcss = '', ''
 
         # Add classic notebook MIME renderer
-        comm_path = os.path.dirname(os.path.abspath(pyviz_comms.__file__))
-        with open(os.path.join(comm_path, 'notebook.js')) as f:
-            widgetjs += '\n\n' + f.read()
+        widgetjs += nb_mime_js
 
         templateLoader = jinja2.FileSystemLoader(os.path.dirname(os.path.abspath(__file__)))
         jinjaEnv = jinja2.Environment(loader=templateLoader)
