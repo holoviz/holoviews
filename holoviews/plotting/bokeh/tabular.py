b1f221954ee815a1f82da230aea566052b50bc77
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2018-09-17 15:29:21 +0100

    Apply correct datetime formatter to Table

--- holoviews/plotting/bokeh/tabular.py
+++ holoviews/plotting/bokeh/tabular.py
@@ -89,7 +89,7 @@ class TablePlot(BokehPlot, GenericElementPlot):
                 formatter = NumberFormatter(format='0,0.0[00000]')
                 editor = NumberEditor()
             elif kind == 'M' or (kind == 'O' and len(data[col]) and type(data[col][0]) in datetime_types):
-                dimtype = element.get_dimension_type(0)
+                dimtype = element.get_dimension_type(col)
                 dformat = Dimension.type_formatters.get(dimtype, '%Y-%m-%d %H:%M:%S')
                 formatter = DateFormatter(format=dformat)
                 editor = DateEditor()
