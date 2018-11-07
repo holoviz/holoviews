f478a29aaac7d977bf02e7137141913c135e06e4
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2018-11-07 19:20:18 +0000

    Avoid deprecation warning in datetime64 conversion utility

--- holoviews/core/util.py
+++ holoviews/core/util.py
@@ -1659,7 +1659,7 @@ def dt64_to_dt(dt64):
     """
     Safely converts NumPy datetime64 to a datetime object.
     """
-    ts = (dt64 - np.datetime64('1970-01-01T00:00:00Z')) / np.timedelta64(1, 's')
+    ts = (dt64 - np.datetime64('1970-01-01T00:00:00')) / np.timedelta64(1, 's')
     return dt.datetime.utcfromtimestamp(ts)
 
 
