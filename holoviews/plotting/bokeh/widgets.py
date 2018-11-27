061ce8613ac8b75db285d881cc47f20110b6d4f8
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2018-11-27 13:00:07 +0000

    Fixed sorting issues in HoloMap export

--- holoviews/plotting/bokeh/widgets.py
+++ holoviews/plotting/bokeh/widgets.py
@@ -275,6 +275,9 @@ class BokehWidget(NdWidget):
 
     def _get_data(self):
         # Get initial frame to draw immediately
+        if not self.plot.dynamic:
+            _, _, init_dim_vals = self.get_widgets()
+            self.plot.update(tuple(init_dim_vals))
         msg, metadata = self.renderer.components(self.plot, comm=False)
         data = super(BokehWidget, self)._get_data()
         return dict(data, init_html=msg['text/html'],
