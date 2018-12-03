f84bc0377775b2ee93efba3ea8edea2e3b457d58
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2018-12-03 23:40:02 +0000

    Fixed bug

--- holoviews/plotting/links.py
+++ holoviews/plotting/links.py
@@ -69,7 +69,7 @@ class Link(param.Parameterized):
                 k: v for k, v in self.get_param_values() if k != 'name'}
             for link in links:
                 link_params = {
-                    k: v for k, v in self.get_param_values() if k != 'name'}
+                    k: v for k, v in link.get_param_values() if k != 'name'}
                 if (type(link) is type(self) and link.source is self.source
                     and link.target is self.target and params == link_params):
                     return
