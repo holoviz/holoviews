51dd840f6c890644cb7708b88542cacf8a3a8262
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2018-12-03 22:06:23 +0000

    Ensure Link is idempotent

--- holoviews/plotting/links.py
+++ holoviews/plotting/links.py
@@ -64,6 +64,15 @@ class Link(param.Parameterized):
         Registers the Link
         """
         if self.source in self.registry:
+            links = self.registry[self.source]
+            params = {
+                k: v for k, v in self.get_param_values() if k != 'name'}
+            for link in links:
+                link_params = {
+                    k: v for k, v in self.get_param_values() if k != 'name'}
+                if (type(link) is type(self) and link.source is self.source
+                    and link.target is self.target and params == link_params):
+                    return
             self.registry[self.source].append(self)
         else:
             self.registry[self.source] = [self]
