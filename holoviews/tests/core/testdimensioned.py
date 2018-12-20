d6374ee1c84f68de9e943a75d99a654b463de490
Philipp Rudiger <philippjfr@continuum.io>
2018-12-20 18:09:42 +0000

    Fixed bug setting options when backends switched (#3323)

--- holoviews/tests/core/testdimensioned.py
+++ holoviews/tests/core/testdimensioned.py
@@ -173,3 +173,14 @@ class TestDimensioned_options(CustomBackendTestCase):
         assert plot_opts.options == {'plot_opt1': 'B'}
         style_opts = Store.lookup_options('backend_2', obj, 'style')
         assert style_opts.options == {'style_opt1': 'A'}
+
+    def test_apply_options_when_backend_switched(self):
+        obj = TestObj([])
+        Store.current_backend = 'backend_2'
+        obj.opts(style_opt1='A', plot_opt1='B')
+        Store.current_backend = 'backend_1'
+        obj.opts(style_opt1='C', plot_opt1='D', backend='backend_2')
+        plot_opts = Store.lookup_options('backend_2', obj, 'plot')
+        assert plot_opts.options == {'plot_opt1': 'D'}
+        style_opts = Store.lookup_options('backend_2', obj, 'style')
+        assert style_opts.options == {'style_opt1': 'C'}
