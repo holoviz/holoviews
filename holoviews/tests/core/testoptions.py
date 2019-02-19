d2b387d7dec969a35ba4d015c493b6ef54801f39
jlstevens <jlrstevens@gmail.com>
2019-02-18 20:48:43 -0600

    Added a unit test for combining dictionary with the backend keyword

--- holoviews/tests/core/testoptions.py
+++ holoviews/tests/core/testoptions.py
@@ -1000,6 +1000,16 @@ class TestCrossBackendOptionSpecification(ComparisonTestCase):
         self.assertEqual(bokeh_lookup['cmap'], 'Purple')
         self.assert_output_options_group_empty(img)
 
+
+    def test_mpl_bokeh_mpl_via_dict_backend_keyword(self):
+        curve = Curve([1,2,3])
+        styled_mpl = curve.opts({'Curve': dict(color='red')}, backend='matplotlib')
+        styled = styled_mpl.opts({'Curve': dict(color='green')}, backend='bokeh')
+        mpl_lookup = Store.lookup_options('matplotlib', styled, 'style')
+        self.assertEqual(mpl_lookup.kwargs['color'], 'red')
+        bokeh_lookup = Store.lookup_options('bokeh', styled, 'style')
+        self.assertEqual(bokeh_lookup.kwargs['color'], 'green')
+
     def test_mpl_bokeh_mpl_via_builders_opts_method_implicit_backend(self):
         img = Image(np.random.rand(10,10))
         Store.set_current_backend('matplotlib')
