cc0f4d201b81d0eefb226606feebd3c338c178d8
Philipp Rudiger <prudiger@anaconda.com>
2019-02-27 01:29:13 +0000

    Bumped bokeh version requirement

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ extras_require['notebook'] = ['ipython>=5.4.0,<=7.1.1', 'notebook']
 
 # IPython Notebook + pandas + matplotlib + bokeh
 extras_require['recommended'] = extras_require['notebook'] + [
-    'pandas', 'matplotlib>=2.1', 'bokeh>=1.0.0,<1.1.0', 'panel']
+    'pandas', 'matplotlib>=2.1', 'bokeh>=1.1.0', 'panel']
 
 # Requirements to run all examples
 extras_require['examples'] = extras_require['recommended'] + [
