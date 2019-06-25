f9e77403e84373f2e47a7a8176480b59ff5a00f6
Justin Bois <bois@caltech.edu>
2019-06-25 09:51:28 -0700

    Updated definition of box plot whiskers to match convention for Bokeh plots. (#3755)
    * Updated definition of box plot whiskers to match convention.
    
    * Corrected definition of box plot whiskers to match convention.

--- holoviews/plotting/bokeh/stats.py
+++ holoviews/plotting/bokeh/stats.py
@@ -137,8 +137,8 @@ class BoxWhiskerPlot(CompositeElementPlot, ColorbarPlot, LegendPlot):
             q1, q2, q3 = (np.percentile(vals, q=q)
                           for q in range(25, 100, 25))
             iqr = q3 - q1
-            upper = min(q3 + 1.5 * iqr, vals.max())
-            lower = max(q1 - 1.5 * iqr, vals.min())
+            upper = vals[vals <= q3 + 1.5*iqr].max()
+            lower = vals[vals >= q1 - 1.5*iqr].min()
         else:
             q1, q2, q3 = 0, 0, 0
             upper, lower = 0, 0
