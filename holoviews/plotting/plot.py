721d99d30af566a42654423a1702e6ef7fe27746
Philipp Rudiger <prudiger@anaconda.com>
2019-12-11 17:35:47 +0100

    Handle nested data

--- holoviews/plotting/plot.py
+++ holoviews/plotting/plot.py
@@ -702,6 +702,8 @@ class DimensionedPlot(Plot):
                             values = el.nodes.dimension_values(2, expanded=False)
                         else:
                             values = el.dimension_values(el_dim, expanded=False)
+                        if values.dtype.kind == 'O' and all(isinstance(v, np.ndarray) for v in values):
+                            values = np.concatenate(values)
                     elif isinstance(el, Graph) and el_dim in el.nodes:
                         values = el.nodes.dimension_values(el_dim, expanded=False)
                     factors = util.unique_array(values)
