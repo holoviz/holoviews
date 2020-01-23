aec570313b36b1952dd3270f65581a8cc24a4eb2
jlstevens <jlrstevens@gmail.com>
2020-01-23 15:44:01 -0600

    Fixed incorrect datashader unit test

--- holoviews/tests/operation/testdatashader.py
+++ holoviews/tests/operation/testdatashader.py
@@ -500,7 +500,7 @@ class DatashaderAggregateTests(ComparisonTestCase):
         xs = [0.25, 0.75, 1.25, 1.75]
         ys = [0.125, 0.375, 0.625, 0.875]
         arr = np.array([
-            [2, 1, 1, 1],
+            [1, 1, 1, 1],
             [0, 1, 0, 1],
             [0, 1, 1, 0],
             [0, 0, 1, 0]
