2f48c196e5f21c4f95222468ef33c0b78bc18cee
Philipp Rudiger <prudiger@anaconda.com>
2020-02-27 16:03:05 -0600

    Cast categorical selections back to original type

--- holoviews/plotting/bokeh/callbacks.py
+++ holoviews/plotting/bokeh/callbacks.py
@@ -883,17 +883,34 @@ class SelectionXYCallback(BoundsCallback):
         msg = super(SelectionXYCallback, self)._process_msg(msg)
         if 'bounds' not in msg:
             return msg
+        el = self.plot.current_frame
         x0, y0, x1, y1 = msg['bounds']
         x_range = self.plot.handles['x_range']
         if isinstance(x_range, FactorRange):
             x0, x1 = int(round(x0)), int(round(x1))
-            msg['x_selection'] = x_range.factors[x0: x1]
+            xfactors = x_range.factors[x0: x1]
+            xdim = el.get_dimension(x_range.tags[0][0])
+            if hasattr(el, 'interface'):
+                dtype = el.interface.dtype(el, xdim)
+                try:
+                    xfactors = list(np.array(xfactors).astype(dtype))
+                except:
+                    pass
+            msg['x_selection'] = xfactors
         else:
             msg['x_selection'] = (x0, x1)
         y_range = self.plot.handles['y_range']
         if isinstance(y_range, FactorRange):
             y0, y1 = int(round(y0)), int(round(y1))
-            msg['y_selection'] = y_range.factors[y0: y1]
+            yfactors = y_range.factors[y0: y1]
+            ydim = el.get_dimension(y_range.tags[0][0])
+            if hasattr(el, 'interface'):
+                dtype = el.interface.dtype(el, ydim)
+                try:
+                    yfactors = list(np.array(yfactors).astype(dtype))
+                except:
+                    pass
+            msg['y_selection'] = yfactors
         else:
             msg['y_selection'] = (y0, y1)
         return msg
