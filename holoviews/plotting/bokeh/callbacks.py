9248c61ca621eaf25cfb05db0e94b8d14886b07a
Philipp Rudiger <prudiger@anaconda.com>
2020-02-28 17:58:00 -0600

    Fix callback bug

--- holoviews/plotting/bokeh/callbacks.py
+++ holoviews/plotting/bokeh/callbacks.py
@@ -889,13 +889,14 @@ class SelectionXYCallback(BoundsCallback):
         if isinstance(x_range, FactorRange):
             x0, x1 = int(round(x0)), int(round(x1))
             xfactors = x_range.factors[x0: x1]
-            xdim = el.get_dimension(x_range.tags[0][0])
-            if hasattr(el, 'interface'):
-                dtype = el.interface.dtype(el, xdim)
-                try:
-                    xfactors = list(np.array(xfactors).astype(dtype))
-                except:
-                    pass
+            if x_range.tags and x_range.tags[0]:
+                xdim = el.get_dimension(x_range.tags[0][0])
+                if xdim and hasattr(el, 'interface'):
+                    dtype = el.interface.dtype(el, xdim)
+                    try:
+                        xfactors = list(np.array(xfactors).astype(dtype))
+                    except:
+                        pass
             msg['x_selection'] = xfactors
         else:
             msg['x_selection'] = (x0, x1)
@@ -903,13 +904,14 @@ class SelectionXYCallback(BoundsCallback):
         if isinstance(y_range, FactorRange):
             y0, y1 = int(round(y0)), int(round(y1))
             yfactors = y_range.factors[y0: y1]
-            ydim = el.get_dimension(y_range.tags[0][0])
-            if hasattr(el, 'interface'):
-                dtype = el.interface.dtype(el, ydim)
-                try:
-                    yfactors = list(np.array(yfactors).astype(dtype))
-                except:
-                    pass
+            if y_range.tags and y_range.tags[0]:
+                ydim = el.get_dimension(y_range.tags[0][0])
+                if ydim and hasattr(el, 'interface'):
+                    dtype = el.interface.dtype(el, ydim)
+                    try:
+                        yfactors = list(np.array(yfactors).astype(dtype))
+                    except:
+                        pass
             msg['y_selection'] = yfactors
         else:
             msg['y_selection'] = (y0, y1)
