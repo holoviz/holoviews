839606d74dbc9a24bb480813cff0f86f669c1454
Philipp Rudiger <prudiger@anaconda.com>
2020-03-04 15:08:49 +0100

    Ensure partials are handled correctly as callback (#4259)

--- holoviews/core/spaces.py
+++ holoviews/core/spaces.py
@@ -875,8 +875,8 @@ class DynamicMap(HoloMap):
         streams = (streams or [])
 
         # If callback is a parameterized method and watch is disabled add as stream
-        if (util.is_param_method(callback, has_deps=True) and params.get('watch', True)
-            or isinstance(callback, FunctionType) and hasattr(callback, '_dinfo')):
+        if (params.get('watch', True) and (util.is_param_method(callback, has_deps=True) or
+            (isinstance(callback, FunctionType) and hasattr(callback, '_dinfo')))):
             streams.append(callback)
 
         if isinstance(callback, types.GeneratorType):
