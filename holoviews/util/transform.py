cb634cbcc3c8141e9c5f8078f38a8461870ed471
Philipp Rudiger <prudiger@anaconda.com>
2020-03-06 18:20:38 +0100

    Defer NumPy function calls to method on data

--- holoviews/util/transform.py
+++ holoviews/util/transform.py
@@ -486,6 +486,9 @@ class dim(object):
         for o in self.ops:
             args = o['args']
             fn = o['fn']
+            fn_name = self._numy_funcs.get(fn)
+            if fn_name and hasattr(data, fn_name):
+                fn = fn_name
             fn_args = [] if isinstance(fn, basestring) else [data]
             for arg in args:
                 if isinstance(arg, dim):
