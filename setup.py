fbd5953a99e99849c6cb64b4a9f542bc93c19e8e
Philipp Rudiger <prudiger@anaconda.com>
2020-03-11 16:26:36 +0100

    Pin latest conda-build

--- setup.py
+++ setup.py
@@ -51,7 +51,9 @@ extras_require['nbtests'] = extras_require['recommended'] + [
 extras_require['doc'] = extras_require['examples'] + [
     'nbsite>0.5.2', 'sphinx', 'sphinx_holoviz_theme', 'mpl_sample_data', 'awscli', 'pscript']
 
-extras_require['build'] = ['param >=1.7.0', 'setuptools >=30.3.0', 'pyct >=0.4.4', 'python <3.8']
+extras_require['build'] = [
+    'param >=1.7.0', 'setuptools >=30.3.0', 'pyct >=0.4.4', 'conda-build >=3.19'
+]
 
 # Everything including cyordereddict (optimization) and nosetests
 extras_require['all'] = list(set(extras_require['unit_tests']) | set(extras_require['nbtests']))
