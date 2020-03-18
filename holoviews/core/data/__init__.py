e862ea9d07daae3900c05cdb7d6e7bde432bb753
James A. Bednar <jbednar@users.noreply.github.com>
2020-03-18 16:14:20 -0500

    Update holoviews/core/data/__init__.py

--- holoviews/core/data/__init__.py
+++ holoviews/core/data/__init__.py
@@ -297,7 +297,7 @@ class Dataset(Element):
             extras = ', ' + extras if extras else ''
             apply_args= 'hv.{class_name}{extras}'.format(class_name=class_name,
                                                          extras=extras)
-            msg = "Cannot construct a {class_name} from the supplied object of type DynamicMap. Implicitly creating a DynamicMap of {class_name} objects, but instead please explicitly call  .apply({apply_args}) on the supplied DynamicMap."
+            msg = "Cannot construct a {class_name} from the supplied object of type DynamicMap. Implicitly creating a DynamicMap of {class_name} objects, but instead please explicitly call .apply({apply_args}) on the supplied DynamicMap."
             cls.warning(cls, msg.format(class_name=class_name, apply_args=apply_args))
             return data.apply(cls, per_element=True, kdims=kdims, vdims=vdims, **kwargs)
         else:
