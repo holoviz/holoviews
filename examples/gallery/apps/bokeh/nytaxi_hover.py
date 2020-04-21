2d9740589373b393dc1a3ff560d27e258343afc4
James A. Bednar <jbednar@continuum.io>
2020-04-21 17:16:37 -0500

    Enabled colorbar now that it works well

--- examples/gallery/apps/bokeh/nytaxi_hover.py
+++ examples/gallery/apps/bokeh/nytaxi_hover.py
@@ -30,7 +30,7 @@ renderer = hv.renderer('bokeh')
 opts.defaults(
     opts.Curve(xaxis=None, yaxis=None, show_grid=False, show_frame=False,
                color='orangered', framewise=True, width=100),
-    opts.Image(width=800, height=400, shared_axes=False, logz=True,
+    opts.Image(width=800, height=400, shared_axes=False, logz=True, colorbar=True,
                xaxis=None, yaxis=None, axiswise=True, bgcolor='black'),
     opts.HLine(color='white', line_width=1),
     opts.Layout(shared_axes=False),
