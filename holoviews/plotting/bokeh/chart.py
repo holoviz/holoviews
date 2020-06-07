4c59bcbfe2b65e88b756e19802f026b043240af0
Philipp Rudiger <prudiger@anaconda.com>
2020-06-07 23:12:36 +0200

    Fixed syntax error

--- holoviews/plotting/bokeh/chart.py
+++ holoviews/plotting/bokeh/chart.py
@@ -480,12 +480,12 @@ class SideHistogramPlot(HistogramPlot):
         dimension = color_dims[0] if color_dims else None
         cmapper = self._get_colormapper(dimension, element, {}, {})
         if cmapper and dimension in element.dimensions():
-            elif isinstance(dimension, dim):
+            if isinstance(dimension, dim):
                 dim_name = dimension.dimension.name
                 data[dim_name] = [] if self.static_source else dimension.apply(element)
             else:
-                dim_name = dim.name
-                data[dim.name] = [] if self.static_source else element.dimension_values(dimension)
+                dim_name = dimension.name
+                data[dim_name] = [] if self.static_source else element.dimension_values(dimension)
             mapping['fill_color'] = {'field': dim_name,
                                      'transform': cmapper}
         return (data, mapping, style)
