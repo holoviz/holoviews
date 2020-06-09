fb8c4263438b85602c70c6378f60fd15ed47c4e3
Philipp Rudiger <prudiger@anaconda.com>
2020-06-09 13:19:42 +0200

    Fix coloring of SideHistogram in bokeh (#4458)

--- holoviews/plotting/bokeh/element.py
+++ holoviews/plotting/bokeh/element.py
@@ -11,7 +11,9 @@ import bokeh.plotting
 
 from bokeh.core.properties import value
 from bokeh.document.events import ModelChangedEvent
-from bokeh.models import Renderer, Title, Legend, ColorBar, tools
+from bokeh.models import (
+    ColorBar, ColorMapper, Legend, Renderer, Title, tools
+)
 from bokeh.models.axes import CategoricalAxis, DatetimeAxis
 from bokeh.models.formatters import (
     FuncTickFormatter, TickFormatter, MercatorTickFormatter
@@ -1720,12 +1722,17 @@ class ColorbarPlot(ElementPlot):
 
         # Attempt to find matching colormapper on the adjoined plot
         if self.adjoined:
-            cmapper_name = dim_name+name
-            cmappers = self.adjoined.traverse(lambda x: (x.handles.get('color_dim'),
-                                                         x.handles.get(name, x.handles.get(cmapper_name))))
-            cmappers = [cmap for cdim, cmap in cmappers if cdim == eldim]
+            cmappers = self.adjoined.traverse(
+                lambda x: (x.handles.get('color_dim'),
+                           x.handles.get(name),
+                           [v for v in x.handles.values()
+                            if isinstance(v, ColorMapper)])
+                )
+            cmappers = [(cmap, mappers) for cdim, cmap, mappers in cmappers
+                        if cdim == eldim]
             if cmappers:
-                cmapper = cmappers[0]
+                cmapper, mappers  = cmappers[0]
+                cmapper = cmapper if cmapper else mappers[0]
                 self.handles['color_mapper'] = cmapper
                 return cmapper
             else:
