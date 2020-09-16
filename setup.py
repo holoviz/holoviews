e09581f2f21eba9be1a4010dca60dcd98383461f
Philipp Rudiger <prudiger@anaconda.com>
2020-09-16 11:16:08 -0400

    Fix doc building [doc-build] (#4614)
    * Rehash after install hoping it will discover firefox [doc-build]
    
    * Add firefox to travis build [doc-build]
    
    * Try to switch to chromedriver [doc-build]
    
    * Try different chromedriver [doc-build]
    
    * Start chrome headless [doc-build]
    
    * Install firefox [doc-build]
    
    * Removed chromedriver [doc-build]

--- setup.py
+++ setup.py
@@ -100,10 +100,7 @@ extras_require['doc'] = extras_require['examples'] + [
     'awscli',
     'pscript',
     'graphviz',
-    'selenium',
     'bokeh <2.2',
-    'firefox',
-    'geckodriver',
     'nbconvert <6.0',
     'mpl_sample_data'
 ]
