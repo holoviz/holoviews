21fec7f75199a1c36932dfd25411462dad3d286d
Jon Mease <jon.mease.dev@gmail.com>
2020-11-13 17:46:11 -0500

    Support link selections on dynamic overlays (e.g. tiles * datashade(...))

--- holoviews/selection.py
+++ holoviews/selection.py
@@ -164,6 +164,11 @@ class _base_link_selections(param.ParameterizedFunction):
                     self._selection_streams, hvobj, operations,
                     self._selection_expr_streams.get(hvobj, None), cache=self._cache
                 )
+            elif issubclass(hvobj.type, Overlay):
+                return Overlay([
+                    self._selection_transform(el, operations=operations)
+                    for el in callback.inputs
+                ]).collate()
             else:
                 # This is a DynamicMap that we don't know how to recurse into.
                 return hvobj
