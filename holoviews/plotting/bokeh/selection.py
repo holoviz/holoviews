397e747ca6ab962d730370ea619ea397536d5bf8
Philipp Rudiger <prudiger@anaconda.com>
2020-12-15 16:54:11 +0100

    Do not drop tools in linked selections

--- holoviews/plotting/bokeh/selection.py
+++ holoviews/plotting/bokeh/selection.py
@@ -30,6 +30,7 @@ class BokehOverlaySelectionDisplay(OverlaySelectionDisplay):
     def _build_element_layer(self, element, layer_color, layer_alpha, **opts):
         backend_options = Store.options(backend='bokeh')
         el_name = type(element).name
+        Store.lookup_options
         style_options = backend_options[(el_name,)]['style']
         allowed = style_options.allowed_keywords
 
@@ -52,7 +53,9 @@ class BokehOverlaySelectionDisplay(OverlaySelectionDisplay):
                 merged_opts[opt] = opts[opt]
 
         filtered = {k: v for k, v in merged_opts.items() if k in allowed}
-        return element.opts(backend='bokeh', clone=True, tools=['box_select'],
+        plot_opts = Store.lookup_options('bokeh', element, 'plot').kwargs
+        tools = plot_opts.get('tools', []) + ['box_select']
+        return element.opts(backend='bokeh', clone=True, tools=tools,
                             **filtered)
 
     def _style_region_element(self, region_element, unselected_color):
