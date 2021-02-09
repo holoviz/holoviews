45eda5747b9ccc5b8cf93f3f06c4d6464f3c8248
Philipp Rudiger <prudiger@anaconda.com>
2021-02-09 17:07:32 +0100

    Update holoviews/tests/plotting/bokeh/testoverlayplot.py

--- holoviews/tests/plotting/bokeh/testoverlayplot.py
+++ holoviews/tests/plotting/bokeh/testoverlayplot.py
@@ -230,7 +230,7 @@ class TestOverlayPlot(LoggingComparisonTestCase, TestBokehPlot):
     def test_overlay_muted_renderer(self):
         overlay = Curve((np.arange(5)), label='increase') * Curve((np.arange(5)*-1+5), label='decrease').opts(muted=True)
         plot = bokeh_renderer.get_plot(overlay)
-        unmuted, muted = plot.values()
+        unmuted, muted = plot.subplots.values()
         self.assertFalse(unmuted.handles['glyph_renderer'].muted)
         self.assertTrue(muted.handles['glyph_renderer'].muted)
 
