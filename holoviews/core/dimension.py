26648d743f3a8e95dee97bdf49df3b0b671e1f94
jlstevens <jlrstevens@gmail.com>
2021-02-22 15:58:15 +0100

    Removed __setstate__ from Dimension

--- holoviews/core/dimension.py
+++ holoviews/core/dimension.py
@@ -348,13 +348,6 @@ class Dimension(param.Parameterized):
         """
         return hash(self.spec)
 
-    def __setstate__(self, d):
-        """
-        Compatibility for pickles before alias attribute was introduced.
-        """
-        super(Dimension, self).__setstate__(d)
-        self.label = self.name
-
     def __eq__(self, other):
         "Implements equals operator including sanitized comparison."
 
