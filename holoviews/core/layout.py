a95aaf5215077ed1af72cbd0fc95d6fdfc5dd2a8
jlstevens <jlrstevens@gmail.com>
2021-03-19 17:26:47 +0100

    Formatting fixes and applied error message consistently

--- holoviews/core/layout.py
+++ holoviews/core/layout.py
@@ -26,8 +26,10 @@ class Composable(object):
     def __radd__(self, other):
         if isinstance(other, int):
             raise TypeError("unsupported operand type(s) for +: 'int' and 'Overlay'. "
-                            "If you are trying to use a reduction like `sum(elements)` to combine a list of elements,"
-                            "we recommend you to use `Layout(elements)` (and similarly Overlay(elements) for making an overlay from a list) instead.")
+                            "If you are trying to use a reduction like `sum(elements)` "
+                            "to combine a list of elements, we recommend you use "
+                            "`Layout(elements)` (and similarly `Overlay(elements)` for "
+                            "making an overlay from a list) instead.")
 
     def __lshift__(self, other):
         "Compose objects into an AdjointLayout"
@@ -317,8 +319,10 @@ class AdjointLayout(Dimensioned):
     def __radd__(self, other):
         if isinstance(other, int):
             raise TypeError("unsupported operand type(s) for +: 'int' and 'Overlay'. "
-                            "If you are using `sum(elements)` to combine a list of elements,"
-                            "we recommend you to use `Layout(elements)` instead.")
+                            "If you are trying to use a reduction like `sum(elements)` "
+                            "to combine a list of elements, we recommend you use "
+                            "`Layout(elements)` (and similarly `Overlay(elements)` for "
+                            "making an overlay from a list) instead.")
         return super(AdjointLayout, self).__radd__(self, other)
 
     def __len__(self):
@@ -393,8 +397,10 @@ class NdLayout(UniformNdMapping):
     def __radd__(self, other):
         if isinstance(other, int):
             raise TypeError("unsupported operand type(s) for +: 'int' and 'Overlay'. "
-                            "If you are using `sum(elements)` to combine a list of elements,"
-                            "we recommend you to use `Layout(elements)` instead.")
+                            "If you are trying to use a reduction like `sum(elements)` "
+                            "to combine a list of elements, we recommend you use "
+                            "`Layout(elements)` (and similarly `Overlay(elements)` for "
+                            "making an overlay from a list) instead.")
         return super(NdLayout, self).__radd__(self, other)
 
     @property
