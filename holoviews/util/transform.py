5ae78255ac96cea8459c558de66a7ca31d1f4563
jlstevens <jlrstevens@gmail.com>
2021-07-06 12:47:05 +0200

    Fixed flakes

--- holoviews/util/transform.py
+++ holoviews/util/transform.py
@@ -279,11 +279,11 @@ class dim(object):
         if ops and ops[-1]['kwargs'].get('accessor'):
             try:
                 ns = getattr(ns, ops[-1]['fn'])
-            except Exception as e:
+            except Exception:
                 # If the namespace doesn't know the method we are
                 # calling then we are using custom API of the dim
                 # transform itself, so set namespace to None
-                ns = None 
+                ns = None
         extras = {ns_attr for ns_attr in dir(ns) if not ns_attr.startswith('_')}
         if attr in extras and attr not in super(dim, self).__dir__():
             return type(self)(self, attr, accessor=True)
