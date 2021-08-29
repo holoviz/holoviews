ae02e4c38f47493072099c845ded1126e7fc3749
Yura52 <strausmg@gmail.com>
2021-08-29 18:31:00 +0300

    update sankey algorithm to d3-sankey-v0.12.3 (#4707)
    * update sankey algorithm to d3-sankey-v0.12.3
    
    * fix bugs and adjust tests
    
    * make flake8 happy on python 2.7
    
    * fix THE bug
    
    * convert the while loop back to the for loop
    
    * fix tests
    
    * explain the difference between this implementation and d3-sankey
    
    * change the comment
    
    * make the implementation closer to d3-sankey

--- holoviews/tests/plotting/matplotlib/test_sankey.py
+++ holoviews/tests/plotting/matplotlib/test_sankey.py
@@ -19,7 +19,7 @@ class TestSankeyPlot(TestMPLPlot):
         labels = plot.handles['labels']
 
         text_data = {'x': np.array([18.75, 18.75, 1003.75, 1003.75, 1003.75]),
-                     'y': np.array([145.454545, 395.454545,  48.787879, 229.090909, 430.30303]),
+                     'y': np.array([125.454545, 375.454545,  48.787879, 229.090909, 430.30303 ]),
                      'text': ['A - 18', 'B - 15', 'X - 7', 'Y - 16', 'Z - 10']}
         for i, text in enumerate(labels):
             self.assertEqual(text.xy[0], text_data['x'][i])
@@ -29,9 +29,8 @@ class TestSankeyPlot(TestMPLPlot):
         quad_data = {
             'x0': [0, 0, 985.0, 985.0, 985.0],
             'x1': [15, 15, 1000.0, 1000.0, 1000.0],
-            'y0': [19.999999999999588, 290.90909090909054, 2.842170943040401e-14,
-                   117.57575757575768, 360.60606060606057],
-            'y1': [270.90909090909054, 500.0, 97.57575757575768, 340.60606060606057, 500.0]
+            'y0': [0.0, 270.9090909090908, -7.105427357601002e-15, 117.57575757575756, 360.6060606060606],
+            'y1': [250.909091, 480.0, 97.575758, 340.606061, 500.0]
         }
         for i, rect in enumerate(rects.get_paths()):
             x0, x1, y0, y1 = [quad_data[c][i] for c in ('x0', 'x1', 'y0', 'y1')]
@@ -49,7 +48,7 @@ class TestSankeyPlot(TestMPLPlot):
         labels = plot.handles['labels']
 
         text_data = {'x': np.array([18.75, 18.75, 1003.75, 1003.75, 1003.75]),
-                     'y': np.array([145.454545, 395.454545,  48.787879, 229.090909, 430.30303]),
+                     'y': np.array([125.454545, 375.454545,  48.787879, 229.090909, 430.30303 ]),
                      'text': ['A - 18', 'B - 15', 'X - 7', 'Y - 16', 'Z - 10']}
         for i, text in enumerate(labels):
             self.assertEqual(text.xy[0], text_data['x'][i])
