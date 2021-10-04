Core/Util: Fix unhandled np.round overflow return
The function numpy.round return for the input
np.float64(2.6558061446181644e+295) the output
numpy.inf. This should lead to
density = full_precision_density.

holoviews/core/util.py
@@ -1965,7 +1965,7 @@ def bound_range(vals, density, time_unit='us'):
             warnings.filterwarnings('ignore', r'invalid value encountered in double_scalars')
             full_precision_density = compute_density(low, high, len(vals)-1)
             density = round(full_precision_density, sys.float_info.dig)
-        if density == 0:
+        if density == 0 or density == np.inf:
             density = full_precision_density
     if density == 0:
         raise ValueError('Could not determine Image density, ensure it has a non-zero range.')