52699e0bb239ed0a20905c56883dc9178e1ed80a
maximlt <mliquet@anaconda.com>
2021-12-07 09:00:26 +0100

    remove pytest usage

--- holoviews/tests/plotting/bokeh/testcallbacks.py
+++ holoviews/tests/plotting/bokeh/testcallbacks.py
@@ -4,7 +4,6 @@ from collections import deque, namedtuple
 from unittest import SkipTest
 
 import numpy as np
-import pytest
 
 from holoviews.core import DynamicMap, NdOverlay
 from holoviews.core.options import Store
@@ -466,7 +465,6 @@ class TestServerCallbacks(CallbackTestCase):
         self.assertEqual(resolved, {'id': cds.ref['id'],
                                     'value': points.columns()})
 
-    @pytest.mark.filterwarnings("ignore::FutureWarning")
     def test_rangexy_datetime(self):
         # Raises a warning because makeTimeDataFrame isn't part of the public API.
         curve = Curve(pd.util.testing.makeTimeDataFrame(), 'index', 'C')
