995578edf2857d648c6e82088af4529ac8dc57b7
maximlt <mliquet@anaconda.com>
2021-12-08 11:10:13 +0100

    move ibis-framework to the unit_tests extras

--- setup.py
+++ setup.py
@@ -48,14 +48,6 @@ extras_require["examples"] = extras_require["recommended"] + [
     "scikit-image"
 ]
 
-if sys.version_info.major > 2:
-    extras_require["examples"].extend(
-        [
-            "pyarrow",
-            "ibis-framework >=1.3",
-        ]  # spatialpandas incompatibility
-    )
-
 # Extra third-party libraries
 extras_require["extras"] = extras_require["examples"] + [
     "pscript ==0.7.1",
@@ -78,6 +70,19 @@ extras_require['tests'] = [
 ]
 
 extras_require["unit_tests"] = extras_require["examples"] + extras_require["tests"]
+
+# Moving ibis-framework from `examples` to `unit_tests`
+# because it could not be installed on Linux with conda-forge
+# (https://github.com/conda-forge/ibis-framework-feedstock/issues/54)
+# which was blocking the docs build. ibis-framework isn't
+# required to build the docs anyway.
+if sys.version_info.major > 2:
+    extras_require["unit_tests"].extend(
+        [
+            "pyarrow",
+            "ibis-framework >=1.3",
+        ]  # spatialpandas incompatibility
+    )
 
 extras_require["basic_tests"] = (
     extras_require["tests"]
