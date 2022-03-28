532b2f800b00cf5dbae5c59d71f195c0e427c50d
jlstevens <jlrstevens@gmail.com>
2022-03-28 18:40:46 +0200

    Made resource loading conditional on saml_cell_execution

--- holoviews/ipython/__init__.py
+++ holoviews/ipython/__init__.py
@@ -183,9 +183,10 @@ class notebook_extension(extension):
         if hasattr(config, 'comms') and comms:
             config.comms = comms
 
-        for r in [r for r in resources if r != 'holoviews']:
-            Store.renderers[r].load_nb(inline=p.inline)
-        Renderer.load_nb(inline=p.inline)
+        if not same_cell_execution:
+            for r in [r for r in resources if r != 'holoviews']:
+                Store.renderers[r].load_nb(inline=p.inline)
+            Renderer.load_nb(inline=p.inline)
 
         if hasattr(ip, 'kernel') and not loaded:
             Renderer.comm_manager.get_client_comm(notebook_extension._process_comm_msg,
