8c3c554010b7eca5a616dce5c2d933aba28887f0
maximlt <mliquet@anaconda.com>
2022-05-18 09:30:46 +0200

    drop Python 2.7

--- holoviews/core/util.py
+++ holoviews/core/util.py
@@ -21,12 +21,6 @@ from types import FunctionType
 import numpy as np
 import param
 
-# Python 2 builtins
-basestring = str
-long = int
-unicode = str
-cmp = lambda a, b: (a>b)-(a<b)
-
 get_keywords = operator.attrgetter('varkw')
 generator_types = (zip, range, types.GeneratorType)
 numpy_version = LooseVersion(np.__version__)
