09527020d6ddb13ba0c12db4ac813eed5a3de951
Philipp Rudiger <prudiger@anaconda.com>
2022-05-26 13:44:10 +0200

    Simplify mpl eq_hist implementation

--- holoviews/plotting/mpl/util.py
+++ holoviews/plotting/mpl/util.py
@@ -409,7 +409,7 @@ class EqHistNormalize(Normalize):
         self._nbins = nbins
         self._bin_edges = None
         self._ncolors = ncolors
-        self._color_bins = np.linspace(0, 1, ncolors)
+        self._color_bins = np.linspace(0, 1, ncolors+1)
 
     def binning(self, data, n=256):
         low = data.min() if self.vmin is None else self.vmin
@@ -417,39 +417,12 @@ class EqHistNormalize(Normalize):
         nbins = self._nbins
         eq_bin_edges = np.linspace(low, high, nbins+1)
         hist, _ = np.histogram(data, eq_bin_edges)
-
         eq_bin_centers = np.convolve(eq_bin_edges, [0.5, 0.5], mode='valid')
         cdf = np.cumsum(hist)
-        cdf_max = cdf[-1]
-        norm_cdf = cdf/cdf_max
-
-        # Iteratively find as many finite bins as there are colors
-        finite_bins = n-1
-        binning = []
-        iterations = 0
-        guess = n*2
-        while ((finite_bins != n) and (iterations < 4) and (finite_bins != 0)):
-            ratio = guess/finite_bins
-            if (ratio > 1000):
-                #Abort if distribution is extremely skewed
-                break
-            guess = np.round(max(n*ratio, n))
-
-            # Interpolate
-            palette_edges = np.arange(0, guess)
-            palette_cdf = norm_cdf*(guess-1)
-            binning = np.interp(palette_edges, palette_cdf, eq_bin_centers)
-
-            # Evaluate binning
-            uniq_bins = np.unique(binning)
-            finite_bins = len(uniq_bins)-1
-            iterations += 1
-        if (finite_bins == 0):
-            binning = [low]+[high]*(n-1)
-        else:
-            binning = binning[-n:]
-            if (finite_bins != n):
-                warnings.warn("EqHistColorMapper warning: Histogram equalization did not converge.")
+        cdf_bins = np.linspace(cdf[0], cdf[nbins-1], n+1)
+        binning = np.interp(cdf_bins, cdf, eq_bin_centers)
+        binning[0] = low
+        binning[-1] = high
         return binning
 
     def __call__(self, data, clip=None):
