dfa33e8f562e69fc692b598a44b6040fc792d00b
Simon Høxbro <simon.hansen@me.com>
2022-07-11 12:51:39 +0200

    Updated unittest

--- holoviews/tests/core/test_apply.py
+++ holoviews/tests/core/test_apply.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import param
 
-from panel.widgets import RadioButtonGroup, TextInput
+from panel.widgets import IntSlider, RadioButtonGroup, TextInput
 
 from holoviews import Dataset, util
 from holoviews.core.spaces import DynamicMap, HoloMap
@@ -292,3 +292,18 @@ def test_nested_widgets():
     df1 = transform.apply(ds, keep_index=True, compute=False)
     df2 = df.groupby(["D", "A"]).mean()
     pd.testing.assert_frame_equal(df1, df2)
+
+
+def test_slice_iloc():
+    df = pd._testing.makeDataFrame()
+    column = IntSlider(start=10, end=40)
+    ds = Dataset(df)
+    transform = util.transform.df_dim("*").iloc[:column].mean(axis=0)
+
+    params = list(transform.params.values())
+    assert len(params) == 1
+    assert params[0] == column.param.value
+
+    df1 = transform.apply(ds, keep_index=True, compute=False)
+    df2 = df.iloc[:10].mean(axis=0)
+    pd.testing.assert_series_equal(df1, df2)
