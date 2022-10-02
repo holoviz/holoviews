d097a77bcbba918a3d73546919c7b705910c235e
Philipp Rudiger <prudiger@anaconda.com>
2022-10-03 00:23:31 +0200

    Cleanup

--- holoviews/plotting/bokeh/callbacks.py
+++ holoviews/plotting/bokeh/callbacks.py
@@ -594,21 +594,24 @@ class RangeXYCallback(Callback):
         'y1': 'cb_obj.y1'
     }
 
+    _js_on_event = """
+    const plot = this.origin
+    const plots = plot.x_range.plots.concat(plot.y_range.plots)
+    const updated = [plot]
+    for (const p of plots) {
+      if (updated.indexOf(p) >= 0 || p.tags.indexOf('ranges-updating') >= 0)
+        continue
+      p.tags.push('ranges-updating')
+      const event = new this.constructor(p.x_range.start, p.x_range.end, p.y_range.start, p.y_range.end)
+      p.trigger_event(event)
+      p.tags = p.tags.filter(x => x !== 'ranges-updating')
+      updated.push(p)
+    }
+    """
+
     def set_callback(self, handle):
         super().set_callback(handle)
-        # Trigger rangesupdate events on all plots with linked axes.
-        handle.js_on_event('rangesupdate', CustomJS(code="""
-        const plot = this.origin
-        const plots = plot.x_range.plots.concat(plot.y_range.plots)
-        const updated = [plot]
-        for (const p of plots) {
-          if (updated.indexOf(p) >= 0 || p.tags.indexOf('ranges-updating') >= 0)
-            continue
-          p.tags.push('ranges-updating')
-          p.trigger_event(new this.constructor(p.x_range.start, p.x_range.end, p.y_range.start, p.y_range.end))
-          p.tags = p.tags.filter(x => x !== 'ranges-updating')
-        }
-        """))
+        handle.js_on_event('rangesupdate', CustomJS(code=self._js_on_event))
 
     def _process_msg(self, msg):
         data = {}
