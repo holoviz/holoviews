a9970bdeeb296dcf13fe07031b91ebefead4522b
Simon Høxbro <simon.hansen@me.com>
2022-10-06 13:12:59 +0200

    Add param to dirs to ignore

--- holoviews/core/data/pandas.py
+++ holoviews/core/data/pandas.py
@@ -19,18 +19,20 @@ from .util import finite_range
 def deprecation_warning(msg):
     "To only run the warning once"
 
-    # Finding the first stacklevel outside holoviews
+    # Finding the first stacklevel outside holoviews and param
     # Inspired by: pandas.util._exceptions.find_stack_level
     import holoviews as hv
+    import param
 
     pkg_dir = os.path.dirname(hv.__file__)
     test_dir = os.path.join(pkg_dir, "tests")
+    param_dir = os.path.dirname(param.__file__)
 
     frame = inspect.currentframe()
     stacklevel = 1
     while frame:
         fname = inspect.getfile(frame)
-        if fname.startswith(pkg_dir) and not fname.startswith(test_dir):
+        if (fname.startswith(pkg_dir) or fname.startswith(param_dir)) and not fname.startswith(test_dir):
             frame = frame.f_back
             stacklevel += 1
         else:
