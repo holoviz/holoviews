26be694b02c456e70f19392e2c9ab6ff8bcf1f7e
maximlt <mliquet@anaconda.com>
2022-10-29 14:50:16 +0200

    remove old opts tests

--- holoviews/tests/core/test_options.py
+++ holoviews/tests/core/test_options.py
@@ -607,30 +607,6 @@ class TestOptsMethod(ComparisonTestCase):
     def lookup_options(self, obj, group):
         return Store.lookup_options(self.backend, obj, group)
 
-    def test_old_opts_clone_disabled(self):
-        im = Image(np.random.rand(10,10))
-        styled_im = im.opts(style=dict(interpolation='nearest', cmap='jet'), clone=False)
-
-        self.assertEqual(self.lookup_options(im, 'plot').options, {})
-        self.assertEqual(self.lookup_options(styled_im, 'plot').options, {})
-
-        assert styled_im is im
-        self.assertEqual(self.lookup_options(im, 'style').options,
-                         {'cmap': 'jet', 'interpolation': 'nearest'})
-
-    def test_old_opts_clone_enabled(self):
-        im = Image(np.random.rand(10,10))
-        styled_im = im.opts(style=dict(interpolation='nearest', cmap='jet'), clone=True)
-
-        self.assertEqual(self.lookup_options(im, 'plot').options, {})
-        self.assertEqual(self.lookup_options(styled_im, 'plot').options, {})
-
-        assert styled_im is not im
-        im_lookup = self.lookup_options(im, 'style').options
-        self.assertEqual(im_lookup['cmap'] == 'jet', False)
-        styled_im_lookup =  self.lookup_options(styled_im, 'style').options
-        self.assertEqual(styled_im_lookup['cmap'] == 'jet', True)
-
     def test_simple_clone_disabled(self):
         im = Image(np.random.rand(10,10))
         styled_im = im.opts(interpolation='nearest', cmap='jet', clone=False)
