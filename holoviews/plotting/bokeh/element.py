60311d04264c9aaaf367ada59d1bb0bc544fc4ac
Philipp Rudiger <prudiger@anaconda.com>
2023-01-10 11:02:38 +0100

    Enable rendering colorbars on bokeh GraphPlots (#5585)

--- holoviews/plotting/bokeh/element.py
+++ holoviews/plotting/bokeh/element.py
@@ -1604,7 +1604,7 @@ class CompositeElementPlot(ElementPlot):
             for k, v in list(self.handles.items()):
                 if not k.endswith('color_mapper'):
                     continue
-                self._draw_colorbar(plot, v, k[:-12])
+                self._draw_colorbar(plot, v, k.replace('color_mapper', ''))
 
 
     def _process_properties(self, key, properties, mapping):
@@ -2013,7 +2013,7 @@ class ColorbarPlot(ElementPlot):
             for k, v in list(self.handles.items()):
                 if not k.endswith('color_mapper'):
                     continue
-                self._draw_colorbar(plot, v, k[:-12])
+                self._draw_colorbar(plot, v, k.replace('color_mapper', ''))
         return ret
 
 
