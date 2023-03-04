70acda8164fdafe17c0c4583feb259b4453ad158
jrudolph <jrudolph@anl.gov>
2023-03-04 16:39:51 -0600

    Added unit tests for "regular" Bokeh `QuadMesh` plots.
    This unit tests checks that any extra vdims provided when instantiating a `QuadMesh` with 1-D coordinate arrays are correctly passed to the Bokeh `ColumnDataSource` for use in hover labels. This feature is fully supported and functioning properly in HoloViews 1.15.4 (so the test should pass), unlike for 2-D coordinates wherein any extra vdims are lost.

--- holoviews/tests/plotting/bokeh/test_quadmeshplot.py
+++ holoviews/tests/plotting/bokeh/test_quadmeshplot.py
@@ -66,6 +66,27 @@ class TestQuadMeshPlot(TestBokehPlot):
         source = plot.handles['source']
         self.assertEqual(source.data['z'], flattened)
 
+    def test_quadmesh_regular_centers(self):
+        X = [0.5, 1.5]
+        Y = [0.5, 1.5, 2.5]
+        Z = np.array([[1., 2., 3.], [4., 5., np.nan]]).T
+        LABELS = np.array([['0-0', '0-1', '0-2'], ['1-0', '1-1', '1-2']]).T
+        qmesh = QuadMesh((X, Y, Z, LABELS), vdims=['Value', 'Label'])
+        plot = bokeh_renderer.get_plot(qmesh.opts(tools=['hover']))
+        source = plot.handles['source']
+        expected = {
+                'left': [0., 0., 0., 1., 1., 1.],
+                'right': [1., 1., 1., 2., 2., 2.],
+                'Value': [ 1.,  2.,  3.,  4.,  5., np.nan],
+                'bottom': [0., 1., 2., 0., 1., 2.],
+                'top': [1., 2., 3., 1., 2., 3.],
+                'Label': ['0-0', '0-1', '0-2', '1-0', '1-1', '1-2'],
+                'x': [0.5, 0.5, 0.5, 1.5, 1.5, 1.5],
+                'y': [0.5, 1.5, 2.5, 0.5, 1.5, 2.5]}
+        self.assertEqual(source.data.keys(), expected.keys())
+        for key in expected:
+            self.assertEqual(list(source.data[key]), expected[key])
+
     def test_quadmesh_irregular_centers(self):
         X = [[0.5, 0.5, 0.5], [1.5, 1.5, 1.5]]
         Y = [[0.5, 1.5, 2.5], [0.5, 1.5, 2.5]]
