3d5bfe640f21424f3bcb923109052ea26c190d11
Simon Høxbro Hansen <simon.hansen@me.com>
2023-04-08 17:58:39 +0200

    Ignore xarray examples for pandas 2

--- examples/conftest.py
+++ examples/conftest.py
@@ -3,18 +3,19 @@ import sys
 import pandas as pd
 from packaging.version import Version
 
+
 collect_ignore_glob = [
     # Needs selenium, phantomjs, firefox, and geckodriver to save a png picture
     "user_guide/Plotting_with_Bokeh.ipynb",
     # Possible timeout error
     "user_guide/17-Dashboards.ipynb",
-    # Give file not found here.
+    # Give file not found
     "user_guide/Plots_and_Renderers.ipynb",
 ]
 
-if sys.version_info[:2] == (3, 11):
+if sys.version_info >= (3, 11):
     collect_ignore_glob += [
-        # numba not supported on Python 3.11
+        # Numba is released on Python 3.11
         "user_guide/15-Large_Data.ipynb",
         "user_guide/16-Streaming_Data.ipynb",
         "user_guide/Network_Graphs.ipynb",
@@ -24,7 +25,12 @@ if sys.version_info[:2] == (3, 11):
 if Version(pd.__version__) >= Version("2.0"):
     collect_ignore_glob += [
         # Xarray incompatibility: https://github.com/pydata/xarray/issues/7716
-        # "user_guide",
+        "user_guide/09-Gridded_Datasets.ipynb",
+        "user_guide/11-Transforming_Elements.ipynb",
+        "user_guide/15-Large_Data.ipynb",
+        "user_guide/16-Streaming_Data.ipynb",
+        "user_guide/Network_Graphs.ipynb",
+        "user_guide/Linked_Brushing.ipynb",
         # Pandas bug: https://github.com/pandas-dev/pandas/issues/52451
         "reference/elements/*/TriMesh.ipynb",
         "gallery/demos/bokeh/point_draw_triangulate.ipynb",
