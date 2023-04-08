c4551134a6e33596efcb2b9eac0ca575b27554c4
Simon Høxbro Hansen <simon.hansen@me.com>
2023-04-08 18:40:03 +0200

    Only ignore pandas bug on windows

--- examples/conftest.py
+++ examples/conftest.py
@@ -3,6 +3,7 @@ import sys
 import pandas as pd
 from packaging.version import Version
 
+PD2 = Version(pd.__version__) >= Version("2.0")
 
 collect_ignore_glob = [
     # Needs selenium, phantomjs, firefox, and geckodriver to save a png picture
@@ -13,25 +14,29 @@ collect_ignore_glob = [
     "user_guide/Plots_and_Renderers.ipynb",
 ]
 
+# Numba incompatibility
 if sys.version_info >= (3, 11):
     collect_ignore_glob += [
-        # Numba is released on Python 3.11
         "user_guide/15-Large_Data.ipynb",
         "user_guide/16-Streaming_Data.ipynb",
-        "user_guide/Network_Graphs.ipynb",
         "user_guide/Linked_Brushing.ipynb",
+        "user_guide/Network_Graphs.ipynb",
     ]
 
-if Version(pd.__version__) >= Version("2.0"):
+# Xarray incompatibility: https://github.com/pydata/xarray/issues/7716
+if PD2:
     collect_ignore_glob += [
-        # Xarray incompatibility: https://github.com/pydata/xarray/issues/7716
         "user_guide/09-Gridded_Datasets.ipynb",
         "user_guide/11-Transforming_Elements.ipynb",
         "user_guide/15-Large_Data.ipynb",
         "user_guide/16-Streaming_Data.ipynb",
-        "user_guide/Network_Graphs.ipynb",
         "user_guide/Linked_Brushing.ipynb",
-        # Pandas bug: https://github.com/pandas-dev/pandas/issues/52451
-        "reference/elements/*/TriMesh.ipynb",
+        "user_guide/Network_Graphs.ipynb",
+    ]
+
+# Pandas bug: https://github.com/pandas-dev/pandas/issues/52451
+if PD2 and sys.platform == "win32":
+    collect_ignore_glob += [
         "gallery/demos/bokeh/point_draw_triangulate.ipynb",
+        "reference/elements/*/TriMesh.ipynb",
     ]
