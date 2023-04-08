77ed9afec8a6f5d35e46194d761120b311e0d8a4
Simon Høxbro Hansen <simon.hansen@me.com>
2023-04-08 17:09:57 +0200

    Better path in conftest

--- examples/conftest.py
+++ examples/conftest.py
@@ -4,27 +4,28 @@ import pandas as pd
 from packaging.version import Version
 
 collect_ignore_glob = [
-    "Plotting_with_Bokeh",  # needs selenium, phantomjs, firefox, and geckodriver to save a png picture.
-    "17-Dashboards",  # can give a timeout error.
-    "Plots_and_Renderers",  # give file not found here.
+    # Needs selenium, phantomjs, firefox, and geckodriver to save a png picture
+    "user_guide/Plotting_with_Bokeh.ipynb",
+    # Possible timeout error
+    "user_guide/17-Dashboards.ipynb",
+    # Give file not found here.
+    "user_guide/Plots_and_Renderers.ipynb",
 ]
 
-# Add sys.version for numba
-if sys.version_info == (3, 11):
+if sys.version_info[:2] == (3, 11):
     collect_ignore_glob += [
         # numba not supported on Python 3.11
-        "15-Large_Data",
-        "16-Streaming_Data",
-        "17-Dashboards",
-        "Network_Graphs",
-        "Linked_Brushing",
+        "user_guide/15-Large_Data.ipynb",
+        "user_guide/16-Streaming_Data.ipynb",
+        "user_guide/Network_Graphs.ipynb",
+        "user_guide/Linked_Brushing.ipynb",
     ]
 
 if Version(pd.__version__) >= Version("2.0"):
     collect_ignore_glob += [
         # Xarray incompatibility: https://github.com/pydata/xarray/issues/7716
-        "user_guide",
+        # "user_guide",
         # Pandas bug: https://github.com/pandas-dev/pandas/issues/52451
         "reference/elements/*/TriMesh.ipynb",
-        "point_draw_triangulate",
+        "gallery/demos/bokeh/point_draw_triangulate.ipynb",
     ]
