63152c5eb7ce6bf82f6829fd84ef4ed82c799bf9
Simon Høxbro Hansen <simon.hansen@me.com>
2023-04-25 09:56:45 +0200

    Remove asyncio

--- holoviews/tests/conftest.py
+++ holoviews/tests/conftest.py
@@ -1,18 +1,6 @@
-import asyncio
-
 import pytest
 
 from panel.io import state
-
-# Tornado 6.2 has deprecated the use of IOLoop.current,
-# when no asyncio event loop is running.
-# This will start an event loop.
-try:
-    asyncio.get_running_loop()
-except RuntimeError:
-    # Create a new asyncio event loop for this thread.
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
 
 
 @pytest.fixture(autouse=True)
