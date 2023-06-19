3eb87c76b5dbeb38c79c84b342623b3578882f34
Simon Høxbro Hansen <simon.hansen@me.com>
2023-06-19 14:39:55 +0200

    Add codereview

--- holoviews/tests/conftest.py
+++ holoviews/tests/conftest.py
@@ -1,4 +1,1 @@
-from panel.tests.conftest import server_cleanup, port, pytest_addoption, pytest_collection_modifyitems  # noqa
-
-
-CUSTOM_MARKS = ('ui',)
+from panel.tests.conftest import server_cleanup, port, pytest_addoption, pytest_collection_modifyitems, pytest_configure  # noqa
