bcfdbacd8cb0029cd3ae5e832490d75562266f2a
Simon Høxbro Hansen <simon.hansen@me.com>
2023-06-20 09:40:49 +0200

    Add custom selection

--- holoviews/tests/conftest.py
+++ holoviews/tests/conftest.py
@@ -1,1 +1,19 @@
-from panel.tests.conftest import server_cleanup, port, pytest_addoption, pytest_collection_modifyitems, pytest_configure  # noqa
+from panel.tests.conftest import server_cleanup, port, pytest_addoption, pytest_configure, optional_markers  # noqa
+
+
+def pytest_collection_modifyitems(config, items):
+    skipped, selected = [], []
+    markers = [m for m in optional_markers if config.getoption(f"--{m}")]
+    empty = not markers
+    for item in items:
+        if empty and any(m in item.keywords for m in optional_markers):
+            skipped.append(item)
+        elif empty:
+            selected.append(item)
+        elif not empty and any(m in item.keywords for m in markers):
+            selected.append(item)
+        else:
+            skipped.append(item)
+
+    config.hook.pytest_deselected(items=skipped)
+    items[:] = selected
