fd0554039734ecaca62eb275b32514a7cc20b638
Simon Høxbro Hansen <simon.hansen@me.com>
2023-07-06 14:28:58 +0200

    Update tests to be bokeh2/bokeh3 specific

--- holoviews/tests/plotting/bokeh/test_rasterplot.py
+++ holoviews/tests/plotting/bokeh/test_rasterplot.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from holoviews.element import Raster, Image, RGB
+from holoviews.plotting.bokeh.util import bokeh3
 
 from .test_plot import TestBokehPlot, bokeh_renderer
 
@@ -46,11 +47,19 @@ class TestRasterPlot(TestBokehPlot):
         raster = Raster(arr).opts(invert_axes=True)
         plot = bokeh_renderer.get_plot(raster)
         source = plot.handles['source']
-        self.assertEqual(source.data['image'][0], np.rot90(arr))
-        self.assertEqual(source.data['x'][0], 0)
-        self.assertEqual(source.data['y'][0], 3)
-        self.assertEqual(source.data['dw'][0], 2)
-        self.assertEqual(source.data['dh'][0], 3)
+
+        if bokeh3:
+            self.assertEqual(source.data['image'][0], arr.T)
+            self.assertEqual(source.data['x'][0], 0)
+            self.assertEqual(source.data['y'][0], 0)
+            self.assertEqual(source.data['dw'][0], 2)
+            self.assertEqual(source.data['dh'][0], 3)
+        else:
+            self.assertEqual(source.data['image'][0], np.rot90(arr))
+            self.assertEqual(source.data['x'][0], 0)
+            self.assertEqual(source.data['y'][0], 3)
+            self.assertEqual(source.data['dw'][0], 2)
+            self.assertEqual(source.data['dh'][0], 3)
 
     def test_image_invert_axes(self):
         arr = np.array([[0, 1, 2], [3, 4,  5]])
@@ -71,11 +80,16 @@ class TestRasterPlot(TestBokehPlot):
         self.assertEqual(x_range.start, 0.5)
         self.assertEqual(x_range.end, -0.5)
         cdata = plot.handles['source'].data
-        self.assertEqual(cdata['x'], [0.5])
         self.assertEqual(cdata['y'], [-0.5])
         self.assertEqual(cdata['dh'], [1.0])
         self.assertEqual(cdata['dw'], [1.0])
-        self.assertEqual(cdata['image'][0], arr[::-1, ::-1])
+
+        if bokeh3:
+            self.assertEqual(cdata['x'], [-0.5])
+            self.assertEqual(cdata['image'][0], arr[::-1])
+        else:
+            self.assertEqual(cdata['x'], [0.5])
+            self.assertEqual(cdata['image'][0], arr[::-1, ::-1])
 
     def test_image_invert_yaxis(self):
         arr = np.random.rand(10, 10)
@@ -86,10 +100,15 @@ class TestRasterPlot(TestBokehPlot):
         self.assertEqual(y_range.end, -0.5)
         cdata = plot.handles['source'].data
         self.assertEqual(cdata['x'], [-0.5])
-        self.assertEqual(cdata['y'], [0.5])
         self.assertEqual(cdata['dh'], [1.0])
         self.assertEqual(cdata['dw'], [1.0])
-        self.assertEqual(cdata['image'][0], arr)
+
+        if bokeh3:
+            self.assertEqual(cdata['y'], [-0.5])
+            self.assertEqual(cdata['image'][0], arr[::-1])
+        else:
+            self.assertEqual(cdata['y'], [0.5])
+            self.assertEqual(cdata['image'][0], arr)
 
     def test_rgb_invert_xaxis(self):
         rgb = RGB(np.random.rand(10, 10, 3)).opts(invert_xaxis=True)
@@ -98,10 +117,14 @@ class TestRasterPlot(TestBokehPlot):
         self.assertEqual(x_range.start, 0.5)
         self.assertEqual(x_range.end, -0.5)
         cdata = plot.handles['source'].data
-        self.assertEqual(cdata['x'], [0.5])
         self.assertEqual(cdata['y'], [-0.5])
         self.assertEqual(cdata['dh'], [1.0])
         self.assertEqual(cdata['dw'], [1.0])
+
+        if bokeh3:
+            self.assertEqual(cdata['x'], [-0.5])
+        else:
+            self.assertEqual(cdata['x'], [0.5])
 
     def test_rgb_invert_yaxis(self):
         rgb = RGB(np.random.rand(10, 10, 3)).opts(invert_yaxis=True)
@@ -111,6 +134,10 @@ class TestRasterPlot(TestBokehPlot):
         self.assertEqual(y_range.end, -0.5)
         cdata = plot.handles['source'].data
         self.assertEqual(cdata['x'], [-0.5])
-        self.assertEqual(cdata['y'], [0.5])
         self.assertEqual(cdata['dh'], [1.0])
         self.assertEqual(cdata['dw'], [1.0])
+
+        if bokeh3:
+            self.assertEqual(cdata['y'], [-0.5])
+        else:
+            self.assertEqual(cdata['y'], [0.5])
