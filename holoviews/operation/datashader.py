96902884d58a9a3e7aee1d29627f4d7754456e7f
Simon Høxbro Hansen <simon.hansen@me.com>
2023-07-18 15:11:07 +0200

    Small cleanup

--- holoviews/operation/datashader.py
+++ holoviews/operation/datashader.py
@@ -164,6 +164,8 @@ class AggregationOperation(ResampleOperation2D):
             col = agg_fn.column if not isinstance(agg_fn.column, rd.SpecialColumn) else agg_fn.selector.column
             vdims = sorted(params["vdims"], key=lambda x: x != col)
             # TODO: Should we add prefix to all of the where columns.
+        elif agg_name == "Summary":
+            vdims = list(agg_fn.keys)
         elif column:
             dims = [d for d in element.dimensions('ranges') if d == column]
             if not dims:
@@ -185,8 +187,6 @@ class AggregationOperation(ResampleOperation2D):
                 vdims.nodata = 0
         else:
             vdims = Dimension(f'{vdim_prefix}{agg_name}', label=agg_name, nodata=0)
-        if agg_name == "Summary":
-            vdims = list(agg_fn.keys)
 
         params['vdims'] = vdims
         return params
@@ -363,9 +363,9 @@ class aggregate(LineAggregationOperation):
                 params["vdims"] = [params["vdims"]]
             sum_agg = ds.summary(**{str(params["vdims"][0]): agg_fn, "index": ds.where(sel_fn)})
             agg = self._apply_datashader(dfdata, cvs_fn, sum_agg, agg_kwargs, x, y)
-            # First sel is index, which we don't care
             sel_prefix = f"{type(sel_fn).__name__}_{sel_fn.column}"
-            sel_vdims = [Dimension(s, label=f"{sel_prefix} {s}") for s in list(agg)[1:] if s not in params["vdims"]]
+            _ignore = [*params["vdims"], "index"]
+            sel_vdims = [Dimension(s, label=f"{sel_prefix} {s}") for s in agg if s not in _ignore]
             params["vdims"] = [*params["vdims"], *sel_vdims]
         else:
             agg = self._apply_datashader(dfdata, cvs_fn, agg_fn, agg_kwargs, x, y)
