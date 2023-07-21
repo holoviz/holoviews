d48950a8c6853648798564cbfb0a58c6bc3aaae0
Simon Høxbro Hansen <simon.hansen@me.com>
2023-07-21 17:35:29 +0200

    Disable Dask's conversion of strings in test  (#5819)

--- holoviews/tests/conftest.py
+++ holoviews/tests/conftest.py
@@ -17,3 +17,12 @@ def pytest_collection_modifyitems(config, items):
 
     config.hook.pytest_deselected(items=skipped)
     items[:] = selected
+
+
+try:
+    # From Dask 2023.7,1 they now automatic convert strings
+    # https://docs.dask.org/en/stable/changelog.html#v2023-7-1
+    import dask
+    dask.config.set({"dataframe.convert-string": False})
+except Exception:
+    pass
