ec579192c9cf33adec6c1f2bc9897a22a59afcf7
Simon Høxbro Hansen <simon.hansen@me.com>
2023-07-21 12:05:38 +0200

    Move to conftest

--- holoviews/tests/core/data/__init__.py
+++
@@ -1,7 +0,0 @@
-try:
-    # From Dask 2023.7,1 they now automatic convert strings
-    # https://docs.dask.org/en/stable/changelog.html#v2023-7-1
-    import dask
-    dask.config.set({"dataframe.convert-string": False})
-except Exception:
-    pass
