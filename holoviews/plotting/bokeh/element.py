69f16b974770131b976bff6f507bb77990d75ffa
Philipp Rudiger <prudiger@anaconda.com>
2023-07-24 22:27:04 +0200

    Skip unnecessary updates to extra ranges

--- holoviews/plotting/bokeh/element.py
+++ holoviews/plotting/bokeh/element.py
@@ -927,6 +927,7 @@ class ElementPlot(BokehPlot, GenericElementPlot):
     def _update_ranges(self, element, ranges):
         x_range = self.handles['x_range']
         y_range = self.handles['y_range']
+        plot = self.handles['plot']
 
         self._update_main_ranges(element, x_range, y_range, ranges)
 
@@ -938,6 +939,10 @@ class ElementPlot(BokehPlot, GenericElementPlot):
             factors = self._get_dimension_factors(element, ranges, axis_dim)
             extra_scale = self.handles[f'extra_{multi_dim}_scales'][axis_dim] # Assumes scales and ranges zip
             log = isinstance(extra_scale, LogScale)
+            range_update = ((not (self.model_changed(extra_y_range) or self.model_changed(plot))
+                             and self.framewise))
+            if self.drawn and not range_update:
+                continue
             self._update_range(
                 extra_y_range, b, t, factors,
                 extra_y_range.tags[1]['invert_yaxis'] if extra_y_range.tags else False,
