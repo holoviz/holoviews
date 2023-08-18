e4d19ecb32998df5757bcc78b997f02e5d3514f1
Simon Høxbro Hansen <simon.hansen@me.com>
2023-08-18 08:22:31 +0200

    Fix bare self.assertRaises

--- holoviews/tests/plotting/matplotlib/test_graphplot.py
+++ holoviews/tests/plotting/matplotlib/test_graphplot.py
@@ -206,7 +206,8 @@ class TestMplGraphPlot(TestMPLPlot):
 
         if Version(mpl.__version__) < Version("3.4.0"):
             # Python 3.6 only support up to matplotlib 3.3
-            with self.assertRaises(Exception):
+            msg = 'TypeError: alpha must be a float or None'
+            with pytest.raises(AbbreviatedException, match=msg):
                 mpl_renderer.get_plot(graph)
         else:
             plot = mpl_renderer.get_plot(graph)
