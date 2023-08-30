e0b1facfb80080979a8d50e85fb5dbf731b3093c
Simon Høxbro Hansen <simon.hansen@me.com>
2023-08-30 10:11:26 +0200

    Align handling of new dimension keyword

--- holoviews/plotting/bokeh/element.py
+++ holoviews/plotting/bokeh/element.py
@@ -420,14 +420,11 @@ class ElementPlot(BokehPlot, GenericElementPlot):
             axis_label = str(dim)
             specs = ((dim.name, dim.label, dim.unit),)
         else:
-            if isinstance(self, OverlayPlot):
-                try:
-                    l, b, r, t = self.get_extents(range_el, ranges, dimension=dim)
-                except TypeError:
-                    # GeoViews ProjectionPlot.get_extents() does not support dimension
-                    # before 1.10.2
-                    l, b, r, t = self.get_extents(range_el, ranges)
-            else:
+            try:
+                l, b, r, t = self.get_extents(range_el, ranges, dimension=dim)
+            except TypeError:
+                # Backward compatibility for e.g. GeoViews=<1.10.1 since dimension
+                # is a newly added keyword argument in HoloViews 1.17
                 l, b, r, t = self.get_extents(range_el, ranges)
             if self.invert_axes:
                 l, b, r, t = b, l, t, r
@@ -988,9 +985,9 @@ class ElementPlot(BokehPlot, GenericElementPlot):
                 range_dim = None
             try:
                 l, b, r, t = self.get_extents(element, ranges, dimension=range_dim)
-            except Exception:
+            except TypeError:
                 # Backward compatibility for e.g. GeoViews=<1.10.1 since dimension
-                # is a newly added keyword argument
+                # is a newly added keyword argument in HoloViews 1.17
                 l, b, r, t = self.get_extents(element, ranges)
             if self.invert_axes:
                 l, b, r, t = b, l, t, r
