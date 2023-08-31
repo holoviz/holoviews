6ed22d1e8874237c61120cf5a20a7deabf178eee
Markéta <meggy.calabkova@gmail.com>
2023-08-31 11:56:10 +0200

    support python3-only systems

--- holoviews/tests/util/test_init.py
+++ holoviews/tests/util/test_init.py
@@ -14,6 +14,6 @@ def test_no_blocklist_imports():
         print(", ".join(mods), end="")
         """
 
-    output = check_output(['python', '-c', dedent(check)])
+    output = check_output(['python3', '-c', dedent(check)])
 
     assert output == b""
