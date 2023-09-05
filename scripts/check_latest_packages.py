9b87fc14a8e5e64264664a88e24874aa90ba21dc
Simon Høxbro Hansen <simon.hansen@me.com>
2023-09-05 11:56:50 +0200

    Update with current release date

--- scripts/check_latest_packages.py
+++ scripts/check_latest_packages.py
@@ -13,13 +13,18 @@ def main(*packages):
         resp = requests.get(url).json()
         latest = resp["info"]["version"]
         current = __import__(package).__version__
-        release_date = datetime.fromisoformat(
+        latest_release_date = datetime.fromisoformat(
             resp["releases"][latest][0]["upload_time_iso_8601"]
-        )
+        ).date()
+        current_release_date = datetime.fromisoformat(
+            resp["releases"][current][0]["upload_time_iso_8601"]
+        ).date()
         version_check = Version(current) >= Version(latest)
-        date_check = release_date.date() >= allowed_date
+        date_check = latest_release_date >= allowed_date
         is_latest &= version_check and date_check
-        print(f"Package: {package:<10} Current: {current:<10} Latest: {latest:<10}")
+        print(
+            f"Package: {package:<10} Current: {current:<7} ({current_release_date})\tLatest: {latest:<7} ({latest_release_date})"
+        )
 
     if not is_latest:
         sys.exit(1)
