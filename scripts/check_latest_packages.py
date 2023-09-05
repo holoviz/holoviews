e6cb0d38f79541d6a4b115dee02ad4e43712ddf0
Simon Høxbro Hansen <simon.hansen@me.com>
2023-09-05 12:10:42 +0200

    sort packages

--- scripts/check_latest_packages.py
+++ scripts/check_latest_packages.py
@@ -8,20 +8,23 @@ from packaging.version import Version
 def main(*packages):
     allowed_date = date.today() - timedelta(days=2)
     is_latest = True
-    for package in packages:
+    for package in sorted(packages):
         url = f"https://pypi.org/pypi/{package}/json"
         resp = requests.get(url).json()
         latest = resp["info"]["version"]
         current = __import__(package).__version__
+
         latest_release_date = datetime.fromisoformat(
             resp["releases"][latest][0]["upload_time_iso_8601"]
         ).date()
         current_release_date = datetime.fromisoformat(
             resp["releases"][current][0]["upload_time_iso_8601"]
         ).date()
+
         version_check = Version(current) >= Version(latest)
         date_check = allowed_date >= latest_release_date
         is_latest &= version_check and date_check
+
         print(
             f"Package: {package:<10} Current: {current:<7} ({current_release_date})\tLatest: {latest:<7} ({latest_release_date})"
         )
