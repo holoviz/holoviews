8adf8b192f488d46eb9e4420117c961e140d4f04
Simon Høxbro Hansen <simon.hansen@me.com>
2023-09-05 11:51:41 +0200

    Add extra time after release date

--- scripts/check_latest_packages.py
+++ scripts/check_latest_packages.py
@@ -1,18 +1,25 @@
 import sys
 import requests
+from datetime import datetime, date, timedelta
 
 from packaging.version import Version
 
 
 def main(*packages):
+    allowed_date = date.today() - timedelta(days=2)
     is_latest = True
     for package in packages:
         url = f"https://pypi.org/pypi/{package}/json"
         resp = requests.get(url).json()
-        latest = Version(resp["info"]["version"])
-        current = Version(__import__(package).__version__)
-        is_latest &= current >= latest
-        print(f"Package: {package:<10} Current: {current!s:<10} Latest: {latest!s:<10}")
+        latest = resp["info"]["version"]
+        current = __import__(package).__version__
+        release_date = datetime.fromisoformat(
+            resp["releases"][latest][0]["upload_time_iso_8601"]
+        )
+        version_check = Version(current) >= Version(latest)
+        date_check = release_date.date() >= allowed_date
+        is_latest &= version_check and date_check
+        print(f"Package: {package:<10} Current: {current:<10} Latest: {latest:<10}")
 
     if not is_latest:
         sys.exit(1)
