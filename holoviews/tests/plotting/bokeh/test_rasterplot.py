228d2ef10a7c753bb68b21fb3c9c1fc12cc21c1a
Andrew Huang <ahuang@anaconda.com>
2023-09-13 11:36:02 -0700

    Add missing self

--- holoviews/tests/plotting/bokeh/test_rasterplot.py
+++ holoviews/tests/plotting/bokeh/test_rasterplot.py
@@ -14,7 +14,7 @@ class TestRasterPlot(TestBokehPlot):
         img = Image(np.random.rand(10, 10)).opts(logz=True)
         self._test_colormapping(img, 2, True)
 
-    def test_image_boolean_array():
+    def test_image_boolean_array(self):
         img = Image(np.array([[True, False], [False, True]]))
         plot = bokeh_renderer.get_plot(img)
         cmapper = plot.handles["color_mapper"]
@@ -23,7 +23,7 @@ class TestRasterPlot(TestBokehPlot):
         assert cmapper.high == 1
         assert np.array_equal(source.data["image"][0], np.array([[0, 1], [1, 0]]))
 
-    def test_nodata_array():
+    def test_nodata_array(self):
         img = Image(np.array([[0, 1], [2, 0]])).opts(nodata=0)
         plot = bokeh_renderer.get_plot(img)
         cmapper = plot.handles["color_mapper"]
@@ -34,7 +34,7 @@ class TestRasterPlot(TestBokehPlot):
             source.data["image"][0], np.array([[2, np.NaN], [np.NaN, 1]])
         )
 
-    def test_nodata_array_uint():
+    def test_nodata_array_uint(self):
         img = Image(np.array([[0, 1], [2, 0]], dtype="uint32")).opts(nodata=0)
         plot = bokeh_renderer.get_plot(img)
         cmapper = plot.handles["color_mapper"]
@@ -45,7 +45,7 @@ class TestRasterPlot(TestBokehPlot):
             source.data["image"][0], np.array([[2, np.NaN], [np.NaN, 1]])
         )
 
-    def test_nodata_rgb():
+    def test_nodata_rgb(self):
         N = 2
         rgb_d = np.linspace(0, 1, N * N * 3).reshape(N, N, 3)
         rgb = RGB(rgb_d).redim.nodata(R=0)
@@ -53,7 +53,7 @@ class TestRasterPlot(TestBokehPlot):
         image_data = plot.handles["source"].data["image"][0]
         assert (image_data == 0).sum() == 1
 
-    def test_raster_invert_axes():
+    def test_raster_invert_axes(self):
         arr = np.array([[0, 1, 2], [3, 4, 5]])
         raster = Raster(arr).opts(invert_axes=True)
         plot = bokeh_renderer.get_plot(raster)
@@ -72,7 +72,7 @@ class TestRasterPlot(TestBokehPlot):
             assert source.data["dw"][0] == 2
             assert source.data["dh"][0] == 3
 
-    def test_image_invert_axes():
+    def test_image_invert_axes(self):
         arr = np.array([[0, 1, 2], [3, 4, 5]])
         raster = Image(arr).opts(invert_axes=True)
         plot = bokeh_renderer.get_plot(raster)
@@ -83,7 +83,7 @@ class TestRasterPlot(TestBokehPlot):
         assert source.data["dw"][0] == 1
         assert source.data["dh"][0] == 1
 
-    def test_image_invert_xaxis():
+    def test_image_invert_xaxis(self):
         arr = np.random.rand(10, 10)
         img = Image(arr).opts(invert_xaxis=True)
         plot = bokeh_renderer.get_plot(img)
@@ -102,7 +102,7 @@ class TestRasterPlot(TestBokehPlot):
             assert cdata["x"] == [0.5]
             assert np.array_equal(cdata["image"][0], arr[::-1, ::-1])
 
-    def test_image_invert_yaxis():
+    def test_image_invert_yaxis(self):
         arr = np.random.rand(10, 10)
         img = Image(arr).opts(invert_yaxis=True)
         plot = bokeh_renderer.get_plot(img)
@@ -121,7 +121,7 @@ class TestRasterPlot(TestBokehPlot):
             assert cdata["y"] == [0.5]
             assert np.array_equal(cdata["image"][0], arr)
 
-    def test_rgb_invert_xaxis():
+    def test_rgb_invert_xaxis(self):
         rgb = RGB(np.random.rand(10, 10, 3)).opts(invert_xaxis=True)
         plot = bokeh_renderer.get_plot(rgb)
         x_range = plot.handles["x_range"]
@@ -137,7 +137,7 @@ class TestRasterPlot(TestBokehPlot):
         else:
             assert cdata["x"] == [0.5]
 
-    def test_rgb_invert_yaxis():
+    def test_rgb_invert_yaxis(self):
         rgb = RGB(np.random.rand(10, 10, 3)).opts(invert_yaxis=True)
         plot = bokeh_renderer.get_plot(rgb)
         y_range = plot.handles["y_range"]
@@ -153,7 +153,7 @@ class TestRasterPlot(TestBokehPlot):
         else:
             assert cdata["y"] == [0.5]
 
-    def test_image_stack_tuple():
+    def test_image_stack_tuple(self):
         x = np.arange(0, 3)
         y = np.arange(5, 8)
         a = np.array([[np.nan, np.nan, 1], [np.nan] * 3, [np.nan] * 3])
@@ -172,7 +172,7 @@ class TestRasterPlot(TestBokehPlot):
         assert source.data["dh"][0] == 3
         assert isinstance(plot, ImageStackPlot)
 
-    def test_image_stack_tuple_unspecified_dims():
+    def test_image_stack_tuple_unspecified_dims(self):
         x = np.arange(0, 3)
         y = np.arange(5, 8)
         a = np.array([[np.nan, np.nan, 1], [np.nan] * 3, [np.nan] * 3])
@@ -192,7 +192,7 @@ class TestRasterPlot(TestBokehPlot):
         assert source.data["dh"][0] == 3
         assert isinstance(plot, ImageStackPlot)
 
-    def test_image_stack_dict():
+    def test_image_stack_dict(self):
         x = np.arange(0, 3)
         y = np.arange(5, 8)
         a = np.array([[np.nan, np.nan, 1], [np.nan] * 3, [np.nan] * 3])
@@ -212,7 +212,7 @@ class TestRasterPlot(TestBokehPlot):
         assert source.data["dh"][0] == 3
         assert isinstance(plot, ImageStackPlot)
 
-    def test_image_stack_dict_unspecified_dims():
+    def test_image_stack_dict_unspecified_dims(self):
         x = np.arange(0, 3)
         y = np.arange(5, 8)
         a = np.array([[np.nan, np.nan, 1], [np.nan] * 3, [np.nan] * 3])
@@ -232,7 +232,7 @@ class TestRasterPlot(TestBokehPlot):
         assert source.data["dh"][0] == 3
         assert isinstance(plot, ImageStackPlot)
 
-    def test_image_stack_xarray_dataset():
+    def test_image_stack_xarray_dataset(self):
         try:
             import xarray as xr
         except ImportError:
@@ -261,7 +261,7 @@ class TestRasterPlot(TestBokehPlot):
         assert source.data["dh"][0] == 3
         assert isinstance(plot, ImageStackPlot)
 
-    def test_image_stack_xarray_dataarray():
+    def test_image_stack_xarray_dataarray(self):
         try:
             import xarray as xr
         except ImportError:
@@ -290,7 +290,7 @@ class TestRasterPlot(TestBokehPlot):
         assert source.data["dh"][0] == 3
         assert isinstance(plot, ImageStackPlot)
 
-    def test_image_stack_invert_xaxis():
+    def test_image_stack_invert_xaxis(self):
         x = np.arange(0, 3)
         y = np.arange(5, 8)
         a = np.array([[np.nan, np.nan, 1], [np.nan] * 3, [np.nan] * 3])
@@ -308,7 +308,7 @@ class TestRasterPlot(TestBokehPlot):
         assert source.data["dw"][0] == 3
         assert source.data["dh"][0] == 3
 
-    def test_image_stack_invert_yaxis():
+    def test_image_stack_invert_yaxis(self):
         x = np.arange(0, 3)
         y = np.arange(5, 8)
         a = np.array([[np.nan, np.nan, 1], [np.nan] * 3, [np.nan] * 3])
@@ -326,7 +326,7 @@ class TestRasterPlot(TestBokehPlot):
         assert source.data["dw"][0] == 3
         assert source.data["dh"][0] == 3
 
-    def test_image_stack_invert_axes():
+    def test_image_stack_invert_axes(self):
         x = np.arange(0, 3)
         y = np.arange(5, 8)
         a = np.array([[np.nan, np.nan, 1], [np.nan] * 3, [np.nan] * 3])
