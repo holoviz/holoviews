794743a8e5a26860d5c6921b32a19c61486e4fde
Simon Høxbro Hansen <simon.hansen@me.com>
2023-09-14 12:50:45 +0200

    Add tests to Colorbar

--- holoviews/tests/plotting/bokeh/test_annotationplot.py
+++ holoviews/tests/plotting/bokeh/test_annotationplot.py
@@ -2,7 +2,7 @@ import unittest
 
 import numpy as np
 
-
+import holoviews as hv
 from holoviews.element import (
     HLine, VLine, Text, Labels, Arrow, HSpan, VSpan, Slope,
     HLines, VLines, HSpans, VSpans,
@@ -357,6 +357,21 @@ class TestHVLinesPlot(TestBokehPlot):
         assert plot.handles["y_range"].start == 0
         assert plot.handles["y_range"].end == 5.5
 
+    def test_coloring_hline(self):
+        hlines = HLines({"y": [1, 2, 3]})
+        hlines = hlines.opts(
+            alpha=hv.dim("y").norm(),
+            line_color="red",
+            line_dash=hv.dim("y").bin([0, 1.5, 3], ["dashed", "solid"]),
+        )
+
+        plot = hv.renderer("bokeh").get_plot(hlines)
+        assert plot.handles["glyph"].line_color == "red"
+
+        data = plot.handles["glyph_renderer"].data_source.data
+        np.testing.assert_allclose(data["alpha"], [0, 0.5, 1])
+        assert data["line_dash"] == ["dashed", "solid", "solid"]
+
 
 class TestHVSpansPlot(TestBokehPlot):
 
@@ -498,3 +513,17 @@ class TestHVSpansPlot(TestBokehPlot):
         assert plot.handles["x_range"].end == 6.5
         assert plot.handles["y_range"].start == 0
         assert plot.handles["y_range"].end == 6.5
+
+    def test_coloring_hline(self):
+        hspans = HSpans({"y0": [1, 3, 5], "y1": [2, 4, 6]}).opts(
+            alpha=hv.dim("y0").norm(),
+            line_color="red",
+            line_dash=hv.dim("y1").bin([0, 3, 6], ["dashed", "solid"]),
+        )
+
+        plot = hv.renderer("bokeh").get_plot(hspans)
+        assert plot.handles["glyph"].line_color == "red"
+
+        data = plot.handles["glyph_renderer"].data_source.data
+        np.testing.assert_allclose(data["alpha"], [0, 0.5, 1])
+        assert data["line_dash"] == ["dashed", "solid", "solid"]
