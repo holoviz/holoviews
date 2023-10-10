827b6f16fc672175fc8eac9d512a679f665daa12
Simon Høxbro Hansen <simon.hansen@me.com>
2023-10-10 12:51:00 +0200

    Add Python 3.12 in setup.py and tox.ini

--- setup.py
+++ setup.py
@@ -184,6 +184,7 @@ setup_args.update(
             "Programming Language :: Python :: 3.9",
             "Programming Language :: Python :: 3.10",
             "Programming Language :: Python :: 3.11",
+            "Programming Language :: Python :: 3.12",
             "Operating System :: OS Independent",
             "Intended Audience :: Science/Research",
             "Intended Audience :: Developers",
