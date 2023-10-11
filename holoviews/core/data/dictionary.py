8c9ce6d8af388890de5bff719891e906f0c80c68
Simon Høxbro Hansen <simon.hansen@me.com>
2023-10-11 12:50:06 +0200

    Fix OrderedDict regression (#5925)

--- holoviews/core/data/dictionary.py
+++ holoviews/core/data/dictionary.py
@@ -1,4 +1,4 @@
-from collections import defaultdict
+from collections import OrderedDict, defaultdict
 
 import numpy as np
 
@@ -18,7 +18,7 @@ class DictInterface(Interface):
     are collections representing the values in that column.
     """
 
-    types = (dict,)
+    types = (dict, OrderedDict)
 
     datatype = 'dictionary'
 
@@ -109,10 +109,11 @@ class DictInterface(Interface):
 
         if not cls.expanded([vs for d, vs in unpacked if d in dimensions and not isscalar(vs)]):
             raise ValueError('DictInterface expects data to be of uniform shape.')
-        if isinstance(data, dict):
+        # OrderedDict can't be replaced with dict: https://github.com/holoviz/holoviews/pull/5925
+        if isinstance(data, OrderedDict):
             data.update(unpacked)
         else:
-            data = dict(unpacked)
+            data = OrderedDict(unpacked)
 
         return data, {'kdims':kdims, 'vdims':vdims}, {}
 
