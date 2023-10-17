9fc52af620b4f18c95c427b04eaae2fc5341b0cd
Simon Høxbro Hansen <simon.hansen@me.com>
2023-10-17 12:06:57 +0200

    More flexible inputs for ImageStack data

--- holoviews/element/raster.py
+++ holoviews/element/raster.py
@@ -530,6 +530,15 @@ class ImageStack(Image):
     _vdim_reductions = {1: Image}
 
     def __init__(self, data, kdims=None, vdims=None, **params):
+        if isinstance(data, np.ndarray) and data.ndim == 3:
+            x = np.arange(data.shape[0])
+            y = np.arange(data.shape[1])
+            data = (x, y, *(data[:, :, n] for n in range(data.shape[2])))
+        elif (
+            isinstance(data, tuple) and len(data) == 3
+            and isinstance(data[2], np.ndarray) and data[2].ndim == 3
+        ):
+            data = (data[0], data[1], *(data[2][:, :,n] for n in range(data[2].shape[2])))
         if vdims is None:
             if isinstance(data, tuple):
                 vdims = [Dimension(f"level_{i}") for i in range(len(data[2:]))]
