83d6cb06fca0de745ad124e713e94e9cf357c060
Simon Høxbro Hansen <simon.hansen@me.com>
2023-10-27 09:34:13 +0200

    Use root parameter in TestFileArchive

--- holoviews/tests/core/test_archives.py
+++ holoviews/tests/core/test_archives.py
@@ -23,33 +23,34 @@ class TestFileArchive:
         FileArchive()
 
     def test_filearchive_image_pickle(self, tmp_path):
-        export_name = os.fspath(tmp_path / 'archive_image')
+        export_name = 'archive_image'
         filenames = ['Group1-Im1.hvz', 'Group2-Im2.hvz']
-        archive = FileArchive(export_name=export_name, pack=False)
+        archive = FileArchive(root=os.fspath(tmp_path), export_name=export_name, pack=False)
         archive.add(self.image1)
         archive.add(self.image2)
         assert len(archive) == 2
         assert archive.listing() == filenames
         archive.export()
-        assert os.path.isdir(export_name), f"No directory {str(export_name)!r} created on export."
-        assert sorted(filenames) == sorted(os.listdir(export_name))
+        assert os.path.isdir(tmp_path / export_name), f"No directory {str(export_name)!r} created on export."
+        assert sorted(filenames) == sorted(os.listdir(tmp_path / export_name))
         assert archive.listing() == []
 
     def test_filearchive_image_pickle_zip(self, tmp_path):
-        export_name = os.fspath(tmp_path / 'archive_image')
+        export_name = 'archive_image'
         filenames = ['Group1-Im1.hvz', 'Group2-Im2.hvz']
-        archive = FileArchive(export_name=export_name,
+        archive = FileArchive(root=os.fspath(tmp_path), export_name=export_name,
                               pack=True, archive_format='zip')
         archive.add(self.image1)
         archive.add(self.image2)
         assert len(archive) == 2
         assert archive.listing() == filenames
         archive.export()
-        if not os.path.isfile(export_name+'.zip'):
+        export_folder = os.fspath(tmp_path / export_name) + '.zip'
+        if not os.path.isfile(export_folder):
             raise AssertionError(f"No zip file {export_name!r} created on export.")
 
         namelist = [f'{export_name}/{f}' for f in filenames]
-        with zipfile.ZipFile(export_name+'.zip', 'r') as f:
+        with zipfile.ZipFile(export_folder, 'r') as f:
             expected = sorted(map(os.path.abspath, namelist))
             output = sorted(map(os.path.abspath, f.namelist()))
             assert expected == output
@@ -57,62 +58,63 @@ class TestFileArchive:
 
 
     def test_filearchive_image_pickle_tar(self, tmp_path):
-        export_name = os.fspath(tmp_path / 'archive_image')
+        export_name = 'archive_image'
         filenames = ['Group1-Im1.hvz', 'Group2-Im2.hvz']
-        archive = FileArchive(export_name=export_name,
+        archive = FileArchive(root=os.fspath(tmp_path), export_name=export_name,
                               pack=True, archive_format='tar')
         archive.add(self.image1)
         archive.add(self.image2)
         assert len(archive) == 2
         assert archive.listing() == filenames
         archive.export()
-        if not os.path.isfile(export_name+'.tar'):
+        export_folder = os.fspath(tmp_path / export_name) + '.tar'
+        if not os.path.isfile(export_folder):
             raise AssertionError(f"No tar file {export_name!r} created on export.")
 
         namelist = [f'{export_name}/{f}' for f in filenames]
-        with tarfile.TarFile(export_name+'.tar', 'r') as f:
+        with tarfile.TarFile(export_folder, 'r') as f:
             assert sorted(namelist) == sorted([el.path for el in f.getmembers()])
         assert archive.listing() == []
 
 
     def test_filearchive_image_serialize(self, tmp_path):
-        export_name = os.fspath(tmp_path / 'archive_image_serizalize')
+        export_name = 'archive_image_serizalize'
         filenames = ['Group1-Im1.pkl', 'Group2-Im2.pkl']
-        archive = FileArchive(export_name=export_name, exporters=[Serializer], pack=False)
+        archive = FileArchive(root=os.fspath(tmp_path), export_name=export_name, exporters=[Serializer], pack=False)
         archive.add(self.image1)
         archive.add(self.image2)
         assert len(archive) == 2
         assert archive.listing() == filenames
         archive.export()
-        if not os.path.isdir(export_name):
+        if not os.path.isdir(tmp_path / export_name):
             raise AssertionError(f"No directory {export_name!r} created on export.")
-        assert sorted(filenames) == sorted(os.listdir(export_name))
+        assert sorted(filenames) == sorted(os.listdir(tmp_path / export_name))
         assert archive.listing() == []
 
     def test_filearchive_image_pickle_name_clash(self, tmp_path):
-        export_name = os.fspath(tmp_path / 'archive_image_test_clash')
+        export_name = 'archive_image_test_clash'
         filenames = ['Group1-Im1.hvz', 'Group1-Im1-1.hvz']
-        archive = FileArchive(export_name=export_name, pack=False)
+        archive = FileArchive(root=os.fspath(tmp_path), export_name=export_name, pack=False)
         archive.add(self.image1)
         archive.add(self.image1)
         assert len(archive) == 2
         assert archive.listing() == filenames
         archive.export()
-        if not os.path.isdir(export_name):
+        if not os.path.isdir(tmp_path / export_name):
             raise AssertionError(f"No directory {export_name!r} created on export.")
-        assert sorted(filenames) == sorted(os.listdir(export_name))
+        assert sorted(filenames) == sorted(os.listdir(tmp_path / export_name))
         assert archive.listing() == []
 
     def test_filearchive_json_single_file(self, tmp_path):
-        export_name = os.fspath(tmp_path / "archive_json")
+        export_name = "archive_json"
         data = {'meta':'test'}
-        archive = FileArchive(export_name=export_name, pack=False)
+        archive = FileArchive(root=os.fspath(tmp_path), export_name=export_name, pack=False)
         archive.add(filename='metadata.json', data=json.dumps(data),
                     info={'mime_type':'text/json'})
         assert len(archive) == 1
         assert archive.listing() == ['metadata.json']
         archive.export()
-        fname = f"{export_name}_metadata.json"
+        fname = os.fspath(tmp_path / f"{export_name}_metadata.json")
         if not os.path.isfile(fname):
             raise AssertionError(f"No file {fname!r} created on export.")
         assert json.load(open(fname)) == data
@@ -124,8 +126,8 @@ class TestFileArchive:
     """
     def test_filearchive_clear_file(self, tmp_path):
         export_name = "archive_for_clear"
-        export_name = os.fspath(tmp_path / "archive_for_clear")
-        archive = FileArchive(export_name=export_name, pack=False)
+        export_name = "archive_for_clear"
+        archive = FileArchive(root=os.fspath(tmp_path), export_name=export_name, pack=False)
         archive.add(self.image1)
         archive.add(self.image2)
         archive.clear()
