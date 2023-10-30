9167a011271b8415b0622409bc604000cf248412
Simon Høxbro Hansen <simon.hansen@me.com>
2023-10-30 14:26:27 +0100

    Update test

--- holoviews/element/tiles.py
+++ holoviews/element/tiles.py
@@ -85,13 +85,13 @@ _ATTRIBUTIONS = {
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, '
         '&copy; <a href="https://cartodb.com/attributions">CartoDB</a>'
     ),
-    ('stamen', 'net/t') : ( # to match both 'toner' and 'terrain'
+    ('stamen', 'png') : ( # to match both 'toner' and 'terrain'
         'Map tiles by <a href="https://stamen.com">Stamen Design</a> / <a href="https://stadiamaps.com">Stadia Maps</a>, '
         'under <a href="https://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. '
         'Data by <a href="https://openstreetmap.org">OpenStreetMap</a>, '
         'under <a href="https://www.openstreetmap.org/copyright">ODbL</a>.'
     ),
-    ('stamen', 'watercolor') : (
+    ('stamen', 'jpg') : (  # watercolor
         'Map tiles by <a href="https://stamen.com">Stamen Design</a> / <a href="https://stadiamaps.com">Stadia Maps</a>, '
         'under <a href="https://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. '
         'Data by <a href="https://openstreetmap.org">OpenStreetMap</a>, '
