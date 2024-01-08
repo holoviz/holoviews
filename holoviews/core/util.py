f05b69e7a5ec8f832fe3e15d3e220eba72e07e21
Theom <49269671+TheoMathurin@users.noreply.github.com>
2024-01-08 12:19:51 +0100

    Ensure partial methods can be used as dmap callables

--- holoviews/core/util.py
+++ holoviews/core/util.py
@@ -445,6 +445,10 @@ def argspec(callable_obj):
         arglen = len(callable_obj.args)
         spec = inspect.getfullargspec(callable_obj.func)
         args = [arg for arg in spec.args[arglen:] if arg not in callable_obj.keywords]
+        while isinstance(callable_obj, partial):
+            callable_obj = callable_obj.func
+        if inspect.ismethod(callable_obj):
+            args = args[1:]
     elif inspect.ismethod(callable_obj):    # instance and class methods
         spec = inspect.getfullargspec(callable_obj)
         args = spec.args[1:]
