3e98f58369185967a6ff95aafdf71a3b71bf7d68
Simon Høxbro Hansen <simon.hansen@me.com>
2024-03-04 08:59:59 +0100

    Always color in Github Actions

--- scripts/check_latest_packages.py
+++ scripts/check_latest_packages.py
@@ -1,3 +1,4 @@
+import os
 import sys
 from datetime import date, datetime, timedelta
 from importlib.metadata import version
@@ -5,10 +6,11 @@ from importlib.metadata import version
 import requests
 from packaging.version import Version
 
-if sys.stdout.isatty():
+if sys.stdout.isatty() or os.environ.get("GITHUB_ACTIONS"):
     GREEN, RED, RESET = "\033[92m", "\033[91m", "\033[0m"
 else:
     GREEN = RED = RESET = ""
+
 
 def main(*packages):
     allowed_date = date.today() - timedelta(days=5)
