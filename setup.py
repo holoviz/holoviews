e31d1ba00167d472916738cbd5919d2af6c512c0
Simon Høxbro Hansen <simon.hansen@me.com>
2024-03-13 19:00:17 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ extras_require['tests_core'] = [
 # to run and pass the test suite without installing any
 # of those.
 extras_require['tests'] = extras_require['tests_core'] + [
-    'dask <2023.3',
+    'dask <2024.3',
     'ibis-framework',  # Mapped to ibis-sqlite in setup.cfg for conda
     'xarray >=0.10.4',
     'networkx',
