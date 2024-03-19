235f09df6b236f5cc6d6c863653ba610f3150efa
Simon Høxbro Hansen <simon.hansen@me.com>
2024-03-19 08:57:08 +0100

    Force vdims to strings for ImageStack

--- holoviews/operation/datashader.py
+++ holoviews/operation/datashader.py
@@ -406,7 +406,7 @@ class aggregate(LineAggregationOperation):
             eldata = agg if ds_version > Version('0.5.0') else (xs, ys, agg.data)
             return self.p.element_type(eldata, **params)
         else:
-            params['vdims'] = list(agg.coords[agg_fn.column].data)
+            params['vdims'] = list(map(str, agg.coords[agg_fn.column].data))
             return ImageStack(agg, **params)
 
     def _apply_datashader(self, dfdata, cvs_fn, agg_fn, agg_kwargs, x, y):
