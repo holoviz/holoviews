3d4473787401580d52436e86daa664f726874303
Simon Høxbro Hansen <simon.hansen@me.com>
2024-04-05 09:55:36 +0200

    Add tmp pin on panel

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ extras_require['tests_core'] = [
     'plotly >=4.0',
     'ipython >=5.4.0',
     'contourpy',
+    'panel ==1.4.0',   # REMOVE BEFORE MERGE
 ]
 
 # Optional tests dependencies, i.e. one should be able
