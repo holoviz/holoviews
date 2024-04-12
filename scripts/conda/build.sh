d3853558d8c5d54cc6b0f1297cb7996c71ea332a
Simon Høxbro Hansen <simon.hansen@me.com>
2024-04-12 11:52:50 +0200

    Update conda build

--- scripts/conda/build.sh
+++ scripts/conda/build.sh
@@ -4,11 +4,10 @@ set -euxo pipefail
 
 git status
 
-export SETUPTOOLS_ENABLE_FEATURES="legacy-editable"
 python -m build -w .
 
 git diff --exit-code
 
 VERSION=$(find dist -name "*.whl" -exec basename {} \; | cut -d- -f2)
 export VERSION
-conda build conda.recipe/ --no-anaconda-upload --no-verify
+conda build scripts/conda/recipe --no-anaconda-upload --no-verify
