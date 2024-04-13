629ae8b8039e66267ffd6866473ccf47ea7d106d
Simon Høxbro Hansen <simon.hansen@me.com>
2024-04-13 10:16:24 +0200

    Simplify build-conda

--- scripts/conda/build.sh
+++ scripts/conda/build.sh
@@ -2,12 +2,22 @@
 
 set -euxo pipefail
 
-git status
+PACKAGE="holoviews"
 
+for file in dist/*.whl dist/*.tar.bz2; do
+    if [ -e "$file" ]; then
+        echo "dist folder already contains $(basename "$file"). Please delete it before running this script."
+        exit 1
+    fi
+done
+
+git diff --exit-code
 python -m build -w .
-
 git diff --exit-code
 
 VERSION=$(find dist -name "*.whl" -exec basename {} \; | cut -d- -f2)
 export VERSION
 conda build scripts/conda/recipe --no-anaconda-upload --no-verify
+
+mkdir -p dist
+mv "$CONDA_PREFIX/conda-bld/noarch/$PACKAGE-$VERSION-py_0.tar.bz2" dist
