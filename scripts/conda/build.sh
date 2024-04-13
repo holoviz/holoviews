821fd40f3274bc2ec26ce7a4179add0a9aef53b2
Simon Høxbro Hansen <simon.hansen@me.com>
2024-04-13 16:46:42 +0200

    Small changes

--- scripts/conda/build.sh
+++ scripts/conda/build.sh
@@ -13,11 +13,9 @@ done
 
 git diff --exit-code
 python -m build .
-git diff --exit-code
 
 VERSION=$(find dist -name "*.whl" -exec basename {} \; | cut -d- -f2)
 export VERSION
 conda build scripts/conda/recipe --no-anaconda-upload --no-verify
 
-mkdir -p dist
 mv "$CONDA_PREFIX/conda-bld/noarch/$PACKAGE-$VERSION-py_0.tar.bz2" dist
