828ec3d0e2df6b32df8501fd40ba9147d803f0ae
Demetris Roumis <roumis.d@gmail.com>
2024-04-16 11:18:54 -0700

    import param directly

--- holoviews/tests/plotting/bokeh/test_elementplot.py
+++ holoviews/tests/plotting/bokeh/test_elementplot.py
@@ -3,6 +3,7 @@ from unittest import SkipTest
 
 import numpy as np
 import panel as pn
+import param
 import pytest
 from bokeh.document import Document
 from bokeh.models import (
@@ -21,7 +22,7 @@ from holoviews.core import DynamicMap, HoloMap, NdOverlay, Overlay
 from holoviews.core.util import dt_to_int
 from holoviews.element import Curve, HeatMap, Image, Labels, Scatter
 from holoviews.plotting.util import process_cmap
-from holoviews.streams import PointDraw, Stream, param
+from holoviews.streams import PointDraw, Stream
 from holoviews.util import render
 
 from ...utils import LoggingComparisonTestCase
