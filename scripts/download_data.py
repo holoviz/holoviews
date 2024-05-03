b1fe1cf06e8cffa101dca7b06c96cf09b2169aab
Simon Høxbro Hansen <simon.hansen@me.com>
2024-05-03 07:48:07 -0700

    Update documentation and small Pixi improvements (#6200)

--- scripts/download_data.py
+++ scripts/download_data.py
@@ -1,13 +1,13 @@
+from contextlib import suppress
+
 import bokeh.sampledata
 
 bokeh.sampledata.download()
 
-try:
+with suppress(ImportError):
     import pooch  # noqa: F401
     import scipy  # noqa: F401
     import xarray as xr
-except ImportError:
-    pass
-else:
+
     xr.tutorial.open_dataset("air_temperature")
     xr.tutorial.open_dataset("rasm")
