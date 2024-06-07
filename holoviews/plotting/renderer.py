226b07b6d4fd9ae3a41e1e667cdc258fd3f6a0b1
Simon Høxbro Hansen <simon.hansen@me.com>
2024-06-07 16:12:09 +0200

    Fix colab

--- holoviews/plotting/renderer.py
+++ holoviews/plotting/renderer.py
@@ -433,6 +433,8 @@ class Renderer(Exporter):
             from IPython.display import display
             display(data, raw=True)
             return {'text/html': '<div style="display: none"></div>'}, {}
+        elif config.comms == 'colab':
+            load_notebook(config.inline)
         return data, {}
 
     def static_html(self, obj, fmt=None, template=None):
