19bcee820ed98378b2ac77983cddfbd1c5fda390
maximlt <mliquet@anaconda.com>
2024-06-28 18:32:00 +0200

    change implementation

--- holoviews/core/data/pandas.py
+++ holoviews/core/data/pandas.py
@@ -1,5 +1,3 @@
-from itertools import chain
-
 import numpy as np
 import pandas as pd
 from packaging.version import Version
@@ -74,7 +72,7 @@ class PandasInterface(Interface, PandasAPI):
 
             if kdims and not (len(kdims) == len(index_names) and {dimension_name(kd) for kd in kdims} == set(index_names)):
                 kdim = dimension_name(kdims[0])
-                if eltype._auto_indexable_1d and ncols == 1 and kdim not in chain(data.columns, cls.indexes(data)):
+                if eltype._auto_indexable_1d and ncols == 1 and kdim not in [*data.columns, *cls.indexes(data)]:
                     data = data.copy()
                     data.insert(0, kdim, np.arange(len(data)))
 
