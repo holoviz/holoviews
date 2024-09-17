077af4e7e4808ddc1395ea493a407e5b85bc7db4
Philipp Rudiger <prudiger@anaconda.com>
2024-09-17 15:07:31 +0200

    Add test

--- holoviews/tests/ui/bokeh/test_callback.py
+++ holoviews/tests/ui/bokeh/test_callback.py
@@ -5,7 +5,7 @@ import pytest
 import holoviews as hv
 from holoviews import Curve, DynamicMap, Scatter
 from holoviews.plotting.bokeh.util import bokeh34
-from holoviews.streams import BoundsX, BoundsXY, BoundsY, Lasso, RangeXY
+from holoviews.streams import BoundsX, BoundsXY, BoundsY, Lasso, MultiAxisTap, RangeXY
 
 from .. import expect, wait_until
 
@@ -161,6 +161,28 @@ def test_multi_axis_rangexy(serve_hv):
         np.testing.assert_almost_equal(s1.x_range, expected_xrange) and
         np.testing.assert_almost_equal(s1.y_range, expected_yrange1) and
         np.testing.assert_almost_equal(s2.y_range, expected_yrange2)
+    ), page)
+
+
+@pytest.mark.usefixtures("bokeh_backend")
+def test_multi_axis_tap(serve_hv):
+    c1 = Curve(np.arange(10).cumsum(), vdims='y1')
+    c2 = Curve(np.arange(20).cumsum(), vdims='y2')
+
+    overlay = (c1 * c2).opts(multi_y=True)
+
+    s = MultiAxisTap(source=overlay)
+
+    page = serve_hv(overlay)
+
+    hv_plot = page.locator('.bk-events')
+
+    expect(hv_plot).to_have_count(1)
+
+    hv_plot.click()
+
+    wait_until(lambda: (
+        s.xs == {'x': 9.5} and s.ys == {'y1': 22.5, 'y2': 95.0}
     ), page)
 
 
