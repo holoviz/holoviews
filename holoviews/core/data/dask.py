be00f989ff226dc12e759059ab28ac789e6bae1b
Simon Høxbro Hansen <simon.hansen@me.com>
2024-10-01 14:11:13 +0200

    Only apply fix if pandas version is lower than 2.2

--- holoviews/core/data/dask.py
+++ holoviews/core/data/dask.py
@@ -218,6 +218,8 @@ class DaskInterface(PandasInterface):
         for coord in indices:
             if any(isinstance(c, float) and np.isnan(c) for c in coord):
                 continue
+            if len(coord) == 1 and not util.PANDAS_GE_220:
+                coord = coord[0]
             group = group_type(groupby.get_group(coord), **group_kwargs)
             data.append((coord, group))
         if issubclass(container_type, NdMapping):
