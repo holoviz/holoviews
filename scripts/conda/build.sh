380ec3fe16df9c9f89d0cc2d7768e9e347390a53
Simon Høxbro Hansen <simon.hansen@me.com>
2024-11-27 09:21:01 +0100

    Use --package-format command

--- scripts/conda/build.sh
+++ scripts/conda/build.sh
@@ -9,7 +9,6 @@ python -m build --sdist .
 VERSION=$(python -c "import $PACKAGE; print($PACKAGE._version.__version__)")
 export VERSION
 
-conda config --env --set conda_build.pkg_format 2
-conda build scripts/conda/recipe --no-anaconda-upload --no-verify
+conda build scripts/conda/recipe --no-anaconda-upload --no-verify -c conda-forge --package-format 2
 
 mv "$CONDA_PREFIX/conda-bld/noarch/$PACKAGE-$VERSION-py_0.conda" dist
