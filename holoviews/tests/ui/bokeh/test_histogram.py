ab83c0e5a048d86b3eb2d82f7593f0982cc460f1
Simon Høxbro Hansen <hoxbro@protonmail.com>
2025-05-19 15:14:32 +0200

    Convert test to parameterize

--- holoviews/tests/ui/bokeh/test_histogram.py
+++ holoviews/tests/ui/bokeh/test_histogram.py
@@ -21,9 +21,20 @@ def watch_hook(dim, pos):
 
     return create_hook
 
+
 @pytest.mark.usefixtures("bokeh_backend")
-def test_histogram_logy(serve_hv):
-    hist = Histogram(([1, 3, 6, 9], [1, 10, 0, 100])).opts(logy=True, hooks=[watch_hook("y", "start")])
+@pytest.mark.parametrize(
+    ["opts_kwargs", "hook", "drag_direction"],
+    [
+        ({"logy": True}, watch_hook("y", "start"), "down"),
+        ({"logx": True, "invert_axes": True}, watch_hook("x", "start"), "right"),
+        ({"logy": True, "invert_yaxis": True}, watch_hook("y", "end"), "up"),
+        ({"logx": True, "invert_axes": True, "invert_xaxis": True}, watch_hook("x", "end"), "left"),
+    ],
+    ids=["logy", "logx", "logy-invert", "logx-invert"],
+)
+def test_histogram_log_scaling(serve_hv, opts_kwargs, hook, drag_direction):
+    hist = Histogram(([1, 3, 6, 9], [1, 10, 0, 100])).opts(hooks=[hook], **opts_kwargs)
 
     page = serve_hv(hist)
     hv_plot = page.locator(".bk-events")
@@ -34,93 +45,21 @@ def test_histogram_logy(serve_hv):
     center_y = bbox["y"] + bbox["height"] / 2
     page.mouse.move(center_x, center_y)
 
-    bottom_y = bbox["y"] - bbox["height"]
+    match drag_direction:
+        case "down":
+            target_x, target_y = center_x, bbox["y"] - bbox["height"]
+        case "up":
+            target_x, target_y = center_x, bbox["y"] + bbox["height"]
+        case "right":
+            target_x, target_y = bbox["x"] + bbox["width"], center_y
+        case "left":
+            target_x, target_y = bbox["x"] - bbox["width"], center_y
+        case _:
+            raise ValueError(f"Unknown drag direction: {drag_direction}")
+
     page.mouse.down()
-    page.mouse.move(center_x, bottom_y)
+    page.mouse.move(target_x, target_y)
     page.mouse.up()
-
-
-    def f():
-        cds_value = page.evaluate("() => window.bokeh_cds_data")[0]
-        range_value = page.evaluate("() => window.bokeh_range")
-        assert cds_value != 0.01  # default value
-        assert cds_value == range_value
-
-    wait_until(f, page=page)
-
-
-@pytest.mark.usefixtures("bokeh_backend")
-def test_histogram_logx(serve_hv):
-    hist = Histogram(([1, 3, 6, 9], [1, 10, 0, 100])).opts(logx=True, invert_axes=True, hooks=[watch_hook("x", "start")])
-
-    page = serve_hv(hist)
-    hv_plot = page.locator(".bk-events")
-    expect(hv_plot).to_have_count(1)
-    bbox = hv_plot.bounding_box()
-
-    center_x = bbox["x"] + bbox["width"] / 2
-    center_y = bbox["y"] + bbox["height"] / 2
-    page.mouse.move(center_x, center_y)
-
-    bottom_x = bbox["x"] + bbox["width"]
-    page.mouse.down()
-    page.mouse.move(bottom_x, center_y)
-    page.mouse.up()
-
-
-    def f():
-        cds_value = page.evaluate("() => window.bokeh_cds_data")[0]
-        range_value = page.evaluate("() => window.bokeh_range")
-        assert cds_value != 0.01  # default value
-        assert cds_value == range_value
-
-    wait_until(f, page=page)
-
-@pytest.mark.usefixtures("bokeh_backend")
-def test_histogram_logy_invert(serve_hv):
-    hist = Histogram(([1, 3, 6, 9], [1, 10, 0, 100])).opts(logy=True, invert_yaxis=True, hooks=[watch_hook("y", "end")])
-
-    page = serve_hv(hist)
-    hv_plot = page.locator(".bk-events")
-    expect(hv_plot).to_have_count(1)
-    bbox = hv_plot.bounding_box()
-
-    center_x = bbox["x"] + bbox["width"] / 2
-    center_y = bbox["y"] + bbox["height"] / 2
-    page.mouse.move(center_x, center_y)
-
-    bottom_y = bbox["y"] + bbox["height"]
-    page.mouse.down()
-    page.mouse.move(center_x, bottom_y)
-    page.mouse.up()
-
-    def f():
-        cds_value = page.evaluate("() => window.bokeh_cds_data")[0]
-        range_value = page.evaluate("() => window.bokeh_range")
-        assert cds_value != 0.01  # default value
-        assert cds_value == range_value
-
-    wait_until(f, page=page)
-
-
-@pytest.mark.usefixtures("bokeh_backend")
-def test_histogram_logx_invert(serve_hv):
-    hist = Histogram(([1, 3, 6, 9], [1, 10, 0, 100])).opts(logx=True, invert_axes=True, invert_xaxis=True, hooks=[watch_hook("x", "end")])
-
-    page = serve_hv(hist)
-    hv_plot = page.locator(".bk-events")
-    expect(hv_plot).to_have_count(1)
-    bbox = hv_plot.bounding_box()
-
-    center_x = bbox["x"] + bbox["width"] / 2
-    center_y = bbox["y"] + bbox["height"] / 2
-    page.mouse.move(center_x, center_y)
-
-    bottom_x = bbox["x"] - bbox["width"]
-    page.mouse.down()
-    page.mouse.move(bottom_x, center_y)
-    page.mouse.up()
-
 
     def f():
         cds_value = page.evaluate("() => window.bokeh_cds_data")[0]
