b682afefa6ff1c03842fcba49f93a7f0db5b6214
Simon Høxbro Hansen <hoxbro@protonmail.com>
2025-05-29 15:49:48 +0200

    clean up and add formatting

--- holoviews/plotting/bokeh/raster.py
+++ holoviews/plotting/bokeh/raster.py
@@ -22,11 +22,11 @@ _EPOCH = np.datetime64("1970-01-01", "ns")
 
 
 class HoverModel(DataModel):
-    __xy__ = bp.Any()
-    d = bp.Any()
+    xy = bp.Any()
+    data = bp.Any()
+
 
 class ServerHoverMixin(param.Parameterized):
-    _model_cache = {}
 
     selector_in_hovertool = param.Boolean(default=True, doc="""
         Whether to show the selector in HoverTool.""")
@@ -72,39 +72,29 @@ class ServerHoverMixin(param.Parameterized):
                 if vdim in vars:
                     vars.remove(vdim)
 
+        hover_model = HoverModel(data={})
         dims = (*coords, *vars)
-
-        # Create a dynamic custom DataModel with the dims as attributes
-        # __xy__ is the cursor position
-        # print(self._model_cache)
-        # if dims in self._model_cache:
-        #     HoverModel = self._model_cache[dims]
-        # else:
-        #     HoverModel = self._model_cache[dims] = type(
-        #         f"HoverModel_{uuid.uuid4().hex}",
-        #         (DataModel,),
-        #         {d: bp.Any() for d in ("__xy__", *dims)},
-        #     )
-
-        hover_model = HoverModel(d={})
         dtypes = {**data.coords.dtypes, **data.data_vars.dtypes}
         is_datetime = [dtypes[c].kind == "M" for c in data.coords]
         def _create_row(attr):
             kwargs = {}
             if BOKEH_GE_3_7_0:
-                kind = dtypes[attr].kind
-                if kind in "uifO":
-                    kwargs["format"] = "@{custom}"
-                    kwargs["formatter"] = CustomJS(args=dict(hover_model=hover_model, attr=attr), code="""
-                    export default ({hover_model, attr}) => {
-                      return hover_model.d[attr];
-                    }; """)
-                elif kind == "M":
-                    kwargs["formatter"] = "datetime"
-                    kwargs["format"] = "%Y-%m-%d %H:%M:%S"
+                kwargs["format"] = "@{custom}"
+                kwargs["formatter"] = CustomJS(args=dict(hover_model=hover_model, attr=attr, fmt=dtypes[attr].kind), code="""
+                export default ({hover_model, attr, fmt}) => {
+                  const templating = Bokeh.require("core/util/templating");
+                  const value = hover_model.data[attr];
+                  if (fmt == "M") {
+                    const formatter = templating.DEFAULT_FORMATTERS.datetime;
+                    return formatter(value, "%Y-%m-%d %H:%M:%S")
+                  } else {
+                    const formatter = templating.get_formatter();
+                    return formatter(value)
+                  }
+                }; """)
             return (
                 Span(children=[f"{ht.get(attr, attr)}:"], style={"color": "#26aae1", "text_align": "right"}),
-                Span(children=[ValueOf(obj=hover_model, attr="d", **kwargs)], style={"text_align": "left"}),
+                Span(children=[ValueOf(obj=hover_model, attr="data", **kwargs)], style={"text_align": "left"}),
             )
         children = [el for dim in dims for el in _create_row(dim)]
 
@@ -144,7 +134,7 @@ class ServerHoverMixin(param.Parameterized):
         hover.tooltips = grid
         hover.callback = CustomJS(
             args={"position": hover_model},
-            code="export default ({position}, _, {geometry: {x, y}}) => {position.__xy__ = [x, y]}",
+            code="export default ({position}, _, {geometry: {x, y}}) => {position.xy = [x, y]}",
         )
 
         def on_change(attr, old, new):
@@ -165,11 +155,11 @@ class ServerHoverMixin(param.Parameterized):
             data_coords = {dim: data_sel['coords'][dim]['data'] for dim in coords}
             data_vars = {dim: data_sel['data_vars'][dim]['data'] for dim in vars}
             with hold(self.document):
-                hover_model.update(d={**data_coords, **data_vars})
+                hover_model.update(data={**data_coords, **data_vars})
                 if self.comm:  # Jupyter Notebook
                     self.push()
 
-        hover_model.on_change("__xy__", on_change)
+        hover_model.on_change("xy", on_change)
 
         return tools
 
