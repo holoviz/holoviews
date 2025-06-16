09ffcbb2b4ce5d9eb6f56b2a1af3fbba0f2b39a7
Simon Høxbro Hansen <simon.hansen@me.com>
2025-06-16 12:00:00 +0200

    Update holoviews/util/__init__.py
    Co-authored-by: Philipp Rudiger <prudiger@anaconda.com>

--- holoviews/util/__init__.py
+++ holoviews/util/__init__.py
@@ -296,7 +296,7 @@ class opts(param.ParameterizedFunction, metaclass=OptsMeta):
         if kwargs and len(kwargs) != 1 and next(iter(kwargs.keys())) != 'backend':
             raise Exception('opts.defaults only accepts "backend" keyword argument')
 
-        backend=kwargs.get('backend')
+        backend = kwargs.get('backend')
         expanded = cls._expand_options(merge_options_to_dict(options), backend=backend)
         expanded = expanded or {}
         cls._linemagic(expanded, backend=backend)
