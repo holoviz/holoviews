c0eaa621269c7b1448bab15dc8345e7fe828fc06
Simon Høxbro Hansen <simon.hansen@me.com>
2025-06-24 21:10:59 +0200

    docs: Add docs fixes (#6641)

--- doc/conf.py
+++ doc/conf.py
@@ -25,12 +25,10 @@ hv.extension.inline = False
 html_theme = 'pydata_sphinx_theme'
 html_logo = '_static/logo_horizontal.png'
 html_favicon = '_static/favicon.ico'
+html_show_sourcelink = False
 
 html_static_path += ['_static']
-
-html_css_files += [
-    'css/custom.css'
-]
+html_css_files += ['css/custom.css']
 
 html_theme_options = {
     'github_url': 'https://github.com/holoviz/holoviews',
