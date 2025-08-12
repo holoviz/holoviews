4720ca72f913e42b05c14711e2ee6b2a6baf6eb4
Philipp Rudiger <prudiger@anaconda.com>
2025-08-12 12:36:44 +0200

    Fix categorical RGB legend when len(categories) > len(colors)

--- holoviews/plotting/util.py
+++ holoviews/plotting/util.py
@@ -1378,7 +1378,7 @@ class categorical_legend(Operation):
         colors = shade_op.color_key or ds.colors.Sets1to3
         color_data = [(0, 0, cat) for cat in cats]
         if isinstance(colors, list):
-            cat_colors = {cat: colors[i] for i, cat in enumerate(cats)}
+            cat_colors = {cat: colors[i % len(colors)] for i, cat in enumerate(cats)}
         else:
             cat_colors = {cat: colors[cat] for cat in cats}
         cmap = {}
