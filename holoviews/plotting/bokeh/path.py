3e78992e61a00b7a7a630406f4620b10d2402fdf
Simon Høxbro Hansen <hoxbro@protonmail.com>
2025-08-29 13:15:05 +0200

    Raise more informative error if for adjoned layout

--- holoviews/plotting/bokeh/path.py
+++ holoviews/plotting/bokeh/path.py
@@ -188,6 +188,9 @@ class DendrogramPlot(PathPlot):
     def initialize_plot(self, ranges=None, plot=None, plots=None, source=None):
         plot = super().initialize_plot(ranges, plot, plots, source)
         if self.adjoined:
+            if self.layout_num and self.shared_axes:
+                msg = "Adjoined dendrogram in a Layout, does not currently support `.opts(shared_axes=True)`"
+                raise RuntimeError(msg)
             pos = ["main", "right", "top"][len(plots)]
             main = self.adjoined[0]
             if pos == "right":
