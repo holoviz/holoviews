d55aa7a2c5fd76b2bb34b7d02a9dcbc82c3ab13f
thuydotm <thuydtm.csk9@gmail.com>
2025-09-19 13:56:36 +0700

    Update holoviews/plotting/bokeh/element.py
    Co-authored-by: Simon Høxbro <hoxbro@protonmail.com>

--- holoviews/plotting/bokeh/element.py
+++ holoviews/plotting/bokeh/element.py
@@ -1795,7 +1795,8 @@ class ElementPlot(BokehPlot, GenericElementPlot):
             y_source_range = self.handles['y_range']
             if isinstance(self.subcoordinate_y, bool):
                 if "subcoordinate_y" not in y_source_range.tags[1]:
-                    msg =  'Failed retrieving "subcoordinate_y". Labels mismatched for initial and updated DynamicMap plots.'
+                    See https://github.com/holoviz/holoviews/issues/6071
+                    msg = 'Failed retrieving "subcoordinate_y". Labels mismatched for initial and updated DynamicMap plots.'
                     raise RuntimeError(msg)
                 center = y_source_range.tags[1]['subcoordinate_y']
                 offset = self.subcoordinate_scale/2.
