2a17a2cdefa9a2bb1704c8fbdf7ae13c93405707
Simon Høxbro Hansen <hoxbro@protonmail.com>
2025-12-02 10:45:15 +0100

    Add ui test

--- holoviews/tests/ui/bokeh/test_hover.py
+++ holoviews/tests/ui/bokeh/test_hover.py
@@ -515,3 +515,36 @@ def test_hover_heatmap_image(serve_hv, x_axis_type, y_axis_type):
     expect(tooltip).to_contain_text("x: 10.100" if is_lambda(x_axis_type) else "x: 10")
     expect(tooltip).to_contain_text("y: 4.100" if is_lambda(y_axis_type) else "y: 4")
     expect(tooltip).to_contain_text("z: 53")
+
+
+@pytest.mark.usefixtures("bokeh_backend")
+def test_hover_across_dynamicmaps(serve_panel):
+    data = pd.DataFrame({"x": range(2), "y": range(2), "category": ["A", "B"]})
+
+
+    def inner(value):
+        el = (
+            hv.Dataset(data)
+            .to(hv.Points, kdims=["x", "y"], groupby="category")
+            .opts(tools=["hover"], size=10)
+        )
+        el = el.get(value)
+        el.opts(show_legend=False, xlim=(-1, 3), ylim=(-1, 3), hit_dilation=100)
+        return el.overlay()
+
+
+    widget = pn.widgets.MultiSelect(value=["A"], options=["A", "B"])
+    col = pn.Column(widget, hv.DynamicMap(inner, streams=[widget.param.value]))
+    page = serve_panel(col)
+
+    hv_plot = page.locator(".bk-events")
+    expect(hv_plot).to_have_count(1)
+    bbox = hv_plot.bounding_box()
+
+    for i, cat in enumerate(("A", "B")):
+        widget.value = [cat]
+        page.mouse.move(bbox["x"] + bbox["width"] / 2, bbox["y"] + bbox["height"] / 2)
+        page.mouse.up()
+        tooltip = page.locator(".bk-Tooltip")
+        expect(tooltip).to_have_count(1)
+        expect(tooltip.first).to_contain_text(f"category: {cat} x: {i} y: {i}")
