b6d2ef7a00092bfd193d343c22d2f288d0565a4f
Simon Høxbro Hansen <hoxbro@protonmail.com>
2026-01-21 15:30:01 +0100

    remove old location of IPTestCase

--- holoviews/ipython/__init__.py
+++ holoviews/ipython/__init__.py
@@ -25,15 +25,6 @@ def show_traceback():
     print(FULL_TRACEBACK)
 
 
-def __getattr__(attr):
-    if attr == "IPTestCase":
-        from ..element.comparison import IPTestCase
-        from ..util.warnings import deprecated
-        deprecated("1.23.0", old="holoviews.ipython.IPTestCase", new="holoviews.element.comparison.IPTestCase")
-        return IPTestCase
-    raise AttributeError(f"module {__name__!r} has no attribute {attr!r}")
-
-
 class notebook_extension(extension):
     """Notebook specific extension to hv.extension that offers options for
     controlling the notebook environment.
