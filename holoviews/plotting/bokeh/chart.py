938da68baf398c3767cf7190a97b266e46624467
Simon Høxbro Hansen <hoxbro@protonmail.com>
2026-03-30 15:39:51 +0200

    enh: Add Histogram.stack

--- holoviews/plotting/bokeh/chart.py
+++ holoviews/plotting/bokeh/chart.py
@@ -16,7 +16,7 @@ from ...core.util import dimension_sanitizer, dtype_kind, isdatetime, isfinite
 from ...operation import interpolate_curve
 from ...util.transform import dim
 from ...util.warnings import warn
-from ..mixins import AreaMixin, BarsMixin, DonutMixin, SpikesMixin, WaterfallMixin
+from ..mixins import AreaMixin, BarsMixin, DonutMixin, HistogramMixin, SpikesMixin, WaterfallMixin
 from ..util import get_min_distance, rgb2hex
 from .element import ColorbarPlot, CompositeElementPlot, ElementPlot, LegendPlot, OverlayPlot
 from .selection import BokehOverlaySelectionDisplay
@@ -465,7 +465,7 @@ class CurvePlot(ElementPlot):
         return data, mapping, style
 
 
-class HistogramPlot(ColorbarPlot):
+class HistogramPlot(HistogramMixin, ColorbarPlot):
     selection_display = BokehOverlaySelectionDisplay(color_prop=["color", "fill_color"])
 
     style_opts = base_properties + fill_properties + line_properties + ["cmap"]
@@ -514,16 +514,15 @@ class HistogramPlot(ColorbarPlot):
             if hasattr(edges, "compute"):
                 edges = edges.compute()
             data = dict(top=values, left=edges[:-1], right=edges[1:])
+            if element._stacked:
+                baseline = element.dimension_values(2)
+                data["bottom"] = baseline
+                if self.invert_axes:
+                    mapping["left"] = "bottom"
+                else:
+                    mapping["bottom"] = "bottom"
             self._get_hover_data(data, element)
         return data, mapping, style
-
-    def get_extents(self, element, ranges, range_type="combined", **kwargs):
-        ydim = element.get_dimension(1)
-        s0, s1 = ranges[ydim.label]["soft"]
-        s0 = min(s0, 0) if isfinite(s0) else 0
-        s1 = max(s1, 0) if isfinite(s1) else 0
-        ranges[ydim.label]["soft"] = (s0, s1)
-        return super().get_extents(element, ranges, range_type)
 
     def _update_range(self, axis_range, low, high, factors, invert, shared, log, streaming=False):
         # We allow zero values with histogram
