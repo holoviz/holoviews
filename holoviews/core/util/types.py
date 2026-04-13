c45f3064191e748dd087b9c9cf87cebbab153f4e
Simon Høxbro Hansen <hoxbro@protonmail.com>
2026-04-13 17:42:28 +0200

    Add information to types.py

--- holoviews/core/util/types.py
+++ holoviews/core/util/types.py
@@ -11,8 +11,28 @@ import numpy as np
 
 from .dependencies import cftime, pd
 
-# gen_types is copied from param, can be removed when
-# we support 2.2 or greater
+if t.TYPE_CHECKING:
+    from pandas.core.arrays.masked import BaseMaskedArray
+    from pandas.core.dtypes.dtypes import DatetimeTZDtype
+    from pandas.core.dtypes.generic import ABCExtensionArray, ABCIndex, ABCSeries
+
+    _PdDatetimeT: t.TypeAlias = pd.Timestamp | pd.Period | DatetimeTZDtype
+    _PdTimedeltaT: t.TypeAlias = pd.Timedelta
+    _CftimeT: t.TypeAlias = cftime.datetime
+    _DatetimeT: t.TypeAlias = (
+        dt.datetime | dt.date | dt.time | np.datetime64 | _PdDatetimeT | _CftimeT
+    )
+    _TimedeltaT: t.TypeAlias = dt.timedelta | np.timedelta64 | _PdTimedeltaT
+    _ArraylikeT: t.TypeAlias = np.ndarray | nw.Series | ABCIndex | ABCSeries | ABCExtensionArray
+    _MaskedT: t.TypeAlias = np.ma.core.MaskedArray | BaseMaskedArray
+
+    _YieldT = t.TypeVar("_YieldT")
+
+    class _GenFunc(t.Protocol[_YieldT]):
+        __name__: str
+
+        def __call__(self) -> t.Iterator[_YieldT]: ...
+
 
 _module_count = 0
 
@@ -50,11 +70,13 @@ class _GeneratorIs(metaclass=_GeneratorIsMeta):
         yield from cls._get_types()
 
 
-def gen_types(gen_func):
+def gen_types(gen_func: _GenFunc[_YieldT]) -> tuple[_YieldT, ...]:
     """
     Decorator which takes a generator function which yields difference types
     make it so it can be called with isinstance and issubclass.
     """
+    if t.TYPE_CHECKING:
+        return tuple(gen_func())
     if not inspect.isgeneratorfunction(gen_func):
         msg = "gen_types decorator can only be applied to generator"
         raise TypeError(msg)
@@ -66,7 +88,7 @@ generator_types = (zip, range, GeneratorType)
 
 
 @gen_types
-def pandas_datetime_types():
+def pandas_datetime_types() -> t.Iterator[type[_PdDatetimeT]]:
     if pd:
         from pandas.core.dtypes.dtypes import DatetimeTZDtype
 
@@ -74,32 +96,32 @@ def pandas_datetime_types():
 
 
 @gen_types
-def pandas_timedelta_types():
+def pandas_timedelta_types() -> t.Iterator[type[_PdTimedeltaT]]:
     if pd:
         yield pd.Timedelta
 
 
 @gen_types
-def cftime_types():
+def cftime_types() -> t.Iterator[type[_CftimeT]]:
     if cftime:
         yield cftime.datetime
 
 
 @gen_types
-def datetime_types():
+def datetime_types() -> t.Iterator[type[_DatetimeT]]:
     yield from (dt.datetime, dt.date, dt.time, np.datetime64)
     yield from pandas_datetime_types
     yield from cftime_types
 
 
 @gen_types
-def timedelta_types():
+def timedelta_types() -> t.Iterator[type[_TimedeltaT]]:
     yield from (dt.timedelta, np.timedelta64)
     yield from pandas_timedelta_types
 
 
 @gen_types
-def arraylike_types():
+def arraylike_types() -> t.Iterator[type[_ArraylikeT]]:
     yield from (np.ndarray, nw.Series)
     if pd:
         from pandas.core.dtypes.generic import ABCExtensionArray, ABCIndex, ABCSeries
